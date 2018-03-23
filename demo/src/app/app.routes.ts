7752a25f94cfceb54ca845170ccfffd6d4167923
luxiaobei <luxiaobei@worktile.com>
2018-03-23 17:37:02 +0800

    #123228 Badges 组件

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { DemoButtonSectionComponent } from './components/+button/button-section.
 import { DemoPopBoxSectionComponent } from './components/+pop-box/pop-box-section.component';
 import { DemoRebootSectionComponent } from './components/+reboot/reboot-section.component';
 import { DemoTableSectionComponent } from './components/+table/table-section.component';
+import { DemoBadgeSectionComponent } from './components/+badge/badge-section.component';
 
 
 const appRoutes = [
@@ -20,6 +21,10 @@ const appRoutes = [
     {
         path: 'components/table',
         component: DemoTableSectionComponent
+    },
+    {
+        path: 'components/badge',
+        component: DemoBadgeSectionComponent
     }
 ];
 
--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,24 @@ import { ThyButtonModule } from './button/button.module';
 import { FirstComponent } from './first/first.component';
 import { NgxPopBoxModule } from './pop-box/pop-box.module';
 
+// import { ThyAvatarModule } from './avatar/avatar.module';
+import { ThyBadgeModule } from './badge/badge.module';
+
 @NgModule({
     declarations: [
         FirstComponent
     ],
     exports: [
         FirstComponent,
-        ThyButtonModule
+        ThyButtonModule,
+        ThyBadgeModule
     ],
     providers: [
     ],
     imports: [
         ThyButtonModule,
-        NgxPopBoxModule
+        NgxPopBoxModule,
+        ThyBadgeModule
     ]
 })
 export class NgxTethysModule {
