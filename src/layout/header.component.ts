1db39240ba96b6af92297982f15b44bb17f4934b
why520crazy <why520crazy@163.com>
2018-04-03 17:16:53 +0800

    layout change

--- src/layout/header.component.ts
+++ src/layout/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostBinding } from '@angular/core';
-
+import { Component, HostBinding, Input } from '@angular/core';
+import { inputValueToBoolean } from '../util/helpers';
 @Component({
     selector: 'thy-header',
     preserveWhitespaces: false,
@@ -8,5 +8,13 @@ import { Component, HostBinding } from '@angular/core';
     `
 })
 export class ThyHeaderComponent {
+
     @HostBinding('class.thy-layout-header') thyLayoutHeaderClass = true;
+
+    @HostBinding('class.header-has-border') _thyHasBorder = false;
+
+    @Input('thyHasBorder')
+    set thyHasBorder(value: string) {
+        this._thyHasBorder = inputValueToBoolean(value);
+    }
 }
