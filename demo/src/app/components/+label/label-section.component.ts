a5039f5e933633c8942f2cf7de75b3751f6884f6
mengshuicmq <mengshuicmq@163.com>
2018-04-25 19:32:05 +0800

    label param

--- demo/src/app/components/+label/label-section.component.ts
+++ demo/src/app/components/+label/label-section.component.ts
@@ -8,6 +8,45 @@ export class DemoLabelSectionComponent {
     public color?: string;
     public color1?: string;
 
+    public apiParameters = [
+        {
+            property: 'thyLabel',
+            description: '标签类型（default、primary、success、info、danger）',
+            type: 'String',
+            default: ''
+        },
+        {
+            property: 'thyLabelType',
+            description: '状态标签',
+            type: 'String',
+            default: 'state'
+        },
+        {
+            property: 'thyHasHover',
+            description: '滑过效果',
+            type: 'Boolean',
+            default: 'false'
+        },
+        {
+            property: 'thyBeforeIcon',
+            description: '可在显示文案前添加图标,如添加图标',
+            type: 'String',
+            default: ''
+        },
+        {
+            property: 'thyAfterIcon',
+            description: '可在显示文案后添加图标，如删除图标',
+            type: 'String',
+            default: ''
+        },
+        {
+            property: '[thyLabelColor]',
+            description: '自定义颜色，#f969aa 或者变量，需要同时加上 thyLabel 属性',
+            type: 'String',
+            default: ''
+        }
+    ];
+
     constructor() {
         this.color = '#7076fa';
         this.color1 = '#f969aa';
