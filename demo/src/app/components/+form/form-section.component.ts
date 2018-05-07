e8f07c933f48d981d46b464b8c78ac4e7ec59102
why520crazy <why520crazy@163.com>
2018-05-07 20:43:59 +0800

    form change

--- demo/src/app/components/+form/form-section.component.ts
+++ demo/src/app/components/+form/form-section.component.ts
@@ -13,7 +13,31 @@ export class DemoFormSectionComponent {
 
     submitSuccess = false;
 
-    public apiParameters = [
+    public apiThyFormParameters = [
+        {
+            property: 'thyForm',
+            description: '表示这个 Form 由 Thy 控制',
+            type: 'Directive',
+            default: ''
+        },
+        {
+            property: 'thyLayout',
+            description: `布局, 'horizontal' | 'vertical' | 'inline' , 默认水平居中 horizontal， 其他2种布局待开发`,
+            type: 'String',
+            default: ''
+        }
+    ];
+
+    public apiThyFormSubmitParameters = [
+        {
+            property: 'thyFormSubmit',
+            description: 'Form 验证通过的提交函数',
+            type: 'Function',
+            default: ''
+        },
+    ];
+
+    public apiFormGroupParameters = [
         {
             property: 'thyLabelText',
             description: 'Label 文本',
