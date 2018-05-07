72f1c9015d5afb2a2743cdfe819800720064073f
why520crazy <why520crazy@163.com>
2018-05-07 17:23:12 +0800

    form group change

--- demo/src/app/components/+form/form-section.component.ts
+++ demo/src/app/components/+form/form-section.component.ts
@@ -11,6 +11,45 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class DemoFormSectionComponent {
 
+    public apiParameters = [
+        {
+            property: 'thyLabelText',
+            description: 'Label 文本',
+            type: 'String',
+            default: ''
+        },
+        {
+            property: 'thyLabelTextTranslateKey',
+            description: 'Label 文本多语言 Key',
+            type: 'String',
+            default: ''
+        },
+        {
+            property: 'thyLabelRequired',
+            description: 'Label 是否显示必填项样式',
+            type: 'Boolean',
+            default: ''
+        },
+        {
+            property: 'thyFeedbackIcon',
+            description: '反馈图标，比如日期输入框显示日期的图标，常用输入 date 表示时间 wtf wtf-schedule-o',
+            type: 'String',
+            default: ''
+        },
+        {
+            property: 'thyTips',
+            description: '提示文案',
+            type: 'String',
+            default: ''
+        },
+        {
+            property: 'formGroup',
+            description: '自定义内容的 Template',
+            type: 'NgTemplate',
+            default: ''
+        }
+    ];
+
     model: any = {
         select: 1
     };
@@ -30,6 +69,5 @@ export class DemoFormSectionComponent {
     }
 
     save(form: any) {
-        debugger;
     }
 }
