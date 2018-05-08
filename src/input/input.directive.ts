bbcfc7277880752cd312215af7bf37a460629c6a
wangkai <walkerkay@163.com>
2018-05-08 13:59:25 +0800

    input size , input search ellipse

--- src/input/input.directive.ts
+++ src/input/input.directive.ts
@@ -1,8 +1,37 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input, ElementRef } from '@angular/core';
+import { UpdateHostClassService } from '../shared';
 
+export type InputSize = 'xs' | 'sm' | 'md' | 'lg' | '';
+
+const inputGroupSizeMap = {
+    'xs': ['form-control-xs'],
+    'sm': ['form-control-sm'],
+    'md': ['form-control-md'],
+    'lg': ['form-control-lg']
+};
 @Directive({
-    selector: '[thyInput]'
+    selector: '[thyInput]',
+    providers: [
+        UpdateHostClassService
+    ],
 })
 export class ThyInputDirective {
+
     @HostBinding('class.form-control') _isFormControl = true;
+
+    @Input()
+    set thySize(size: InputSize) {
+        if (size && inputGroupSizeMap[size]) {
+            this.updateHostClassService.updateClass(inputGroupSizeMap[size]);
+        } else {
+            this.updateHostClassService.updateClass([]);
+        }
+    }
+
+    constructor(
+        private updateHostClassService: UpdateHostClassService,
+        private elementRef: ElementRef
+    ) {
+        this.updateHostClassService.initializeElement(elementRef.nativeElement);
+    }
 }
