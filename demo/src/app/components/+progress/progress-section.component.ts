caacfe4499d28b2006d09d776c9b7935ebf2fa90
machuanjia <machuanjia@worktile.com>
2018-05-17 14:56:09 +0800

    add progress in thymudules

--- demo/src/app/components/+progress/progress-section.component.ts
+++ demo/src/app/components/+progress/progress-section.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: '',
     templateUrl: './progress-section.component.html'
 })
 
-export class DemoProgressSectionComponent {
+export class DemoProgressSectionComponent implements OnInit {
+    stacked: any[] = [];
     constructor() {
 
+    }
+    randomStacked(): void {
+        const types = ['success', 'info', 'warning', 'danger'];
+
+        this.stacked = [];
+        const n = Math.floor(Math.random() * 4 + 1);
+        for (let i = 0; i < n; i++) {
+            const index = Math.floor(Math.random() * 4);
+            const value = Math.floor(Math.random() * 27 + 3);
+            this.stacked.push({
+                value,
+                type: types[index]
+            });
+        }
+    }
+    ngOnInit() {
+        this.randomStacked();
     }
 }
