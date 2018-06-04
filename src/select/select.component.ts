04c21ad86a5db7bb7abfef1857eb82132a48cf5d
why520crazy <why520crazy@163.com>
2018-06-04 18:40:37 +0800

    select style change

--- src/select/select.component.ts
+++ src/select/select.component.ts
@@ -1,5 +1,6 @@
-import { Component, forwardRef, HostBinding, Input, ElementRef } from '@angular/core';
+import { Component, forwardRef, HostBinding, Input, ElementRef, OnInit } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { UpdateHostClassService } from '../shared/update-host-class.service';
 
 export type InputSize = 'xs' | 'sm' | 'md' | 'lg' | '';
 
@@ -14,20 +15,25 @@ const noop = () => {
             provide: NG_VALUE_ACCESSOR,
             useExisting: forwardRef(() => ThySelectComponent),
             multi: true
-        }
+        },
+        UpdateHostClassService
     ]
 })
-export class ThySelectComponent implements ControlValueAccessor {
+export class ThySelectComponent implements ControlValueAccessor, OnInit {
 
     // The internal data model
     _innerValue: any = null;
     _disabled = false;
+    _size: InputSize;
     private onTouchedCallback: () => void = noop;
     private onChangeCallback: (_: any) => void = noop;
 
     @HostBinding('class.thy-select') _isSelect = true;
 
-    @Input() thySize: InputSize;
+    @Input()
+    set thySize(value: InputSize) {
+        this._size = value;
+    }
 
     writeValue(obj: any): void {
         if (obj !== this._innerValue) {
@@ -48,11 +54,18 @@ export class ThySelectComponent implements ControlValueAccessor {
     }
 
     constructor(
-        private elementRef: ElementRef
+        private elementRef: ElementRef,
+        private updateHostClassService: UpdateHostClassService
     ) {
+        this.updateHostClassService.initializeElement(elementRef.nativeElement);
     }
 
     ngModelChange() {
         this.onChangeCallback(this._innerValue);
+    }
+
+    ngOnInit() {
+        const classes = this._size ? [`thy-select-${this._size}`] : [];
+        this.updateHostClassService.updateClass(classes);
     }
 }
