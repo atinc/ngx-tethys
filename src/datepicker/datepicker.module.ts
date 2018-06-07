1f305c92b319701fc66ae54d3ebd746ab4a15fe4
zhenshuai <zhenshuai@worktile.com>
2018-06-07 16:28:10 +0800

    daterangepicker

--- src/datepicker/datepicker.module.ts
+++ src/datepicker/datepicker.module.ts
@@ -7,21 +7,27 @@ import { ThyButtonModule } from '../button/button.module';
 
 import { ThyDatepickerFormatPipe } from './pipe';
 import { ThyDatepickerDirective } from './datepicker.directive';
+import { ThyDaterangepickerDirective } from './daterangepicker.directive';
 import { ThyDatepickerContainerComponent } from './datepicker-container.component';
+import { ThyDaterangepickerContainerComponent } from './daterangepicker-container.component';
 import { ThyDatepickerConfig } from './datepicker.config';
 import { ThyDatepickerService } from './datepicker.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ThyDaterangepickerConfig } from './daterangepicker.config';
 
 
 @NgModule({
     declarations: [
         ThyDatepickerContainerComponent,
+        ThyDaterangepickerContainerComponent,
         ThyDatepickerDirective,
+        ThyDaterangepickerDirective,
         ThyDatepickerFormatPipe,
     ],
     entryComponents: [
         ThyDatepickerContainerComponent,
+        ThyDaterangepickerContainerComponent,
     ],
     imports: [
         CommonModule,
@@ -33,10 +39,12 @@ import { FormsModule } from '@angular/forms';
     exports: [
         BsDatepickerModule,
         ThyDatepickerDirective,
+        ThyDaterangepickerDirective,
         ThyDatepickerFormatPipe,
     ],
     providers: [
         ThyDatepickerConfig,
+        ThyDaterangepickerConfig,
         ThyDatepickerService,
     ]
 })
--- src/datepicker/i.datepicker.ts
+++ src/datepicker/i.datepicker.ts
@@ -3,10 +3,50 @@ export interface DatepickerValueEntry {
     with_time: boolean | number;
 }
 
+export interface DaterangepickerValueEntry {
+    begin: {
+        date: Date | '' | number | any;
+        with_time: boolean | number;
+    };
+    end: {
+        date: Date | '' | number | any;
+        with_time: boolean | number;
+    };
+}
+
 export interface DatepickerInitialState {
     value: DatepickerValueEntry;
     changeValue: Function;
 }
+// #region DatepickerValueShowTypesEnum
+// dateRangTime
+// {
+//     begin: 1234567890,
+//     end: 1234567890
+// }
+// daterangepickerObject
+// {
+//     begin: {
+//          date: new Date(),
+//          with_time: false,
+//     },
+//     end: {
+//          date: new Date(),
+//          with_time: false,
+//     }
+// }
+// daterangepickerTimeObject
+// {
+//     begin: {
+//          date: 1234567890,
+//          with_time: false,
+//     },
+//     end: {
+//          date: 1234567890,
+//          with_time: false,
+//     }
+// }
+// #endregion
 
 export enum DatepickerValueShowTypesEnum {
     dateTime,
@@ -16,6 +56,16 @@ export enum DatepickerValueShowTypesEnum {
     datepickerTimeObject,
     datepickerTimeLongObject,
     datepickerNullValue,
-    nullValue
+    nullValue,
+    daterangepickerObject,
+    daterangepickerTimeObject,
+    daterangepickerNullValue,
+}
+
+
+export enum DatepickerFormatRules {
+    default = 'yyyy-MM-dd',
+    short = 'yyyy-MM-dd',
+    full = 'yyyy-MM-dd HH:mm',
 }
 
