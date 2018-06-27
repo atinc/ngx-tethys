824001bdb0fbe6a95064273751b316207278e878
why520crazy <why520crazy@163.com>
2018-06-27 10:30:09 +0800

    form group footer

--- src/radio/radio.component.ts
+++ src/radio/radio.component.ts
@@ -1,7 +1,6 @@
 import { Component, forwardRef, HostBinding, HostListener, Input, ElementRef } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { ThyTranslate } from '../shared';
-import { inputValueToBoolean } from '../util/helpers';
 import { ThyFormCheckBaseComponent } from '../shared';
 
 
