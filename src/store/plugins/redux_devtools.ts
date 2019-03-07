9d04cd428c403aa8de91d73831036867f0aeba89
pubuzhixing8 <pubuzhixing@gmail.com>
2019-03-07 09:46:06 +0800

    #205682 修复未装reduxdevtool时报错的问题，调整初始化逻辑

--- src/store/plugins/redux_devtools.ts
+++ src/store/plugins/redux_devtools.ts
@@ -1,6 +1,8 @@
+import { isDevMode } from '@angular/core';
 export abstract class StorePlugin {
     // abstract handleNewState(state: Readonly<object>): void;
     abstract handleNewState(actionName: string, state: Readonly<object>): void;
+    abstract isConnectSuccessed(): boolean;
 }
 /**
  * @internal
@@ -10,11 +12,12 @@ export interface ReduxDevtoolsInstance {
 }
 
 export function tinyStateVersion(): string {
-    return 'v0.1.0';
+    return 'v0.5.0';
 }
 
 export class ReduxDevtoolsPlugin implements StorePlugin {
     private _devTools: ReduxDevtoolsInstance | null = null;
+
     _window = window;
     constructor(
     ) {
@@ -26,6 +29,8 @@ export class ReduxDevtoolsPlugin implements StorePlugin {
             (this._window as any)['devToolsExtension'];
 
         if (!globalDevtools) {
+            console.log(`未安装Chrome浏览器的拓展插件: Redux DevTools.`);
+            console.log(`插件下载地址: https://www.chromefor.com/redux-devtools_v2-17-0/`);
             return;
         }
         this._devTools = globalDevtools.connect({
@@ -34,18 +39,24 @@ export class ReduxDevtoolsPlugin implements StorePlugin {
     }
 
     handleNewState(actionName: string, state: object): void {
-        // if (this._config.enabled === false || !this._devTools) {
-        //     return;
-        // }
-        this._devTools.send(actionName, state);
+        if (this.isConnectSuccessed()) {
+            this._devTools.send(actionName, state);
+        }
+    }
+
+    isConnectSuccessed(): boolean {
+        if (this._devTools === null) {
+            return false;
+        }
+        return true;
     }
 }
 
-function getReduxDevTools(): any {
+function getReduxDevToolsPlugin(): any {
     if (!window[`___ReduxDevtoolsPlugin___`]) {
         window[`___ReduxDevtoolsPlugin___`] = new ReduxDevtoolsPlugin();
     }
     return window[`___ReduxDevtoolsPlugin___`];
 }
 
-export default getReduxDevTools;
+export default getReduxDevToolsPlugin;
