b240f95c57924cb64ba57d7081c25b818c3b6942
why520crazy <why520crazy@163.com>
2019-06-03 08:36:08 +0800

    fix(tooltip): change style as design specification

--- src/tooltip/tooltip-animations.ts
+++ src/tooltip/tooltip-animations.ts
@@ -1,10 +1,3 @@
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
 import { animate, AnimationTriggerMetadata, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 // 'cubic-bezier(0, 0, 0.2, 1)';
