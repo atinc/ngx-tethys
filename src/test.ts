71dd9c4ceb411259a6d2e3b45741c9b29e9a4cc8
甄帅 <231640921@qq.com>
2019-06-24 14:52:50 +0800

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -4,6 +4,8 @@ import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
+declare const require: any;
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
