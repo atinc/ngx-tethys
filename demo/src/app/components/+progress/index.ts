fb94df3ad8487f77e092b8cac114805ba6748286
zhangwei <way12123@126.com>
2019-07-02 11:45:16 +0800

    fix(component): exports component

--- demo/src/app/components/+progress/index.ts
+++ demo/src/app/components/+progress/index.ts
@@ -1,4 +1,5 @@
 import { ProgressBasicDemoComponent } from './basic/progress-basic-demo.component';
 import { ProgressStackedDemoComponent } from './stacked/progress-stacked-demo.component';
+import { TemplateComponent } from './template/template.component';
 
-export const PROGRESS_COMPONENTS = [ProgressBasicDemoComponent, ProgressStackedDemoComponent];
+export const PROGRESS_COMPONENTS = [ProgressBasicDemoComponent, ProgressStackedDemoComponent, TemplateComponent];
