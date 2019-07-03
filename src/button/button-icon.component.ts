77f38b135718ce645f9c779cb8d6cb3a6e587b1e
why520crazy <why520crazy@163.com>
2019-07-03 08:58:58 +0800

    feat(button): change thyButtonIcon support svg icon #243057

--- src/button/button-icon.component.ts
+++ src/button/button-icon.component.ts
@@ -1,6 +1,12 @@
 import {
-    Component, Directive, HostBinding,
-    Input, ElementRef, Renderer2, ViewEncapsulation, ChangeDetectionStrategy
+    Component,
+    Directive,
+    HostBinding,
+    Input,
+    ElementRef,
+    Renderer2,
+    ViewEncapsulation,
+    ChangeDetectionStrategy
 } from '@angular/core';
 import { AfterContentInit, OnChanges, OnInit } from '@angular/core';
 import { inputValueToBoolean, isUndefined } from '../util/helpers';
@@ -9,9 +15,9 @@ import { UpdateHostClassService } from '../shared';
 export type IconShape = '' | 'dashed' | 'solid';
 
 const sizeClassesMap: any = {
-    'lg': ['btn-icon-lg'],
-    'sm': ['btn-icon-sm'],
-    'xs': ['btn-icon-xs']
+    lg: ['btn-icon-lg'],
+    sm: ['btn-icon-sm'],
+    xs: ['btn-icon-xs']
 };
 
 const shapeClassesMap: any = {
@@ -29,70 +35,71 @@ const themeClassesMap: any = {
 @Component({
     selector: '[thy-button-icon],[thyButtonIcon]',
     templateUrl: './button-icon.component.html',
-    providers: [
-        UpdateHostClassService
-    ],
+    providers: [UpdateHostClassService],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThyButtonIconComponent implements OnInit {
+    private initialized = false;
 
-    private _initialized = false;
+    private shape: IconShape;
 
-    private _shape: IconShape;
+    private size: string;
 
-    private _size: string;
+    iconPrefix = 'wtf';
 
-    _iconPrefix = 'wtf';
+    iconClasses: string[];
 
-    _iconClasses: string[];
+    icon: string;
 
-    _icon: string;
+    theme: string;
 
-    _theme: string;
+    svgIconName: string;
 
     @HostBinding('class.btn') _isBtn = true;
     @HostBinding('class.btn-icon') _isBtnIcon = true;
     @HostBinding('class.btn-icon-light') _isLighted = false;
     @HostBinding('class.btn-icon-active') _isActive = false;
 
-    _setIconClass(icon: string) {
+    private setIconClass(icon: string) {
         if (icon) {
-            const classes = icon.split(' ');
-            if (classes.length === 1) {
-                classes.unshift('wtf');
+            if (icon.includes('wtf')) {
+                const classes = icon.split(' ');
+                if (classes.length === 1) {
+                    classes.unshift('wtf');
+                }
+                this.iconClasses = classes;
+                this.svgIconName = null;
+            } else {
+                this.svgIconName = icon;
             }
-            this._iconClasses = classes;
         } else {
-            this._iconClasses = null;
+            this.iconClasses = null;
+            this.svgIconName = null;
         }
     }
 
     @Input()
     set thySize(size: string) {
-        this._size = size;
-        if (this._initialized) {
-            this._setClasses();
-        }
+        this.size = size;
+        this.setClasses();
     }
 
     // 字体前缀，默认 wtf
     @Input()
     set thyIcon(icon: string) {
-        this._setIconClass(icon);
+        this.setIconClass(icon);
     }
 
     @Input()
     set thyButtonIcon(icon: string) {
-        this._setIconClass(icon);
+        this.setIconClass(icon);
     }
 
     @Input()
     set thyShape(value: IconShape) {
-        this._shape = value;
-        if (this._initialized) {
-            this._setClasses();
-        }
+        this.shape = value;
+        this.setClasses();
     }
 
     @Input()
@@ -107,37 +114,36 @@ export class ThyButtonIconComponent implements OnInit {
 
     @Input()
     set thyTheme(value: string) {
-        this._theme = value;
-        if (this._initialized) {
-            this._setClasses();
-        }
+        this.theme = value;
+        this.setClasses();
     }
 
-    private _setClasses() {
-        const classes = sizeClassesMap[this._size] ? [...sizeClassesMap[this._size]] : [];
-        if (this._shape && shapeClassesMap[this._shape]) {
-            shapeClassesMap[this._shape].forEach((className: string) => {
+    private setClasses(first = false) {
+        // 设置样式判断是否已经初始化，未初始化直接返回，除非是初次调用
+        // 只有 ngOnInit 调用会传入 first = true
+        if (!first && !this.initialized) {
+            return;
+        }
+        const classes = sizeClassesMap[this.size] ? [...sizeClassesMap[this.size]] : [];
+        if (this.shape && shapeClassesMap[this.shape]) {
+            shapeClassesMap[this.shape].forEach((className: string) => {
                 classes.push(className);
             });
         }
-        if (this._theme && themeClassesMap[this._theme]) {
-            themeClassesMap[this._theme].forEach((className: string) => {
+        if (this.theme && themeClassesMap[this.theme]) {
+            themeClassesMap[this.theme].forEach((className: string) => {
                 classes.push(className);
             });
         }
         this.updateHostClassService.updateClass(classes);
     }
 
-    constructor(
-        private elementRef: ElementRef,
-        private renderer: Renderer2,
-        private updateHostClassService: UpdateHostClassService
-    ) {
+    constructor(elementRef: ElementRef, private updateHostClassService: UpdateHostClassService) {
         this.updateHostClassService.initializeElement(elementRef.nativeElement);
     }
 
     ngOnInit() {
-        this._setClasses();
-        this._initialized = true;
+        this.setClasses(true);
+        this.initialized = true;
     }
 }
