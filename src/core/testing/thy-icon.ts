bd8487266d5c6d3c8cf2e9007877d244e8e4e6ed
luxiaobei <luxiaobei@worktile.com>
2019-07-24 17:43:36 +0800

    test(alert): fix alert tests #238645

--- src/core/testing/thy-icon.ts
+++ src/core/testing/thy-icon.ts
@@ -10,37 +10,21 @@ export const bypassSanitizeProvider: Provider = {
     }
 };
 
-export const defaultInlineIconSet =
-    `
+export const defaultInlineIconSet = `
   <svg>
     <defs>
-        <svg id="inbox">
-        <path d="M4 10v7h3v-7H4zm6 0v7h3v-7h-3zM2 22h19v-3H2v3zm14-12v7h3v-` +
-    `7h-3zm-4.5-9L2 6v2h19V6l-9.5-5z"/>
-        </svg>
-        <svg id="close">
-        <path d="M21 18v1c0 1.1-.9 2-2 2H5c-1.11 0-2-.9-2-2V5c0-1.1.89-2 2-` +
-    `2h14c1.1 0 2 .9 2 2v1h-9c-1.11 0-2 .9-2 2v8c0 1.1.89 2 2 2h9zm-9` +
-    `-2h10V8H12v8zm4-2.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"
-        />
-        </svg>
-        <svg id="calendar-check">
-        <path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H` +
-    `5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-` +
-    `3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z"/>
-        </svg>
-        <svg id="angle-down">
-           <path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H/>
-        </svg>
-        <svg id="check">
-           <path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H/>
-        </svg>
-        <svg id="application-fill">
-           <path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H/>
-        </svg>
-        <svg id="angle-right">
-           <path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H/>
-        </svg>
+        <svg id="inbox"></svg>
+        <svg id="close"></svg>
+        <svg id="calendar-check"></svg>
+        <svg id="angle-down"></svg>
+        <svg id="check"></svg>
+        <svg id="application-fill"></svg>
+        <svg id="angle-right"></svg>
+        <svg id="minus-circle-fill"></svg>
+        <svg id="check-circle-fill"></svg>
+        <svg id="waring-fill"></svg>
+        <svg id="close-circle-fill"></svg>
+       <svg id="question-circle-fill"></svg>
     </defs>
   </svg>
 `;
