1391b9fda1e02e621aab52d38633969a5d574e8e
pubuzhixing8 <pubuzhixing@gmail.com>
2019-07-26 14:38:53 +0800

    refactor(flexible-text): refactor content oberve;add test case(#252170)

--- src/flexible-text/flexible-text.module.ts
+++ src/flexible-text/flexible-text.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThyFlexibleTextComponent } from './flexible-text.component';
 import { ThyTooltipModule } from '../tooltip';
+import { ObserversModule } from '@angular/cdk/observers';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [ThyFlexibleTextComponent],
-    imports: [CommonModule, ThyTooltipModule],
+    imports: [CommonModule, ThyTooltipModule, ObserversModule],
     exports: [ThyFlexibleTextComponent]
 })
 export class ThyFlexibleTextModule {}
--- src/flexible-text/test/flexible-text.spec.ts
+++ src/flexible-text/test/flexible-text.spec.ts
@@ -1,73 +1,128 @@
-// import { async, ComponentFixture, TestBed, flush, fakeAsync, tick } from '@angular/core/testing';
-// import { ThyFlexibleTextModule } from '../flexible-text.module';
-// import { ThyFlexibleTextComponent } from '../flexible-text.component';
-// import { Component, ViewChild } from '@angular/core';
-// import { ThyTooltipModule } from '../../tooltip';
+import { async, ComponentFixture, TestBed, flush, fakeAsync, tick } from '@angular/core/testing';
+import { ThyFlexibleTextModule } from '../flexible-text.module';
+import { ThyFlexibleTextComponent } from '../flexible-text.component';
+import { Component, ViewChild } from '@angular/core';
+import { ThyTooltipModule } from '../../tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MutationObserverFactory } from '@angular/cdk/observers';
 
-// @Component({
-//     selector: 'thy-demo-flexible-text',
-//     template: `
-//         <thy-flexible-text
-//             class="flexible-text-section"
-//             #FlexibleText
-//             [thyContent]="item.title"
-//             [thyPlacement]="item.placement"
-//         >
-//             {{ item.title }}
-//         </thy-flexible-text>
-//     `,
-//     styles: [
-//         `
-//             .flexible-text-section {
-//                 margin-bottom: 20px;
-//                 width: 100px;
-//                 display: block;
-//             }
-//             .flexible-text-container {
-//                 width: 100%;
-//                 overflow: hidden;
-//                 text-overflow: ellipsis;
-//                 white-space: nowrap;
-//             }
-//         `
-//     ]
-// })
-// class FlexibleTextTestComponent {
-//     @ViewChild('FlexibleText') flexibleText: ThyFlexibleTextComponent;
-//     item = {
-//         title: `ddddd`,
-//         value: 0,
-//         placement: 'bottom'
-//     };
-// }
+@Component({
+    selector: 'thy-demo-flexible-text',
+    template: `
+        <thy-flexible-text
+            class="flexible-text-section"
+            #FlexibleText
+            [thyTooltipContent]="tooltipContent"
+            [thyTooltipPlacement]="placement"
+            [thyTooltipTrigger]="trigger"
+        >
+            {{ content }}
+        </thy-flexible-text>
+    `,
+    styles: [
+        `
+            .flexible-text-section {
+                margin-bottom: 20px;
+                width: 100px;
+                display: block;
+            }
+            .flexible-text-container {
+                width: 100%;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+            }
+        `
+    ]
+})
+class FlexibleTextTestComponent {
+    @ViewChild('FlexibleText') flexibleText: ThyFlexibleTextComponent;
+    tooltipContent = '默认内容。。。';
+    placement = 'bottom';
+    content = '默认内容。。。';
+    trigger = 'click';
+}
 
-// describe('FlexibleTextComponent', () => {
-//     let componentInstance: FlexibleTextTestComponent;
-//     let fixture: ComponentFixture<FlexibleTextTestComponent>;
+describe('FlexibleTextComponent', () => {
+    let componentInstance: FlexibleTextTestComponent;
+    let fixture: ComponentFixture<FlexibleTextTestComponent>;
 
-//     beforeEach(async(() => {
-//         TestBed.configureTestingModule({
-//             imports: [ThyTooltipModule, ThyFlexibleTextModule],
-//             declarations: [FlexibleTextTestComponent]
-//         }).compileComponents();
-//         fixture = TestBed.createComponent(FlexibleTextTestComponent);
-//         componentInstance = fixture.componentInstance;
-//         fixture.detectChanges();
-//     }));
+    let callbacks: Function[];
+    const invokeCallbacks = (args?: any) => callbacks.forEach(callback => callback(args));
 
-//     it('should hide the tooltip', () => {
-//         const component = componentInstance.flexibleText;
-//         expect(component.isOverflow).toBe(false);
-//     });
+    beforeEach(async(() => {
+        callbacks = [];
 
-//     it('should show the tooltip', fakeAsync(() => {
-//         const component = componentInstance.flexibleText;
-//         componentInstance.item.title = `周杰伦练琴辛酸史家长进游戏群控诉韩国一桑拿房起火伊斯兰堡会谈推迟游客夫妻美国被捕黄晓明否认拒演京东回应收集隐私救护
-//         车高速被堵沈祥福回应炮轰烟台回应广告牌美国奥罗周杰伦练琴辛酸史家长进游戏群控诉韩国一桑拿房起火伊斯兰堡会谈推迟游客夫妻美国被捕黄晓明否认拒演京东回应收集隐私救护
-//         车高速被堵沈祥福回应炮轰烟台回应广告牌美国奥罗`;
-//         fixture.detectChanges();
-//         tick(10000);
-//         fixture.detectChanges();
-//         expect(component.isOverflow).toBe(true);
-//     }));
-// });
+        TestBed.configureTestingModule({
+            imports: [ThyTooltipModule, ThyFlexibleTextModule, BrowserAnimationsModule],
+            declarations: [FlexibleTextTestComponent],
+            providers: [
+                {
+                    provide: MutationObserverFactory,
+                    useValue: {
+                        create: function(callback: Function) {
+                            callbacks.push(callback);
+
+                            return {
+                                observe: () => {},
+                                disconnect: () => {}
+                            };
+                        }
+                    }
+                }
+            ]
+        }).compileComponents();
+        fixture = TestBed.createComponent(FlexibleTextTestComponent);
+        componentInstance = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
+
+    it('should not overflow when content is less', () => {
+        const component = componentInstance.flexibleText;
+        expect(component.isOverflow).toBe(false);
+    });
+
+    it('should overflow when content is more', fakeAsync(() => {
+        const component = componentInstance.flexibleText;
+        const content = `周杰伦练琴辛酸史家长进游戏群控诉韩国一桑拿房起火伊斯兰堡会谈推迟游客夫妻美国被捕黄晓明否认拒演京东回应收集隐私救护
+        车高速被堵沈祥福回应炮轰烟台回应广告牌美国奥罗周杰伦练琴辛酸史家长进游戏群控诉韩国一桑拿房起火伊斯兰堡会谈推迟游客夫妻美国被捕黄晓明否认拒演京东回应收集隐私救护
+        车高速被堵沈祥福回应炮轰烟台回应广告牌美国奥罗`;
+        componentInstance.content = content;
+        invokeCallbacks();
+        fixture.detectChanges();
+        tick(100);
+        expect(component.isOverflow).toBe(true);
+    }));
+
+    it('should change content of thyTooltipDirective when set thyTooltipContent of thy-flexible-text', fakeAsync(() => {
+        const component = componentInstance.flexibleText;
+        expect(component.tooltipService.thyTooltipDirective.content as string).toBe(componentInstance.tooltipContent);
+        const newContent = `新内容。。。`;
+        componentInstance.tooltipContent = newContent;
+        fixture.detectChanges();
+        expect(component.tooltipService.thyTooltipDirective.content as string).toBe(newContent);
+    }));
+
+    it('should change placement of thyTooltipDirective when set placement of thy-flexible-text', fakeAsync(() => {
+        const component = componentInstance.flexibleText;
+        expect(component.tooltipService.thyTooltipDirective.placement).toBe(componentInstance.placement);
+        const newPlacement = `left`;
+        componentInstance.placement = newPlacement;
+        fixture.detectChanges();
+        expect(component.tooltipService.thyTooltipDirective.placement).toBe(newPlacement);
+    }));
+
+    it('should change placement of thyTooltipDirective when set placement of thy-flexible-text', fakeAsync(() => {
+        const component = componentInstance.flexibleText;
+        expect(component.tooltipService.thyTooltipDirective.placement).toBe(componentInstance.placement);
+        const newPlacement = `left`;
+        componentInstance.placement = newPlacement;
+        fixture.detectChanges();
+        expect(component.tooltipService.thyTooltipDirective.placement).toBe(newPlacement);
+    }));
+
+    it('should apply trigger="click"', fakeAsync(() => {
+        const component = componentInstance.flexibleText;
+        expect(component.tooltipService.thyTooltipDirective.trigger).toBe('click');
+    }));
+});
