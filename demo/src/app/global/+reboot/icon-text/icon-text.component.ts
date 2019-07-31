61553160717019d45de3bda53da48d0edb0e76ba
why520crazy <why520crazy@163.com>
2019-07-31 10:59:31 +0800

    docs: refactor global demo contains link, variables, reboot

---
+++ demo/src/app/global/+reboot/icon-text/icon-text.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-demo-reboot-icon-text',
+    templateUrl: './icon-text.component.html'
+})
+export class DemoRebootIconTextComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
---
+++ demo/src/app/global/index.ts
@@ -0,0 +1,5 @@
+import { DemoVariablesModule } from './+variables';
+import { DemoLinkModule } from './+link';
+import { DemoRebootModule } from './+reboot';
+
+export const GLOBAL_MODULES = [DemoVariablesModule, DemoLinkModule, DemoRebootModule];
