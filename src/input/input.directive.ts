3cdb388ab76299ae1650a3d8a2e5939ee7ee299b
zhenshuaiwws <231640921@qq.com>
2019-11-05 11:29:18 +0800

    feat(input): input default autocomplete off (#INF-133,#AE-140)

--- src/input/input.directive.ts
+++ src/input/input.directive.ts
@@ -1,23 +1,26 @@
-import { Directive, HostBinding, Input, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Input, OnInit, Renderer2 } from '@angular/core';
 import { UpdateHostClassService } from '../shared';
+import { inputValueToBoolean } from '../util/helpers';
 
 export type InputSize = 'xs' | 'sm' | 'md' | 'lg' | '';
 
 const inputGroupSizeMap = {
-    'xs': ['form-control-xs'],
-    'sm': ['form-control-sm'],
-    'md': ['form-control-md'],
-    'lg': ['form-control-lg']
+    xs: ['form-control-xs'],
+    sm: ['form-control-sm'],
+    md: ['form-control-md'],
+    lg: ['form-control-lg']
 };
+
 @Directive({
     selector: '[thyInput]',
-    providers: [
-        UpdateHostClassService
-    ],
+    providers: [UpdateHostClassService]
 })
-export class ThyInputDirective {
-
+export class ThyInputDirective implements OnInit {
     @HostBinding('class.form-control') _isFormControl = true;
+
+    private autocomplete: boolean;
+
+    private isInitialized = false;
 
     @Input()
     set thySize(size: InputSize) {
@@ -28,10 +31,28 @@ export class ThyInputDirective {
         }
     }
 
+    @Input()
+    set thyAutocomplete(value: boolean) {
+        this.autocomplete = inputValueToBoolean(value);
+        if (this.isInitialized) {
+            this.setAutocomplete();
+        }
+    }
+
     constructor(
         private updateHostClassService: UpdateHostClassService,
-        private elementRef: ElementRef
+        private elementRef: ElementRef,
+        private render: Renderer2
     ) {
         this.updateHostClassService.initializeElement(elementRef.nativeElement);
+    }
+
+    ngOnInit() {
+        this.isInitialized = true;
+        this.setAutocomplete();
+    }
+
+    private setAutocomplete() {
+        this.render.setAttribute(this.elementRef.nativeElement, 'autocomplete', this.autocomplete ? 'on' : 'off');
     }
 }
