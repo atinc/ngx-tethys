2b4ba88b06794392238f645e13471b5c1111b695
luxiaobei <luxiaobei@worktile.com>
2019-11-13 11:22:32 +0800

    feat(alert): add strong type of danger & secondary

--- demo/src/app/components/+alert/module.ts
+++ demo/src/app/components/+alert/module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { DemoAlertSectionComponent } from './alert-section.component';
 import { DemoAlertBasicComponent } from './basic/basic.component';
 import { DemoAlertIconComponent } from './icon/icon.component';
-import { DemoAlertBannerComponent } from './banner/banner.component';
+import { DemoAlertStrongComponent } from './strong/strong.component';
+import { DemoAlertWeakComponent } from './weak/weak.component';
 import { SharedModule } from 'app/shared.module';
 
 const components = [
     DemoAlertSectionComponent,
     DemoAlertBasicComponent,
     DemoAlertIconComponent,
-    DemoAlertBannerComponent
+    DemoAlertStrongComponent,
+    DemoAlertWeakComponent
 ];
 
 @NgModule({
