811c2b2d2089a4027a27c58d504255ac69f0ee38
zhangwen <1062680993@qq.com>
2019-12-16 15:41:31 +0800

    feat(result): add result component (#451)
    * feat(result): add result component
    
    * feat(result): add result component
    
    * fix(action-menu): change action-menu-divider-margin-y
    
    * fix(result): change parmas name and add icon template
    
    * fix(result): change element spacing and $primary in demo

---
+++ demo/src/app/components/+result/basic/basic.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-demo-result-basic',
+    templateUrl: './basic.component.html'
+})
+export class DemoResultBasicComponent implements OnInit {
+    constructor() {}
+    ngOnInit() {}
+}
