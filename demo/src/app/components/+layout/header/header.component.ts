15f35431e48dc5a42cdfdf56a3c7ff87ba47d340
why520crazy <why520crazy@163.com>
2020-03-09 17:48:28 +0800

    fix(nav): set border width avoid to bounce when hover or active #INF-478
    fix(nav): update comment for set border width #INF-478
    
    fix(nav): fix nav-link theme #INF-478
    
    fix(nav): vertical set border width as 0px

---
+++ demo/src/app/components/+layout/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-demo-layout-header',
+    templateUrl: './header.component.html'
+})
+export class DemoLayoutHeaderComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
