3cd6c0b3d9633526861089860d586ce6306ccb9e
msyeal <1207752795@qq.com>
2020-03-24 15:14:07 +0800

    feat: #INF-526 add badge test and use docs

--- demo/src/app/components/+badge/badge-section.component.ts
+++ demo/src/app/components/+badge/badge-section.component.ts
@@ -5,9 +5,7 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './badge-section.component.html',
     styleUrls: ['./badge-section.component.scss']
 })
-
 export class DemoBadgeSectionComponent implements OnInit {
-
     badgeDotTheme;
 
     themes = ['danger', 'primary', 'warning', 'secondary'];
@@ -16,9 +14,73 @@ export class DemoBadgeSectionComponent implements OnInit {
 
     badgeCount = 0;
 
-    constructor() {
+    isdot = true;
 
-    }
+    public apiParameters = [
+        {
+            property: 'thyType',
+            description: `badge类型, 类型为 'primary' | 'danger' | 'warning' | 'secondary'`,
+            type: 'string',
+            default: 'danger'
+        },
+        {
+            property: 'thyCount',
+            description: '徽标内容为数字时',
+            type: 'number',
+            default: ''
+        },
+        {
+            property: 'thyContext',
+            description: '徽标内容是字符串时',
+            type: 'string',
+            default: 'false'
+        },
+        {
+            property: 'thyMaxCount',
+            description:
+                '徽标显示的最大值, 与thyCount一起使用,thyCount超过了thyMaxCount设置的值时,徽标内容为thyMaxCount+',
+            type: 'number',
+            default: ''
+        },
+        {
+            property: 'thySize',
+            description: '徽标显示的大小',
+            type: `lg | sm`,
+            default: `font-size: 0.75rem, padding: 2px 0.3rem`
+        },
+        {
+            property: 'thyIsDot',
+            description: '徽标是个状态点且实心时',
+            type: 'boolean',
+            default: 'false'
+        },
+        {
+            property: 'thyIsHollow',
+            description: '徽标是个状态点且空心时',
+            type: 'Boolean',
+            default: 'false'
+        },
+        {
+            property: 'thyKeepShow',
+            description: 'thyCount为0时,强制显示',
+            type: 'boolean',
+            default: ''
+        },
+        {
+            property: 'thyTextColor',
+            description: '自定义设置徽标字体的颜色',
+            type: 'string',
+            default: ''
+        },
+        {
+            property: 'thyBackgroundColor',
+            description: '自定义设置徽标的背景颜色',
+            type: 'string',
+            default: ''
+        }
+    ];
+
+    constructor() {}
 
     ngOnInit() {
         setInterval(() => {
