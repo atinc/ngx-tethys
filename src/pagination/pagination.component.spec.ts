86a1ff722225e96a417fc5c466300ac4304b3111
mandy1219 <729147826@qq.com>
2020-04-13 16:39:55 +0800

    feat: thy-pagination add total and test

--- src/pagination/pagination.component.spec.ts
+++ src/pagination/pagination.component.spec.ts
@@ -1,24 +1,86 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit, NgModule, DebugElement, TemplateRef, ViewChild } from '@angular/core';
+import { ThyPaginationComponent } from './pagination.component';
+import { ThyPaginationModule } from './pagination.module';
+import { By } from '@angular/platform-browser';
 
-// import { ThyPaginationComponent } from './pagination.component';
+@Component({
+    selector: 'thy-test-pagination-basic',
+    template: `
+        <thy-pagination
+            [thyPageIndex]="pagination.index"
+            [thyTotal]="pagination.total"
+            [thyPageSize]="pagination.size"
+            [thyShowTotal]="showTotal"
+            (thyPageChanged)="onPageChange($event)"
+            (thyPageIndexChange)="onPageIndexChange($event)"
+        ></thy-pagination>
+        <ng-template #total let-total>共计{{ total }}条</ng-template>
+    `
+})
+class ThyPaginationBasicComponent implements OnInit {
+    pagination = {
+        index: 1,
+        size: 10,
+        total: 50
+    };
 
-// describe('ThyPaginationComponent', () => {
-//     let component: ThyPaginationComponent;
-//     let fixture: ComponentFixture<ThyPaginationComponent>;
+    showTotal: boolean | TemplateRef<{ $implicit: number; ranges: [number, number] }> = false;
+    @ViewChild('total') totalTemplate: TemplateRef<any>;
 
-//     beforeEach(async(() => {
-//         TestBed.configureTestingModule({
-//             declarations: [ThyPaginationComponent]
-//         }).compileComponents();
-//     }));
+    constructor() {}
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(ThyPaginationComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+    ngOnInit(): void {}
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+    onPageChange() {
+        return 'onPageChange is ok';
+    }
+
+    onPageIndexChange() {
+        return 'onPageChange is ok';
+    }
+}
+
+@NgModule({
+    imports: [ThyPaginationModule],
+    declarations: [ThyPaginationBasicComponent],
+    exports: [ThyPaginationBasicComponent]
+})
+export class ThyPaginationTestModule {}
+
+describe('ThyPagination', () => {
+    let basicTestComponent: ThyPaginationBasicComponent;
+    let fixture: ComponentFixture<ThyPaginationBasicComponent>;
+    let pageComponent: DebugElement;
+
+    beforeEach(fakeAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [ThyPaginationModule, ThyPaginationTestModule]
+        });
+        TestBed.compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ThyPaginationBasicComponent);
+        basicTestComponent = fixture.debugElement.componentInstance;
+        pageComponent = fixture.debugElement.query(By.directive(ThyPaginationComponent));
+    });
+
+    it('should create', () => {
+        expect(pageComponent).toBeTruthy();
+    });
+
+    it('should have total when showTotal is true', () => {
+        basicTestComponent.showTotal = true;
+        fixture.detectChanges();
+        const paginationLeft = pageComponent.nativeElement.querySelector('div.thy-pagination-total');
+        expect(paginationLeft).toBeTruthy();
+    });
+
+    it('should have total when showTotal is template', () => {
+        basicTestComponent.showTotal = basicTestComponent.totalTemplate;
+        fixture.detectChanges();
+        const paginationLeft = pageComponent.nativeElement.querySelector('div.thy-pagination-total');
+        expect(paginationLeft).toBeTruthy();
+    });
+});
