a45a576a77d87c7b4ff7f18f8a478e04ce51f0f8
mengshuicmq <mengshuicmq@163.com>
2020-04-23 14:19:42 +0800

    fix(avatar): add demo when avatar name from wechat

--- demo/src/app/components/+avatar/custom-avatar.service.ts
+++ demo/src/app/components/+avatar/custom-avatar.service.ts
@@ -1,21 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ThyAvatarService } from '../../../../../src/public-api';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable()
 export class CustomAvatarService extends ThyAvatarService {
-    constructor(private domSanitizer: DomSanitizer) {
-        super();
-    }
-
     avatarSrcTransform(src: string, size: number): string {
         return `https://s3.cn-north-1.amazonaws.com.cn/lcavatar/${src}`;
     }
 
     avatarNameTransform(name: string) {
-        // 来自企业微信团队的成员
-        return this.domSanitizer.bypassSecurityTrustHtml(
-            `<ww-open-data type="userName" openid="${name}"></ww-open-data>`
-        );
+        return name;
     }
 }
