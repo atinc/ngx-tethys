337464b4263f38528d5cac7d8de5b3a1f908d717
mengshuicmq <mengshuicmq@163.com>
2020-04-23 18:45:19 +0800

    fix: fix property description

--- demo/src/app/components/+avatar/avatar-section.component.ts
+++ demo/src/app/components/+avatar/avatar-section.component.ts
@@ -111,7 +111,7 @@ export class DemoAvatarSectionComponent implements OnInit {
         {
             property: 'thyName',
             description:
-                '人员名称（来自企业微信的人员名称，需要通过自定义服务 ThyAvatarService，重写 avatarNameTransform 方法去实现转换）',
+                '人员名称（可设置自定义名称，需通过自定义服务 ThyAvatarService，重写 avatarNameTransform 方法去实现转换）',
             type: 'string',
             default: ''
         },
