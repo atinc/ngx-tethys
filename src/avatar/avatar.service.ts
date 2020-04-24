58ca7fbb7e6192034903c93f33f708426ea5b02d
cmq <mengshuicmq@163.com>
2020-04-24 09:41:52 +0800

    feat(avatar): avatar组件增加 avatarNameTransform (#INFR-350)

--- src/avatar/avatar.service.ts
+++ src/avatar/avatar.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
 
 export abstract class ThyAvatarService {
-
     ignoreAvatarSrcPaths = [`default.png`];
 
     abstract avatarSrcTransform(src: string, size: number): string;
-}
 
+    abstract nameTransform(name: string): string | SafeHtml;
+}
 
 @Injectable()
 export class ThyDefaultAvatarService extends ThyAvatarService {
     avatarSrcTransform(src: string, size: number): string {
         return src;
+    }
+
+    nameTransform(name: string): string | SafeHtml {
+        return name;
     }
 }
