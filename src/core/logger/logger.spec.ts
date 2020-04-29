823f156e28c5d13dcb85dfec1a5674492d55579f
why520crazy <why520crazy@163.com>
2020-04-29 09:38:59 +0800

    feat(core): add createWarnDeprecation for logger #INFR-399

--- src/core/logger/logger.spec.ts
+++ src/core/logger/logger.spec.ts
@@ -1,4 +1,4 @@
-import { PREFIX, log, warn, warnDeprecation } from './logger';
+import { PREFIX, log, warn, warnDeprecation, createWarnDeprecation } from './logger';
 
 describe('#logger', () => {
     beforeEach(function() {
@@ -45,9 +45,11 @@ describe('#logger', () => {
     describe('#warnDeprecation', () => {
         it(`should log warnDeprecation to the console and print a special symbol`, () => {
             expect(console.warn).toHaveBeenCalledTimes(0);
-            warnDeprecation('Spec warnDeprecation has no expectations.');
+            const message = 'Spec warnDeprecation has no expectations.';
+            warnDeprecation(message);
             expect(console.warn).toHaveBeenCalledTimes(1);
             expect(console.warn).toHaveBeenCalled();
+            expect(console.warn).toHaveBeenCalledWith('[NGX-TETHYS]:', 'deprecated:', `${message}`, jasmine.anything());
         });
 
         it(`should not log warnDeprecation to the console again when the content is the same`, () => {
@@ -57,6 +59,29 @@ describe('#logger', () => {
             expect(console.warn).toHaveBeenCalled();
 
             warnDeprecation('Spec diff warnDeprecation has no expectations.');
+            expect(console.warn).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('#createWarnDeprecation', () => {
+        it(`should log warnDeprecation when createWarnDeprecation my-test`, () => {
+            const myWarnDeprecation = createWarnDeprecation('[my-test]:');
+            expect(console.warn).toHaveBeenCalledTimes(0);
+            const message = 'Spec createWarnDeprecation my-test has no expectations.';
+            myWarnDeprecation(message);
+            expect(console.warn).toHaveBeenCalledTimes(1);
+            expect(console.warn).toHaveBeenCalledWith('[my-test]:', 'deprecated:', `${message}`, jasmine.anything());
+        });
+
+        it(`should not log warnDeprecation to the console again when the content is the same`, () => {
+            const myWarnDeprecation = createWarnDeprecation('[my-test]:');
+            expect(console.warn).toHaveBeenCalledTimes(0);
+            const message = 'Spec createWarnDeprecation my-test has no expectations again.';
+            myWarnDeprecation(message);
+            expect(console.warn).toHaveBeenCalledTimes(1);
+            expect(console.warn).toHaveBeenCalledWith('[my-test]:', 'deprecated:', `${message}`, jasmine.anything());
+
+            myWarnDeprecation(message);
             expect(console.warn).toHaveBeenCalledTimes(1);
         });
     });
