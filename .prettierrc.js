14ee755f3a9d85922a21121b194234a594a46540
why520crazy <why520crazy@163.com>
2020-06-09 20:31:11 +0800

    docs: add paths ngx-tethys for tsconfig.spec.json and format ts config json

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,14 @@
 const { prettier } = require('@worktile-devkit/config');
 
 module.exports = {
-  ...prettier.frontendConfig,
-  overrides: [
-    ...prettier.frontendConfig.overrides,
-    {
-      files: '*.scss',
-      options: {
-        tabWidth: 4
-      }
-    }
-  ]
+    ...prettier.frontendConfig,
+    overrides: [
+        ...prettier.frontendConfig.overrides,
+        {
+            files: '*.scss',
+            options: {
+                tabWidth: 4
+            }
+        }
+    ]
 };
