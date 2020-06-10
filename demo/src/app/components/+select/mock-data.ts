eab88ef43e75ba56e472dd028601c29d7444902c
pubuzhixing8 <pubuzhixing@gmail.com>
2020-06-10 11:31:55 +0800

    chore: refactor custom-select and form demo

--- demo/src/app/components/+select/mock-data.ts
+++ demo/src/app/components/+select/mock-data.ts
@@ -1,47 +1,22 @@
-export const taskTypes = [{
-    icon: 'wtf wtf-type-task',
-    display_name: '任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务',
-    name: 'task',
-    pin_yin: 'r,re,ren,renw,renwu',
-    _id: '5b0527cfc8f2ff200a33d4aa'
-}, {
-    icon: 'wtf wtf-type-money',
-    display_name: '钱',
-    name: 'money',
-    pin_yin: 'qian',
-    _id: '5b0527cfc8f2ff200a33d4ab'
-}, {
-    icon: 'wtf wtf-type-worksheet',
-    display_name: '工时',
-    name: 'worksheet',
-    pin_yin: 'gongshi',
-    _id: '5b0527cfc8f2ff200a33d4ac'
-}, {
-    icon: 'wtf wtf-type-demand',
-    display_name: '需求',
-    name: 'requirement',
-    _id: '5b0527cfc8f2ff200a33d4ad'
-}, {
-    icon: 'wtf wtf-type-ios',
-    display_name: 'IOS缺陷',
-    name: 'ios',
-    _id: '5b0527cfc8f2ff200a33d4b1'
-},
-{
-    icon: 'wtf wtf-type-ios',
-    display_name: 'IOS缺陷',
-    name: 'ios1',
-    _id: '5b0527cfc8f2ff200a33d4b2'
-},
-{
-    icon: 'wtf wtf-type-ios',
-    display_name: 'IOS缺陷',
-    name: 'ios2',
-    _id: '5b0527cfc8f2ff200a33d4b3'
-},
-{
-    icon: 'wtf wtf-type-ios',
-    display_name: 'IOS缺陷',
-    name: 'ios3',
-    _id: '5b0527cfc8f2ff200a33d4b4'
-}];
+export const taskTypes = [
+    {
+        icon: 'task-square-fill',
+        display_name:
+            '任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务任务',
+        name: 'task',
+        pin_yin: 'r,re,ren,renw,renwu',
+        _id: '5b0527cfc8f2ff200a33d4aa'
+    },
+    {
+        icon: 'user-story-square-fill',
+        display_name: '用户故事',
+        name: 'requirement',
+        _id: '5b0527cfc8f2ff200a33d4ad'
+    },
+    {
+        icon: 'bug-square-fill',
+        display_name: 'IOS缺陷',
+        name: 'ios',
+        _id: '5b0527cfc8f2ff200a33d4b1'
+    }
+];
