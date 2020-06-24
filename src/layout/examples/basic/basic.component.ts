cdb3b70a29f6de544194948cc5bde1d5c248b735
why520crazy <why520crazy@163.com>
2020-06-24 12:04:58 +0800

    docs(layout): add next docs for layout #INFR-703

---
+++ src/layout/examples/basic/basic.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-layout-basic-example',
+    templateUrl: './basic.component.html'
+})
+export class ThyLayoutBasicExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
