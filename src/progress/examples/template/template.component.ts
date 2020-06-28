028a1d9c76c5f7fff0115ab525d2082dfe560470
aoilti <1051127659@qq.com>
2020-06-28 14:07:06 +0800

    feat(progress): add progress example#INFR-725

---
+++ src/progress/examples/template/template.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-progress-template',
+    templateUrl: './template.component.html'
+})
+export class ThyProgressTemplateExampleComponent implements OnInit {
+    stacked = [
+        {
+            value: 62,
+            color: '#fa5a55'
+        },
+        {
+            value: 35,
+            color: '#39ba5d'
+        },
+        {
+            value: 92,
+            color: '#4e8af9'
+        },
+        {
+            value: 72,
+            color: '#2cccda'
+        },
+        {
+            value: 66,
+            color: '#66c060'
+        }
+    ];
+
+    constructor() {}
+
+    ngOnInit() {}
+}
