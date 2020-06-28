abf51e06d502bf1f96257e999fcf331a2ddd2a67
ls32140 <13125799712@163.com>
2020-06-28 18:07:20 +0800

    docs(vote): add next docs for thy-vote #INFR-726

--- src/vote/examples/module.ts
+++ src/vote/examples/module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgxTethysModule } from 'ngx-tethys';
+import { ThyVoteBasicExampleComponent } from './basic/basic.component';
+import { ThyVoteIconExampleComponent } from './icon/icon.component';
+import { ThyVoteWeakExampleComponent } from './weak/weak.component';
 
 const COMPONENTS = [ThyVoteBasicExampleComponent, ThyVoteIconExampleComponent, ThyVoteWeakExampleComponent];
 
