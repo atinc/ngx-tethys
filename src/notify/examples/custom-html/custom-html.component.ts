998f671cfd8504fe4857107764e7fb0d97354c8a
yeal <39734122+msyeal@users.noreply.github.com>
2020-07-13 13:41:43 +0800

    feat: #INFR-729 add notify doc (#788)

---
+++ src/notify/examples/custom-html/custom-html.component.ts
@@ -0,0 +1,23 @@
+import { ThyNotifyService } from 'ngx-tethys';
+
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+    selector: 'app-notify-custom-example',
+    templateUrl: './custom-html.component.html'
+})
+export class ThyNotifyCustomHtmlExampleComponent implements OnInit {
+    @ViewChild('notify', { static: true }) notify: ElementRef;
+
+    constructor(private notifyService: ThyNotifyService) {}
+
+    ngOnInit() {}
+
+    showWithHtml() {
+        this.notifyService.show({
+            type: 'error',
+            title: '错误 ',
+            html: this.notify
+        });
+    }
+}
