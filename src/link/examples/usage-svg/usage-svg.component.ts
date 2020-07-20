9359cfe86dcb9de495779c73ea43d8cd5dbf8945
zhenshuaiwws <231640921@qq.com>
2020-07-20 16:53:47 +0800

    docs: link

---
+++ src/link/examples/usage-svg/usage-svg.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-link-usage-svg-example',
+    templateUrl: './usage-svg.component.html'
+})
+export class ThyLinkUsageSvgExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
---
+++ src/link/examples/usage-wtf/usage-wtf.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-link-usage-wtf-example',
+    templateUrl: './usage-wtf.component.html'
+})
+export class ThyLinkUsageWtfExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
