b27e7991dbad7d6c968d3297e40c936c88cb83b2
zhangwen <1062680993@qq.com>
2020-07-28 19:01:06 +0800

    fix(grid): add grid examples #INFR-740

--- src/grid/examples/module.ts
+++ src/grid/examples/module.ts
@@ -1,18 +1,16 @@
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { ThyGridModule } from 'ngx-tethys/grid';
-import { ThySelectModule } from 'ngx-tethys/select';
+import { NgxTethysModule } from 'ngx-tethys';
 import { CommonModule } from '@angular/common';
 import { ThyGridBasicExampleComponent } from './basic/basic.component';
-import { ThyGridBorderedExampleComponent } from './bordered/bordered.component';
 import { ThyGridGroupExampleComponent } from './group/group.component';
 
-const COMPONENTS = [ThyGridBasicExampleComponent, ThyGridBorderedExampleComponent, ThyGridGroupExampleComponent];
+const COMPONENTS = [ThyGridBasicExampleComponent, ThyGridGroupExampleComponent];
 
 @NgModule({
     declarations: [...COMPONENTS],
     entryComponents: [...COMPONENTS],
-    imports: [CommonModule, ThyGridModule, ThySelectModule, FormsModule],
+    imports: [CommonModule, NgxTethysModule, FormsModule],
     exports: [...COMPONENTS]
 })
 export class ThyGridExamplesModule {}
