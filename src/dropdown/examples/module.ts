bccf0c25acdbf5c59848457ead9b504ecbf61ba2
frankwang <wangsen9108@gmail.com>
2020-07-29 16:57:52 +0800

    fix(dropdown): remove api file and other examples

--- src/dropdown/examples/module.ts
+++ src/dropdown/examples/module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxTethysModule } from 'ngx-tethys';
 import { ThyDropdownBasicExampleComponent } from './basic/basic.component';
-import { ThyDropdownSizeExampleComponent } from './size/size.component';
 import { ThyDropdownSplitExampleComponent } from './split/split.component';
 
-const COMPONENTS = [ThyDropdownBasicExampleComponent, ThyDropdownSizeExampleComponent, ThyDropdownSplitExampleComponent];
+const COMPONENTS = [ThyDropdownBasicExampleComponent, ThyDropdownSplitExampleComponent];
 @NgModule({
     imports: [CommonModule, NgxTethysModule],
     exports: COMPONENTS,
