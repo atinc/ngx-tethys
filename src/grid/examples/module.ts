d8a0ae9aa9e93ce5daf93552c25417600d661e1b
Maple13 <mr_daijc@163.com>
2020-07-29 11:31:07 +0800

    feat(grid): increase tree type data display

--- src/grid/examples/module.ts
+++ src/grid/examples/module.ts
@@ -4,8 +4,9 @@ import { NgxTethysModule } from 'ngx-tethys';
 import { CommonModule } from '@angular/common';
 import { ThyGridBasicExampleComponent } from './basic/basic.component';
 import { ThyGridGroupExampleComponent } from './group/group.component';
+import { ThyGridTreeExampleComponent } from './tree/tree.component';
 
-const COMPONENTS = [ThyGridBasicExampleComponent, ThyGridGroupExampleComponent];
+const COMPONENTS = [ThyGridBasicExampleComponent, ThyGridGroupExampleComponent, ThyGridTreeExampleComponent];
 
 @NgModule({
     declarations: [...COMPONENTS],
