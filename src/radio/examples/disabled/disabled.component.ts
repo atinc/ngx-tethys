3fc567448adb082900fc322340a22b228d4cee22
Frank <wangsen9108@gmail.com>
2020-07-30 17:36:34 +0800

    feat(radio): add radio doc #INFR-650 (#819)
    Co-authored-by: Walker <15701592+walkerkay@users.noreply.github.com>
    Co-authored-by: 甄帅 <231640921@qq.com>

---
+++ src/radio/examples/disabled/disabled.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-radio-disabled-example',
+    templateUrl: './disabled.component.html'
+})
+export class ThyRadioDisabledExampleComponent {}
