5650a40f8b504b0ba51b0e0910142ccb535a5650
zhangwen <1062680993@qq.com>
2020-07-30 12:49:36 +0800

    docs(menu): add next docs for menu #INFR-707

--- src/grid/api/zh-cn.js
+++ src/grid/api/zh-cn.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
         type: 'component',
-        name: 'thyGrid',
+        name: 'thy-grid',
         description: 'Grid 列表',
         properties: [
             {
@@ -148,7 +148,7 @@ module.exports = [
     },
     {
         type: 'component',
-        name: 'ThyGridColumn',
+        name: 'thy-grid-column',
         description: 'Grid Column',
         properties: [
             {
