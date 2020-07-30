06275ef57d1dc43b4d9f643626cd97a179ab3409
lxying <18800168665@163.com>
2020-07-30 10:49:43 +0800

    docs(statistic): add statistc flexible-text example (#814)

---
+++ src/flexible-text/api/zh-cn.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        type: 'component',
+        name: 'FlexibleText 文本提示',
+        description: '两种使用方式：组件方式、指令方式。',
+        properties: [
+            {
+                name: 'thyTooltipContent',
+                description: '需要展示的全部内容',
+                type: 'String | TemplateRef',
+                default: 'String'
+            },
+            {
+                name: 'thyTooltipPlacement',
+                description: 'tooltip 的提示位置，top | bottom | left | right',
+                type: 'String',
+                default: 'top'
+            },
+            {
+                type: 'string',
+                name: 'thyTooltipTrigger',
+                description: '触发提示方式，hover, focus, click',
+                default: 'hover'
+            },
+            {
+                type: 'string',
+                name: 'thyContainerClass',
+                description: '自定义class类，如果不设置默认会包含flexible-text-contianer',
+                default: undefined
+            }
+        ]
+    }
+];
---
+++ src/statistic/examples/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-card-example',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
+})
+export class ThyStatisticCardExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
