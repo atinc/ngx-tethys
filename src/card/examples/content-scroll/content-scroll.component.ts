65dc2ffd3df3f8cabb8c1847c34b82428a6289dd
minlovehua <321512939@qq.com>
2020-07-30 18:37:11 +0800

    feat(card): add card #INFR-741

---
+++ src/card/examples/content-scroll/content-scroll.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-card-content-scroll-example', //app-demo-card-content-scroll
+    templateUrl: './content-scroll.component.html'
+})
+export class ThyCardContentScrollExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
