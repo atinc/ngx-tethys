54cfc1f47af5238bc9b5b809f66741228baad7e2
Maple13 <mr_daijc@163.com>
2020-08-01 11:48:55 +0800

    docs(result): add examples and documentation

---
+++ src/result/examples/error/error.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-result-error-example',
+    templateUrl: './error.component.html'
+})
+export class ThyResultErrorExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
