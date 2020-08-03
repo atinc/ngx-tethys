10a791a2d6c0f5d68aac9c5e584bb9ea38b7297e
Maple13 <mr_daijc@163.com>
2020-08-03 18:26:05 +0800

    docs(markdown): add examples and documentation

---
+++ src/markdown/examples/parser/index.md
@@ -0,0 +1,7 @@
+---
+category: display
+title: thyMardownParser
+subtitle: markdown 解析器
+name: 'thyMardownParser'
+order: 1
+---
---
+++ src/markdown/examples/parser/parser.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-markdown-parser-example',
+    templateUrl: './parser.component.html',
+    styleUrls: ['../style.css']
+})
+export class ThyMarkdownParserExampleComponent implements OnInit {
+    constructor() {}
+
+    public markdownValue = `**加粗** *斜体* \`代码\` \`\`\`格式化\`\`\` [Worktile](http://worktile.com) [http://worktile.com|这是一个 Worktile 链接] [@54704b26c7dd2059dfeb81c6|Terry] <////--><details open ontoggle=confirm(/我是脚本/)> # <span id='t1'>Worktile 异常追踪 Node.js SDK 使用指南</span>`;
+
+    public safeValue = `**加粗** *斜体* \`代码\` \`\`\`格式化\`\`\` [Worktile](http://worktile.com) [http://worktile.com|这是一个 Worktile 链接] [@54704b26c7dd2059dfeb81c6|Terry] # <span id='t1'>Worktile 异常追踪 Node.js SDK 使用指南</span>`;
+
+    ngOnInit() {}
+}
