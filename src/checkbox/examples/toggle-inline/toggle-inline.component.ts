a941eab711b3231d64b4261d90b7a6807445f966
waker744524423 <18689553172@163.com>
2020-08-06 19:35:44 +0800

    docs(checkbox): add checkbox docs

---
+++ src/checkbox/examples/toggle-inline/toggle-inline.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-checkbox-toggle-inline-example',
+    templateUrl: './toggle-inline.component.html'
+})
+export class ThyCheckboxToggleInlineExampleComponent implements OnInit {
+    public inlineStatus = false;
+
+    constructor() {}
+
+    ngOnInit() {}
+}
