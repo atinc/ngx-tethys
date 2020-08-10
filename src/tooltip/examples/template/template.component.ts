3e2e867c189857199be8bdbfd28f9db2c50ec3f4
aoilti <1051127659@qq.com>
2020-08-10 16:09:07 +0800

    docs(tooltip): add tooltip docs #INFR-736

---
+++ src/tooltip/examples/template/template.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-tooltip-template-example',
+    templateUrl: './template.component.html'
+})
+export class ThyTooltipTemplateExampleComponent {
+    tooltipConfig = {
+        trigger: 'hover',
+        placement: 'top',
+        disabled: false,
+        showDelay: 200,
+        hideDelay: 100,
+        offset: 4
+    };
+}
