2f58e04251b68995fca737f25f26481cba6a0ad0
王焕 <2323666315@qq.com>
2020-08-12 14:47:07 +0800

    doc(raster): add raster doc #INFR-910

--- src/raster/module.ts
+++ src/raster/module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ThyRowDirective } from './thy-row.directive';
 import { ThyColDirective } from './thy-col.directive';
+import { ThyDirectiveModule } from 'ngx-tethys/directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    imports: [],
-    exports: [ThyRowDirective, ThyColDirective],
     declarations: [ThyRowDirective, ThyColDirective],
-    providers: []
+    exports: [ThyRowDirective, ThyColDirective],
+    imports: [CommonModule,ThyDirectiveModule]
+
 })
-export class ThyRasterModule {}
+
+export class ThyRasterModule { }
