6bae755b40133dd21f9b6ad4848d12353096b957
王焕 <2323666315@qq.com>
2020-08-15 15:59:53 +0800

    doc(raster): update import module  position #INFR-910

--- src/raster/examples/module.ts
+++ src/raster/examples/module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgxTethysModule } from 'ngx-tethys';
 import { ThyRasterBasicExampleComponent } from './basic/basic.component';
-
+import { ThyDirectiveModule } from 'ngx-tethys/directive';
 import { CommonModule } from '@angular/common';
+
 @NgModule({
-    imports: [CommonModule,NgxTethysModule],
+    imports: [CommonModule,NgxTethysModule,ThyDirectiveModule],
     declarations: [ThyRasterBasicExampleComponent],
     exports: [ThyRasterBasicExampleComponent],
     entryComponents: [ThyRasterBasicExampleComponent]
