b0b4a8dfb8b305c8fb92f2fa3bb9a44f8a6356a0
王焕 <2323666315@qq.com>
2020-08-15 16:10:20 +0800

    doc(raster): update import position

--- src/raster/module.ts
+++ src/raster/module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ThyRowDirective } from './thy-row.directive';
 import { ThyColDirective } from './thy-col.directive';
-import { ThyDirectiveModule } from 'ngx-tethys/directive';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [ThyRowDirective, ThyColDirective],
     exports: [ThyRowDirective, ThyColDirective],
-    imports: [CommonModule,ThyDirectiveModule]
+    imports: []
 
 })
 
