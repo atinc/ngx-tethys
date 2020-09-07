313d069d56fa50480878b444b2613b0c4f0103a5
luxiaobei <luxiaobei@worktile.com>
2020-09-07 14:50:31 +0800

    style(link): set pointEvent:none on disable link

--- commitlint.config.js
+++ commitlint.config.js
@@ -35,6 +35,7 @@ module.exports = {
                 'input',
                 'label',
                 'layout',
+                'link',
                 'list',
                 'loading',
                 'markdown',
