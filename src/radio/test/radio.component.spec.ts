c967ceaff0f6d6eaab2af30a5272db5be01ab7a5
xinglu01 <15245457817@163.com>
2020-10-29 14:00:26 +0800

    test(radio): add readio test(#INFR-1144)

--- src/radio/test/radio.component.spec.ts
+++ src/radio/test/radio.component.spec.ts
@@ -1,79 +1,81 @@
-import { Component, DebugElement, ViewChild, OnChanges } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ThyRadioModule } from '../module';
 import { ThyRadioComponent } from '../radio.component';
 import { ThyRadioGroupComponent } from '../group/radio-group.component';
 
-// @Component({
-//     selector: 'thy-radio-test',
-//     template: `
-//         <label thyRadio [thyLabelText]="isRadio" [thyDisabled]="isDisabled"></label>
-//     `
-// })
-// class RadioTestComponent {
-//     isRadio: '单选选项';
-//     isDisabled: false;
-// }
+@Component({
+    selector: 'thy-radio-test',
+    template: `
+        <label thyRadio [thyLabelText]="isRadio" [thyDisabled]="isDisabled"></label>
+    `
+})
+class RadioTestComponent {
+    isRadio: '单选选项';
+    isDisabled: false;
+}
 
-// describe('radio component', () => {
-//     let fixture: ComponentFixture<RadioTestComponent>;
-//     let testRadioComponent: RadioTestComponent;
-//     let radioDebugComponent: DebugElement;
-//     let radioElement;
-//     let labelNode;
+describe('radio component', () => {
+    let fixture: ComponentFixture<RadioTestComponent>;
+    let testRadioComponent: RadioTestComponent;
+    let radioDebugComponent: DebugElement;
+    let radioElement;
+    let labelNode;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             imports: [ThyRadioModule, FormsModule],
-//             declarations: [RadioTestComponent],
-//             providers: []
-//         });
-//         TestBed.compileComponents();
-//     });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ThyRadioModule, FormsModule],
+            declarations: [RadioTestComponent],
+            providers: []
+        });
+        TestBed.compileComponents();
+    });
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(RadioTestComponent);
-//         testRadioComponent = fixture.debugElement.componentInstance;
-//         radioDebugComponent = fixture.debugElement.query(By.directive(ThyRadioComponent));
-//         radioElement = radioDebugComponent.nativeElement;
-//         labelNode = radioElement.children[0];
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RadioTestComponent);
+        testRadioComponent = fixture.debugElement.componentInstance;
+        radioDebugComponent = fixture.debugElement.query(By.directive(ThyRadioComponent));
+        radioElement = radioDebugComponent.nativeElement;
+        labelNode = radioElement.children[0];
+    });
 
-//     it('should create', () => {
-//         expect(testRadioComponent).toBeTruthy();
-//         expect(radioElement).toBeTruthy();
-//     });
+    it('should create', () => {
+        expect(testRadioComponent).toBeTruthy();
+        expect(radioElement).toBeTruthy();
+    });
 
-//     it('should select when click', () => {
-//         labelNode.click();
-//         fixture.detectChanges();
-//         expect(fixture.nativeElement.children[0].children[0].checked).toBe(true);
-//     });
+    it('should select when click', () => {
+        labelNode.click();
+        fixture.detectChanges();
+        expect(fixture.nativeElement.children[0].children[0].checked).toBe(true);
+    });
 
-//     it('should have correct class when it‘s isDisabled has changed', () => {
-//         labelNode.isDisabled = true;
-//         fixture.detectChanges();
-//         expect(labelNode.classList.contains('form-check-no-label-text')).toBeTruthy();
-//     });
-// });
+    it('should have correct class when it‘s isDisabled has changed', () => {
+        labelNode.isDisabled = true;
+        fixture.detectChanges();
+        expect(labelNode.classList.contains('form-check-no-label-text')).toBeTruthy();
+    });
+});
 
 @Component({
     selector: 'thy-radio-group-test',
     template: `
         <thy-radio-group [(ngModel)]="checkedValue">
-            <label thyRadio thyLabelText="选项一" [thyValue]="1"></label>
-            <label thyRadio thyLabelText="选项二" [thyValue]="2"></label>
-            <label thyRadio thyLabelText="选项三" [thyValue]="3"></label>
+            <label thyRadio thyLabelText="选项一" [thyValue]="1" [thyInline]="inlineStatus"></label>
+            <label thyRadioButton thyLabelText="选项二" [thyValue]="2" [thyInline]="inlineStatus"></label>
+            <label thyRadio thyLabelText="选项三" [thyValue]="3" [thyInline]="inlineStatus"></label>
         </thy-radio-group>
+        <p class="mt-2">选中的选项值： {{ checkedValue }}</p>
     `
 })
 class RadioGroupTestComponent {
     checkedValue = 1;
+    inlineStatus = false;
 }
 
-describe('thy-radio-group', () => {
+describe('thy-radio-group component', () => {
     let groupFixture: ComponentFixture<RadioGroupTestComponent>;
     let groupComponent: RadioGroupTestComponent;
     let radioGroupDebugComponent: DebugElement;
@@ -84,37 +86,47 @@ describe('thy-radio-group', () => {
             imports: [ThyRadioModule, FormsModule],
             declarations: [RadioGroupTestComponent],
             providers: []
-        });
-        TestBed.compileComponents();
+        }).compileComponents();
     });
-
     beforeEach(() => {
         groupFixture = TestBed.createComponent(RadioGroupTestComponent);
-        groupComponent = groupFixture.debugElement.componentInstance;
+        groupComponent = groupFixture.componentInstance;
         radioGroupDebugComponent = groupFixture.debugElement.query(By.directive(ThyRadioGroupComponent));
         labelComponent = radioGroupDebugComponent.nativeElement.children;
         groupFixture.detectChanges();
+    });
+
+    it('should create thy-radio-group component', () => {
+        expect(groupFixture).toBeTruthy();
+        expect(groupComponent).toBeTruthy();
+        expect(labelComponent).toBeTruthy();
         groupFixture.detectChanges();
     });
 
-    it('should create thy-radio-group', () => {
-        // groupFixture.detectChanges();
-        // console.log('前:::', groupFixture);
-        // console.log('ffefefe:::', radioGroupDebugComponent);
-        expect(groupComponent).toBeTruthy();
-        expect(radioGroupDebugComponent.componentInstance).toBeTruthy();
-    });
+    it(`should has class thyRadioButton to thy-radio-group`, fakeAsync(() => {
+        groupFixture.detectChanges();
+        tick(500);
+        groupFixture.detectChanges();
+        expect(labelComponent[1].classList.contains('btn')).toBeTruthy();
+    }));
 
-    // it('should show class checked', () => {
-    //     groupComponent.checkedValue = 2;
-    //     console.log('ffefefe:::', radioGroupDebugComponent);
-    //     groupFixture.detectChanges();
-    //     [1, 2, 3].forEach((item: number, index: number) => {
-    //         groupComponent.checkedValue = item;
-    //         groupFixture.detectChanges();
+    it(`should change select value`, fakeAsync(() => {
+        groupComponent.checkedValue = 2;
+        groupFixture.detectChanges();
+        tick(500);
+        groupFixture.detectChanges();
+        expect(labelComponent[1].classList.contains('form-check-checked')).toBeTruthy();
+    }));
 
-    //         expect(groupFixture.nativeElement.children[index].classList.contains('form-check-checked')).toBeTruthy();
-    //     });
-    //     expect(groupFixture.nativeElement.children[1].classList.contains('form-check-checked')).toBeTruthy();
-    // });
+    it(`should  has class Toggle inline to thy-radio-group`, fakeAsync(() => {
+        groupComponent.inlineStatus = true;
+        groupComponent.checkedValue = 3;
+        groupFixture.detectChanges();
+        tick(500);
+        groupFixture.detectChanges();
+        expect(labelComponent[2].classList.contains('form-check-checked')).toBeTruthy();
+        [1, 2, 3].forEach((element: number, index: number) => {
+            expect(labelComponent[index].classList.contains('form-check-inline')).toBeTruthy();
+        });
+    }));
 });
