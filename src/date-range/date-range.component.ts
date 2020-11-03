50c6972be0695adbdeea72d5ab5bf96877d511b6
waker744524423 <18689553172@163.com>
2020-11-03 11:23:33 +0800

    chore: review

--- src/date-range/date-range.component.ts
+++ src/date-range/date-range.component.ts
@@ -35,9 +35,9 @@ export class ThyDateRangeComponent implements OnInit, ControlValueAccessor {
 
     @Input() thyMaxDate: Date | number;
 
-    @Input() customKey: 'custom' | 'exception' = 'custom';
+    @Input() thyCustomKey: 'custom' | 'exception' = 'custom';
 
-    @Input() pickerFormat: string;
+    @Input() thyPickerFormat: string;
 
     public selectedDate?: DateRangeItemInfo;
 
@@ -122,13 +122,13 @@ export class ThyDateRangeComponent implements OnInit, ControlValueAccessor {
                     return {
                         begin: getUnixTime(addDays(beginDate, -1 * interval)),
                         end: getUnixTime(addDays(endDate, -1 * interval)),
-                        key: this.customKey
+                        key: this.thyCustomKey
                     };
                 } else {
                     return {
                         begin: getUnixTime(addDays(beginDate, 1 * interval)),
                         end: getUnixTime(addDays(endDate, 1 * interval)),
-                        key: this.customKey
+                        key: this.thyCustomKey
                     };
                 }
             } else if (this.selectedDate.timestamp.unit === 'month') {
@@ -136,13 +136,13 @@ export class ThyDateRangeComponent implements OnInit, ControlValueAccessor {
                     return {
                         begin: getUnixTime(addMonths(beginDate, -1 * interval)),
                         end: getUnixTime(addMonths(endDate, -1 * interval)),
-                        key: this.customKey
+                        key: this.thyCustomKey
                     };
                 } else {
                     return {
                         begin: getUnixTime(addMonths(beginDate, 1 * interval)),
                         end: getUnixTime(addMonths(endDate, 1 * interval)),
-                        key: this.customKey
+                        key: this.thyCustomKey
                     };
                 }
             } else if (this.selectedDate.timestamp.unit === 'year') {
@@ -150,13 +150,13 @@ export class ThyDateRangeComponent implements OnInit, ControlValueAccessor {
                     return {
                         begin: getUnixTime(addYears(beginDate, -1 * interval)),
                         end: getUnixTime(addYears(endDate, -1 * interval)),
-                        key: this.customKey
+                        key: this.thyCustomKey
                     };
                 } else {
                     return {
                         begin: getUnixTime(addYears(beginDate, 1 * interval)),
                         end: getUnixTime(addYears(endDate, 1 * interval)),
-                        key: this.customKey
+                        key: this.thyCustomKey
                     };
                 }
             }
@@ -166,13 +166,13 @@ export class ThyDateRangeComponent implements OnInit, ControlValueAccessor {
                 return {
                     begin: this.selectedDate.begin - interval,
                     end: this.selectedDate.end - interval,
-                    key: this.customKey
+                    key: this.thyCustomKey
                 };
             } else {
                 return {
                     begin: this.selectedDate.begin + interval,
                     end: this.selectedDate.end + interval,
-                    key: this.customKey
+                    key: this.thyCustomKey
                 };
             }
         }
@@ -210,7 +210,7 @@ export class ThyDateRangeComponent implements OnInit, ControlValueAccessor {
                 minDate: this.thyMinDate,
                 maxDate: this.thyMaxDate,
                 customValue: this.thyCustomTextValue,
-                customKey: this.customKey,
+                customKey: this.thyCustomKey,
                 selectedDateRange: (dateRange: DateRangeItemInfo) => {
                     this.onModelChange(dateRange);
                     this.selectedDate = dateRange;
