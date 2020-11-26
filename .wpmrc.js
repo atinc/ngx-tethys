5fdf5e1168ddb17e22445af3e2e1a7b80a79c09f
why520crazy <why520crazy@163.com>
2020-11-26 17:30:30 +0800

    chore: add v9.* to allowBranch

--- .wpmrc.js
+++ .wpmrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  allowBranch: ['master', 'v7.*', 'v8.*'],
-  bumpFiles: [
-    'package.json',
-    'package-lock.json',
-    'src/package.json',
-    {
-      filename: './src/version.ts',
-      type: 'code'
+    allowBranch: ['master', 'v7.*', 'v8.*', 'v9.*'],
+    bumpFiles: [
+        'package.json',
+        'package-lock.json',
+        'src/package.json',
+        {
+            filename: './src/version.ts',
+            type: 'code'
+        }
+    ],
+    // backward compatibility changelog
+    // because we didn't use tag prefix(v) when create tag before
+    // should set tagPrefix as empty (default is 'v')
+    // otherwise, the changelog will rebuild, and will be lost past versions
+    tagPrefix: '',
+    hooks: {
+        prepublish: 'npm run build',
+        postpublish: 'npm run pub-only'
     }
-  ],
-  // backward compatibility changelog
-  // because we didn't use tag prefix(v) when create tag before
-  // should set tagPrefix as empty (default is 'v')
-  // otherwise, the changelog will rebuild, and will be lost past versions
-  tagPrefix: '',
-  hooks: {
-    prepublish: 'npm run build',
-    postpublish: 'npm run pub-only'
-  }
 };
