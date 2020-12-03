b469596fa9fcb758d9bd34299ef5abe92a1c8291
zhangwen <1062680993@qq.com>
2020-12-03 14:32:26 +0800

    fix(fullscreen): export fullscreen in public api

--- src/fullscreen/fullscreen.module.ts
+++ src/fullscreen/fullscreen.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThyFullscreenComponent } from './fullscreen.component';
-import { ThyTooltipModule } from '../tooltip';
-import { ObserversModule } from '@angular/cdk/observers';
 
 @NgModule({
     declarations: [ThyFullscreenComponent],
-    imports: [CommonModule, ThyTooltipModule, ObserversModule],
+    imports: [CommonModule],
     exports: [ThyFullscreenComponent]
 })
 export class ThyFullscreenModule {}
