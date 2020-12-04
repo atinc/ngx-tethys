d0326b7b8c7290ac8ba40d1d85d32fcedf274876
why520crazy <why520crazy@163.com>
2020-12-04 13:45:41 +0800

    feat(icon): add printErrorWhenNotFound config for test #INFR-1330

---
+++ src/icon/config.ts
@@ -0,0 +1,9 @@
+let printErrorWhenNotFound = true;
+
+export function setPrintErrorWhenIconNotFound(value: boolean) {
+    printErrorWhenNotFound = value;
+}
+
+export function getWhetherPrintErrorWhenIconNotFound() {
+    return printErrorWhenNotFound;
+}
