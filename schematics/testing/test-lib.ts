959c8aa1575675ff6598987da5ad9263c4095ce5
why520crazy <why520crazy@163.com>
2020-12-05 09:44:16 +0800

    refactor(schematics): rename createTestApp to createTestLibrary

--- schematics/testing/test-lib.ts
+++ schematics/testing/test-lib.ts
@@ -2,6 +2,6 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import { createTestProject } from './test-project';
 
-export async function createTestApp(runner: SchematicTestRunner, libOptions: any = {}, tree?: Tree): Promise<UnitTestTree> {
+export async function createTestLibrary(runner: SchematicTestRunner, libOptions: any = {}, tree?: Tree): Promise<UnitTestTree> {
     return createTestProject(runner, 'library', libOptions, tree);
 }
