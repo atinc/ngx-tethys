3ce6c73ebf8ccbd7d696924b3f0ed2533aa2f052
minlovehua <321512939@qq.com>
2020-12-09 16:46:08 +0800

    feat(popover): add popover-header (#INFR-1339)

---
+++ src/popover/header/popover-header.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ThyTranslate } from 'ngx-tethys/core';
+
+@Component({
+    selector: 'thy-popover-header',
+    templateUrl: './popover-header.component.html',
+    exportAs: 'thyPopoverHeader'
+})
+export class PopoverHeaderComponent {
+    @Input() thyTitle: string;
+
+    @Input()
+    set thyTitleTranslationKey(key: string) {
+        if (key && !this.thyTitle) {
+            this.thyTitle = this.translate.instant(key);
+        }
+    }
+
+    constructor(private translate: ThyTranslate) {}
+}
--- src/popover/module.ts
+++ src/popover/module.ts
@@ -5,12 +5,13 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
 import { THY_POPOVER_DEFAULT_CONFIG_PROVIDER } from './popover.config';
 import { ThyPopoverDirective } from './popover.directive';
+import { PopoverHeaderComponent } from './header/popover-header.component';
 
 @NgModule({
-    declarations: [ThyPopoverContainerComponent, ThyPopoverDirective],
+    declarations: [ThyPopoverContainerComponent, ThyPopoverDirective, PopoverHeaderComponent],
     entryComponents: [ThyPopoverContainerComponent],
     imports: [CommonModule, OverlayModule, PortalModule],
-    exports: [ThyPopoverDirective],
+    exports: [ThyPopoverDirective, PopoverHeaderComponent],
     providers: [THY_POPOVER_DEFAULT_CONFIG_PROVIDER]
 })
 export class ThyPopoverModule {}
