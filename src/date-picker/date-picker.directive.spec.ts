de52d1f3749c766195575d57bf64e11634dafdac
minlovehua <321512939@qq.com>
2020-12-11 10:42:03 +0800

    test(datepicker): test datepicker-directive-thyStoppropagation (#INFR-1351)

--- src/date-picker/date-picker.directive.spec.ts
+++ src/date-picker/date-picker.directive.spec.ts
@@ -15,6 +15,7 @@ import { ThyPropertyOperationComponent, ThyPropertyOperationModule } from '../pr
 import { ThyDatePickerDirective } from './date-picker.directive';
 import { ThyDatePickerModule } from './date-picker.module';
 import { DatePopupComponent } from './lib/popups/date-popup.component';
+import { de } from 'date-fns/locale';
 
 registerLocaleData(zh);
 
@@ -315,6 +316,38 @@ describe('should get correct default thyPlacement and offset', () => {
     }
 });
 
+describe('should get correct default thyStopPropagation', () => {
+    let fixture: ComponentFixture<ThyTestPickerStopPropagationDirective>;
+    let fixtureInstance: ThyTestPickerStopPropagationDirective;
+    let debugElement: DebugElement;
+
+    beforeEach(fakeAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [ThyDatePickerModule, ThyPropertyOperationModule, BrowserAnimationsModule],
+            declarations: [ThyTestPickerStopPropagationDirective]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ThyTestPickerStopPropagationDirective);
+        fixtureInstance = fixture.componentInstance;
+        debugElement = fixture.debugElement.query(By.css('thy-property-operation'));
+        fixture.detectChanges();
+    });
+
+    it('should get correct default thyStopPropagation value', () => {
+        debugElement.nativeElement.click();
+        expect(fixtureInstance.testStopPropagation).toHaveBeenCalledTimes(0);
+    });
+
+    it('should propagation when thyStopPropagation is false', () => {
+        fixtureInstance.thyStopPropagation = false;
+        fixture.detectChanges();
+        debugElement.nativeElement.click();
+        expect(fixtureInstance.testStopPropagation).toHaveBeenCalledTimes(1);
+    });
+});
+
 @Component({
     template: `
         <thy-property-operation
@@ -330,6 +363,7 @@ describe('should get correct default thyPlacement and offset', () => {
             [thyPlacement]="thyPlacement"
             [thyHasBackdrop]="thyHasBackdrop"
             [thyPopoverOptions]="popoverOptions"
+            [thyStopPropagation]="thyStopPropagation"
         ></thy-property-operation>
     `
 })
@@ -365,3 +399,16 @@ class ThyTestPickerDirective {
     `
 })
 class ThyTestPickerPlacementDirective {}
+
+@Component({
+    template: `
+        <div (click)="testStopPropagation()">
+            <thy-property-operation thyLabelText="开始时间" thyDatePicker [thyStopPropagation]="thyStopPropagation">
+            </thy-property-operation>
+        </div>
+    `
+})
+class ThyTestPickerStopPropagationDirective {
+    thyStopPropagation = true;
+    testStopPropagation = jasmine.createSpy('opened event spy callback');
+}
