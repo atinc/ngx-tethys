0c2fcb2de5faae22299c84d801221fb55c2dd30b
Guoxiin <guoxiin@163.com>
2020-12-17 10:59:06 +0800

    chore(empty): code review

--- src/empty/empty.module.ts
+++ src/empty/empty.module.ts
@@ -1,11 +1,15 @@
+import { ThyIconModule } from 'ngx-tethys/icon';
+import { ThySharedModule } from 'ngx-tethys/shared';
+
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
 import { ThyEmptyComponent } from './empty.component';
 import { ThyEmptyConfig } from './empty.config';
-import { ThySharedModule } from 'ngx-tethys/shared';
+
 @NgModule({
     declarations: [ThyEmptyComponent],
-    imports: [CommonModule, ThySharedModule],
+    imports: [CommonModule, ThySharedModule, ThyIconModule],
     exports: [ThyEmptyComponent],
     providers: [ThyEmptyConfig]
 })
