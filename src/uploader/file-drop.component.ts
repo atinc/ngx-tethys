029a323b713a0aeba5c22bc216ff8321ca1b1a21
mengshuicmq <mengshuicmq@163.com>
2021-01-20 13:55:29 +0800

    fix(uploader): 修复页面中可点击的元素可拖拽上传的问题(#INFR-1329)

--- src/uploader/file-drop.component.ts
+++ src/uploader/file-drop.component.ts
@@ -155,7 +155,7 @@ export class ThyFileDropComponent extends FileSelectBaseComponent implements OnI
         for (let index = 0; index < items.length; index++) {
             const element = items[index];
             const _entry = this._getAsEntry(element);
-            if (_entry && !_entry.isFile) {
+            if (element.kind !== 'file' || (_entry && !_entry.isFile)) {
                 res = false;
                 throw new Error(`file extensions not support`);
             }
