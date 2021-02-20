c7c532739d6e6442f00e63663979d9278827ed45
why520crazy <why520crazy@163.com>
2021-02-20 12:54:15 +0800

    docs: upgrade docgeni to fix error build budgets exceed max size #INFR-1645

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { AppComponent } from './app.component';
 import { NgxTethysModule } from 'ngx-tethys';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EXAMPLE_MODULES, DOCGENI_SITE_PROVIDERS } from './content/index';
+import { DOCGENI_SITE_PROVIDERS } from './content/index';
 import { DocgeniTemplateModule } from '@docgeni/template';
+
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, DocgeniTemplateModule, RouterModule.forRoot([]), NgxTethysModule, ...EXAMPLE_MODULES],
+    imports: [BrowserModule, BrowserAnimationsModule, DocgeniTemplateModule, RouterModule.forRoot([]), NgxTethysModule],
     providers: [...DOCGENI_SITE_PROVIDERS],
     bootstrap: [AppComponent]
 })
