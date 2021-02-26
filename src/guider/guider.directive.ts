9103f6eee952613d36ffd5c8ecea25ccbf86ef54
walkekay <walkerkay@163.com>
2021-02-26 18:11:44 +0800

    fix(guider): fix not display hint after route jump

--- src/guider/guider.directive.ts
+++ src/guider/guider.directive.ts
@@ -1,24 +1,28 @@
 import { ThyGuiderManager } from './guider-manager';
-import { AfterViewInit, Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, OnDestroy, OnInit, NgZone } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Directive({ selector: '[thyGuiderTarget]', exportAs: 'thyGuiderTarget' })
 export class ThyGuiderTargetDirective implements OnInit, OnDestroy, AfterViewInit {
     @Input('thyGuiderTarget')
     target: string;
 
-    constructor(private guiderManager: ThyGuiderManager, private el: ElementRef) {}
+    constructor(private guiderManager: ThyGuiderManager, private el: ElementRef, private ngZone: NgZone) {}
 
     ngOnInit() {
         this.guiderManager.addStepTarget(this.target, this.el.nativeElement);
+        this.ngZone.onStable.pipe(take(1)).subscribe(() => {
+            const { key, guiderRef } = this.guiderManager.getActive();
+            if (key === this.target) {
+                const index = guiderRef.steps.findIndex(step => step.key === this.target);
+                this.ngZone.run(() => {
+                    guiderRef.active(index);
+                });
+            }
+        });
     }
 
-    ngAfterViewInit() {
-        const { key, guiderRef } = this.guiderManager.getActive();
-        if (key === this.target) {
-            const index = guiderRef.steps.findIndex(step => step.key === this.target);
-            guiderRef.active(index);
-        }
-    }
+    ngAfterViewInit() {}
 
     ngOnDestroy() {
         const { guiderRef } = this.guiderManager.getActive();
