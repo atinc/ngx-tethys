5d8eae5bac3b105d042622ebe14bcc4a00b10459
luxiaobei <luxiaobei@worktile.com>
2021-04-13 18:52:55 +0800

    docs(slide): refactor slide docs INFR-1799 (#1207)

---
+++ src/slide/examples/layout/layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-slide-layout-example',
+    templateUrl: './layout.component.html',
+    styleUrls: ['./layout.component.scss']
+})
+export class ThySlideLayoutExampleComponent {
+    constructor() {}
+}
---
+++ src/slide/examples/mode/index.md
@@ -0,0 +1,4 @@
+---
+title: Mode
+order: 50
+---
