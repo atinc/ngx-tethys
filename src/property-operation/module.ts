0daf9b4f2c7f9818477620960b1dde3e0ffc6157
Guoxiin <guoxiin@163.com>
2021-04-25 17:25:53 +0800

    feat(property-operation): add thyFlexibleText #INFR-1911 (#1234)

--- src/property-operation/module.ts
+++ src/property-operation/module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ThyButtonModule } from 'ngx-tethys/button';
-import { ThyPropertyOperationComponent } from './property-operation.component';
-import { ThyPropertyOperationGroupComponent } from './group/property-operation-group.component';
+import { ThyFlexibleTextModule } from 'ngx-tethys/flexible-text';
 import { ThyIconModule } from 'ngx-tethys/icon';
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ThyPropertyOperationGroupComponent } from './group/property-operation-group.component';
+import { ThyPropertyOperationComponent } from './property-operation.component';
 
 @NgModule({
     declarations: [ThyPropertyOperationComponent, ThyPropertyOperationGroupComponent],
-    imports: [CommonModule, ThyButtonModule, ThyIconModule],
+    imports: [CommonModule, ThyButtonModule, ThyIconModule, ThyFlexibleTextModule],
     exports: [ThyPropertyOperationComponent, ThyPropertyOperationGroupComponent]
 })
 export class ThyPropertyOperationModule {}
