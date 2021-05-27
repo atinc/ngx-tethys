976e1d3f0af77b36ca6820187119d35b5798bb8c
why520crazy <why520crazy@163.com>
2021-05-27 18:58:38 +0800

    test(icon): add test coverage to 92.86% #INFR-1641

--- src/icon/test/icon.spec.ts
+++ src/icon/test/icon.spec.ts
@@ -1,74 +1,366 @@
-import { Component } from '@angular/core';
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
-import { ThyIconModule } from '../icon.module';
-import { HttpClientModule } from '@angular/common/http';
-import { ThyIconRegistry } from '../icon-registry';
 import { bypassSanitizeProvider, injectDefaultSvgIconSet } from 'ngx-tethys/testing';
+import { generateRandomStr } from 'ngx-tethys/util';
+import { of } from 'rxjs';
 
-//#region test component
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By, DomSanitizer } from '@angular/platform-browser';
+
+import { getWhetherPrintErrorWhenIconNotFound, setPrintErrorWhenIconNotFound } from '../config';
+import { ThyIconRegistry } from '../icon-registry';
+import { ThyIconComponent } from '../icon.component';
+import { ThyIconModule } from '../icon.module';
 
 @Component({
     template: `
-        <thy-icon [thyIconName]="thyIconName" [thyIconType]="thyIconType" [thyIconLegging]="thyIconLegging"></thy-icon>
+        <thy-icon
+            [thyIconName]="iconName"
+            [thyIconLinearGradient]="linearGradient"
+            [thyIconType]="iconType"
+            [thyIconRotate]="rotate"
+            [thyIconLegging]="legging"
+            [thyTwotoneColor]="twotoneColor"
+            [thyIconSet]="iconSet"
+        ></thy-icon>
     `
 })
-class ThyIconBasicComponent {
+class ThyIconTestBasicComponent {
     constructor(public iconRegistry: ThyIconRegistry) {}
-    thyIconName = 'check';
-    thyIconType = '';
-    thyIconLegging = null;
+    iconName = 'check';
+    iconType = '';
+    legging = false;
+    linearGradient = false;
+    rotate: number;
+    twotoneColor: string;
+    iconSet: string;
 }
 
-//#endregion
-
 describe('ThyIconComponent', () => {
-    beforeEach(async(() => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [ThyIconModule, HttpClientModule],
-            declarations: [ThyIconBasicComponent],
+            imports: [ThyIconModule, HttpClientTestingModule],
+            declarations: [ThyIconTestBasicComponent],
             providers: [bypassSanitizeProvider]
         });
         TestBed.compileComponents();
         injectDefaultSvgIconSet();
-    }));
+    });
 
-    describe('test begin', () => {
-        let fixture: ComponentFixture<ThyIconBasicComponent>;
-        let componentInstance: ThyIconBasicComponent;
-        const iconSvgClassPrefix = 'thy-icon-';
+    describe('Basic', () => {
+        let fixture: ComponentFixture<ThyIconTestBasicComponent>;
+        let componentInstance: ThyIconTestBasicComponent;
+        let iconDebugElement: DebugElement;
+        const iconSvgClassPrefix = 'thy-icon';
 
-        beforeEach(async(() => {
-            fixture = TestBed.createComponent(ThyIconBasicComponent);
+        function assertSvgIcon(iconElement: HTMLElement, iconName: string, classSuffix?: 'fill' | 'tt') {
+            expect(iconElement.classList.contains(iconSvgClassPrefix)).toBeTruthy();
+            const expectClass = classSuffix ? `${iconSvgClassPrefix}-${iconName}-${classSuffix}` : `${iconSvgClassPrefix}-${iconName}`;
+            expect(iconElement.classList.contains(expectClass)).toBeTruthy(
+                `expect class ${expectClass}, actual classList is ${iconElement.classList}`
+            );
+            const svgElement = iconElement.querySelector('svg');
+            expect(svgElement).toBeTruthy();
+        }
+
+        beforeEach(() => {
+            fixture = TestBed.createComponent(ThyIconTestBasicComponent);
             componentInstance = fixture.debugElement.componentInstance;
             fixture.detectChanges();
-        }));
-
-        it('should has icon name class', () => {
-            expect(fixture.nativeElement.querySelector(`.${iconSvgClassPrefix}${componentInstance.thyIconName}`)).toBeTruthy();
+            iconDebugElement = fixture.debugElement.query(By.directive(ThyIconComponent));
         });
 
-        it('should has new icon name class', () => {
-            expect(fixture.nativeElement.querySelector(`.${iconSvgClassPrefix}${componentInstance.thyIconName}`)).toBeTruthy();
+        it('should create icon success', () => {
+            expect(iconDebugElement.componentInstance).toBeTruthy();
+            assertSvgIcon(iconDebugElement.nativeElement, 'check');
         });
 
-        it('should not be found old icon name class', () => {
-            const oldIconName = componentInstance.thyIconName;
-            componentInstance.thyIconName = 'close';
+        it('should set new icon success', () => {
+            const oldIconName = componentInstance.iconName;
+            componentInstance.iconName = 'close';
             fixture.detectChanges();
+            assertSvgIcon(iconDebugElement.nativeElement, 'close');
             expect(fixture.nativeElement.querySelector(`.${iconSvgClassPrefix}${oldIconName}`)).toBeFalsy();
         });
 
-        it('should set thyIconType fill have correct class', () => {
-            componentInstance.thyIconType = 'fill';
+        it('should set icon type success', () => {
+            componentInstance.iconType = 'fill';
             fixture.detectChanges();
-            expect(fixture.nativeElement.querySelector(`.${iconSvgClassPrefix}${componentInstance.thyIconName}`)).toBeFalsy();
-            expect(fixture.nativeElement.querySelector(`.${iconSvgClassPrefix}${componentInstance.thyIconName}-fill`)).toBeTruthy();
+            assertSvgIcon(iconDebugElement.nativeElement, 'check', 'fill');
+
+            componentInstance.iconType = 'twotone';
+            fixture.detectChanges();
+            assertSvgIcon(iconDebugElement.nativeElement, 'check', 'tt');
         });
 
-        it('should set thyIconLegging be true have correct class', () => {
-            componentInstance.thyIconLegging = true;
+        it('should set thyIconLegging success', () => {
+            componentInstance.legging = true;
             fixture.detectChanges();
-            expect(fixture.nativeElement.querySelector(`.${iconSvgClassPrefix}legging`)).toBeTruthy();
+            assertSvgIcon(iconDebugElement.nativeElement, 'check');
+            expect(iconDebugElement.nativeElement.classList.contains(`${iconSvgClassPrefix}-legging`)).toBeTruthy();
+
+            componentInstance.legging = false;
+            fixture.detectChanges();
+            expect(iconDebugElement.nativeElement.classList.contains(`${iconSvgClassPrefix}-legging`)).toBeFalsy();
         });
+
+        it('should set linearGradient success', () => {
+            componentInstance.iconName = 'filter';
+            componentInstance.linearGradient = true;
+            fixture.detectChanges();
+            const svgElement: HTMLElement = iconDebugElement.nativeElement.querySelector('svg');
+            expect(svgElement).toBeTruthy();
+            expect(svgElement.querySelector('title')).toBeFalsy();
+        });
+
+        it('should set rotate success', () => {
+            componentInstance.rotate = 90;
+            fixture.detectChanges();
+            assertSvgIcon(iconDebugElement.nativeElement, 'check');
+            const svgElement: HTMLElement = iconDebugElement.nativeElement.querySelector('svg');
+            expect(svgElement).toBeTruthy();
+            expect(svgElement.style.transform).toEqual(`rotate(90deg)`);
+        });
+
+        it('should set twotone success', () => {
+            componentInstance.iconType = 'twotone';
+            componentInstance.iconName = 'sort-tt';
+            componentInstance.twotoneColor = '#66666';
+            fixture.detectChanges();
+
+            const svgElement: HTMLElement = iconDebugElement.nativeElement.querySelector('svg');
+            expect(svgElement).toBeTruthy();
+            const ttPathElement = svgElement.querySelector('#sort-secondary-color');
+            expect(ttPathElement).toBeTruthy();
+            expect(ttPathElement.getAttribute('fill')).toBe('#66666');
+        });
+
+        it('should set icon mode to font', () => {
+            const iconRegistry = TestBed.inject(ThyIconRegistry);
+            iconRegistry.setIconMode('font');
+            componentInstance.iconName = 'inbox';
+            fixture.detectChanges();
+            expect(iconDebugElement.componentInstance).toBeTruthy();
+            const iconElement: HTMLElement = iconDebugElement.nativeElement;
+            expect(iconElement.classList.contains('wt-icon'));
+            expect(iconElement.classList.contains('wt-icon-inbox'));
+            iconRegistry.setIconMode('svg');
+        });
+
+        it('should set icon mode to font and custom iconSet', () => {
+            const iconRegistry = TestBed.inject(ThyIconRegistry);
+            iconRegistry.setIconMode('font');
+            componentInstance.iconName = 'inbox';
+            componentInstance.iconSet = 'myset';
+            fixture.detectChanges();
+            expect(iconDebugElement.componentInstance).toBeTruthy();
+            const iconElement: HTMLElement = iconDebugElement.nativeElement;
+            expect(iconElement.classList.contains('myset-icon'));
+            expect(iconElement.classList.contains('myset-icon-inbox'));
+            iconRegistry.setIconMode('svg');
+        });
+    });
+});
+
+describe('IconRegistry', () => {
+    let iconRegistry: ThyIconRegistry;
+    let domSanitizer: DomSanitizer;
+    let svgRandomName: string;
+    let httpClientSpy: { get: jasmine.Spy };
+
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+        TestBed.configureTestingModule({
+            imports: [ThyIconModule, HttpClientTestingModule],
+            declarations: [],
+            providers: [bypassSanitizeProvider, { provide: HttpClient, useValue: httpClientSpy }]
+        });
+        TestBed.compileComponents();
+        iconRegistry = TestBed.inject(ThyIconRegistry);
+        domSanitizer = TestBed.inject(DomSanitizer);
+        svgRandomName = generateRandomStr();
+    });
+
+    function createFakeSvg(name: string) {
+        const svg = `<svg id="${name}"><path name="${name}"></path></svg>`;
+        return svg;
+    }
+
+    function createFakeSvgSymbol(name: string) {
+        const svg = `<symbol id="${name}"><path name="${name}"></path></symbol>`;
+        return svg;
+    }
+
+    function createRandomUrl() {
+        return `http://127.0.0.1/svg/${generateRandomStr()}`;
+    }
+
+    function assertSvgElement(svg: SVGElement, name: string) {
+        expect(svg).toBeTruthy();
+        expect(svg.getAttribute('width')).toBe('1em');
+        expect(svg.getAttribute('height')).toBe('1em');
+        expect(svg.getAttribute('preserveAspectRatio')).toBe('xMidYMid meet');
+        expect(svg.querySelector('path').getAttribute('name')).toEqual(name);
+    }
+
+    function assertGetSvgIconSuccess(name: string, namespace?: string) {
+        const spy = jasmine.createSpy('get svg icon success spy');
+        iconRegistry.getSvgIcon(name, namespace).subscribe(spy);
+        expect(spy).toHaveBeenCalled();
+        const callArgs = spy.calls.argsFor(0);
+        assertSvgElement(callArgs[0], name);
+    }
+
+    function assertGetSvgIconFail(name: string, namespace?: string) {
+        const spy = jasmine.createSpy('get svg icon success spy');
+        const failSpy = jasmine.createSpy('get svg icon fail spy');
+        iconRegistry.getSvgIcon(name, namespace).subscribe(spy, failSpy);
+        expect(spy).not.toHaveBeenCalled();
+        expect(failSpy).toHaveBeenCalled();
+        expect(failSpy.calls.argsFor(0)[0].message).toBe(`Unable to find icon with the name ":${name}"`);
+    }
+
+    it('should create success', () => {
+        expect(iconRegistry).toBeTruthy();
+    });
+
+    it('should addSvgIconLiteral success', () => {
+        const svg = createFakeSvg(svgRandomName);
+        iconRegistry.addSvgIconLiteral(svgRandomName, domSanitizer.bypassSecurityTrustHtml(svg));
+        assertGetSvgIconSuccess(svgRandomName);
+    });
+
+    it('should addSvgIconLiteral fail when svg is not trusted html', () => {
+        const svg = createFakeSvg(svgRandomName);
+        expect(() => {
+            iconRegistry.addSvgIconLiteral(svgRandomName, svg);
+        }).toThrowError(
+            `The literal provided to ThyIconRegistry was not trusted as safe HTML by Angular's DomSanitizer. Attempted literal was "${svg}".`
+        );
+    });
+
+    it('should addSvgIconLiteralInNamespace success', () => {
+        const svg = createFakeSvg(svgRandomName);
+        iconRegistry.addSvgIconLiteralInNamespace('nsp1', svgRandomName, domSanitizer.bypassSecurityTrustHtml(svg));
+
+        assertGetSvgIconSuccess(svgRandomName, 'nsp1');
+        assertGetSvgIconFail(svgRandomName);
+    });
+
+    it('should addSvgIcon success', () => {
+        const svg = createFakeSvg(svgRandomName);
+        const url = createRandomUrl();
+        iconRegistry.addSvgIcon(svgRandomName, domSanitizer.bypassSecurityTrustResourceUrl(url));
+        httpClientSpy.get.and.returnValue(of(svg));
+        assertGetSvgIconSuccess(svgRandomName);
+    });
+
+    it('should addSvgIcon fail when url is not trusted url', () => {
+        const url = createRandomUrl();
+        const sanitizer = TestBed.inject(DomSanitizer);
+        sanitizer.sanitize = (ctx, value) => {
+            if (url === value) {
+                return null;
+            } else {
+                sanitizer.sanitize(ctx, value);
+            }
+        };
+
+        expect(() => {
+            iconRegistry.addSvgIcon(svgRandomName, url);
+            iconRegistry.getSvgIcon(svgRandomName).subscribe();
+        }).toThrowError(
+            `The URL provided to ThyIconRegistry was not trusted as a resource URL via Angular's DomSanitizer. Attempted URL was "null".`
+        );
+    });
+
+    it('should addSvgIcon fail when url is null', () => {
+        expect(() => {
+            iconRegistry.addSvgIcon(svgRandomName, null);
+            iconRegistry.getSvgIcon(svgRandomName).subscribe();
+        }).toThrowError(`Cannot fetch icon from URL "${null}".`);
+    });
+
+    it('should addSvgIconSet success', () => {
+        const svg1Name = generateRandomStr();
+        const svg2Name = generateRandomStr();
+        const svg1 = createFakeSvg(svg1Name);
+        const svg2 = createFakeSvg(svg2Name);
+        const url = createRandomUrl();
+        iconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl(url));
+        httpClientSpy.get.and.returnValue(of(`<svg><defs>${svg1}${svg2}</defs></svg>`));
+        assertGetSvgIconSuccess(svg2Name);
+    });
+
+    it('should addSvgIconSet success for symbol', () => {
+        const symbol1Name = generateRandomStr();
+        const symbol2Name = generateRandomStr();
+        const symbol1 = createFakeSvgSymbol(symbol1Name);
+        const symbol2 = createFakeSvgSymbol(symbol2Name);
+        const url = createRandomUrl();
+        iconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl(url));
+        httpClientSpy.get.and.returnValue(of(`<svg>${symbol1}${symbol2}</svg>`));
+        assertGetSvgIconSuccess(symbol1Name);
+        assertGetSvgIconSuccess(symbol2Name);
+    });
+
+    it('should addSvgIconSetInNamespace success', () => {
+        const svg1Name = generateRandomStr();
+        const svg2Name = generateRandomStr();
+        const svg1 = createFakeSvg(svg1Name);
+        const svg2 = createFakeSvg(svg2Name);
+        const url = createRandomUrl();
+        iconRegistry.addSvgIconSetInNamespace('nps2', domSanitizer.bypassSecurityTrustResourceUrl(url));
+        httpClientSpy.get.and.returnValue(of(`<svg><defs>${svg1}${svg2}</defs></svg>`));
+        assertGetSvgIconSuccess(svg1Name, 'nps2');
+        assertGetSvgIconSuccess(svg2Name, 'nps2');
+        assertGetSvgIconFail(svg1Name);
+        assertGetSvgIconFail(svg2Name);
+    });
+
+    describe('splitIconName', () => {
+        it('should split icon name success use default namespace', () => {
+            const [namespace, name] = iconRegistry.splitIconName(svgRandomName);
+            expect(namespace).toEqual('');
+            expect(name).toEqual(svgRandomName);
+        });
+
+        it('should split icon name success with namespace', () => {
+            const [namespace, name] = iconRegistry.splitIconName(`nsp3:${svgRandomName}`);
+            expect(namespace).toEqual('nsp3');
+            expect(name).toEqual(svgRandomName);
+        });
+
+        it('should split icon name success with empty', () => {
+            const [namespace, name] = iconRegistry.splitIconName(``);
+            expect(namespace).toEqual('');
+            expect(name).toEqual('');
+        });
+
+        it('should throw error when split icon name invalid', () => {
+            expect(() => {
+                iconRegistry.splitIconName(`nsp3:${svgRandomName}:hello`);
+            }).toThrowError(`Invalid icon name: "nsp3:${svgRandomName}:hello"`);
+        });
+    });
+});
+
+describe('Config', () => {
+    let originalWhetherPrintErrorWhenIconNotFound: boolean;
+
+    beforeEach(() => {
+        originalWhetherPrintErrorWhenIconNotFound = getWhetherPrintErrorWhenIconNotFound();
+    });
+
+    afterEach(() => {
+        setPrintErrorWhenIconNotFound(originalWhetherPrintErrorWhenIconNotFound);
+    });
+
+    it('should set print error when icon not found success', () => {
+        setPrintErrorWhenIconNotFound(true);
+        expect(getWhetherPrintErrorWhenIconNotFound()).toEqual(true);
+        setPrintErrorWhenIconNotFound(false);
+        expect(getWhetherPrintErrorWhenIconNotFound()).toEqual(false);
     });
 });
--- src/testing/thy-icon.ts
+++ src/testing/thy-icon.ts
@@ -1,6 +1,7 @@
+import { ThyIconRegistry } from 'ngx-tethys/icon';
+
 import { Provider, Sanitizer, SecurityContext } from '@angular/core';
 import { inject } from '@angular/core/testing';
-import { ThyIconRegistry } from 'ngx-tethys/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
 export const bypassSanitizeProvider: Provider = {
@@ -13,13 +14,13 @@ export const bypassSanitizeProvider: Provider = {
 export const defaultInlineIconSet = `
   <svg>
     <defs>
-        <svg id="inbox"></svg>
-        <svg id="close"></svg>
-        <svg id="close-bold"></svg>
-        <svg id="calendar-check"></svg>
-        <svg id="calendar"></svg>
-        <svg id="angle-down"></svg>
-        <svg id="check"></svg>
+        <svg id="inbox"><path d="M10 10"/></svg>
+        <svg id="close"><path d="M10 10"/></svg>
+        <svg id="close-bold"><path d="M10 10"/></svg>
+        <svg id="calendar-check"><path d="M10 10"/></svg>
+        <svg id="calendar"><path d="M10 10"/></svg>
+        <svg id="angle-down"><path d="M10 10"/></svg>
+        <svg id="check"><path d="M10 10"/></svg>
         <svg id="application-fill"></svg>
         <svg id="angle-right"></svg>
         <svg id="minus-circle-fill"></svg>
@@ -28,8 +29,14 @@ export const defaultInlineIconSet = `
         <svg id="close-circle-fill"></svg>
         <svg id="question-circle-fill"></svg>
         <svg id="more-bold"></svg>
-        <svg id="filter"></svg>
+        <svg id="filter">
+         <title>Filter Title</title>
+         <defs> <linearGradient id="Gradient1"><stop class="stop1" offset="0%"/><stop class="stop2" offset="50%"/><stop class="stop3" offset="100%"/></linearGradient></defs>
+         <style> #rect1 { fill: url(#Gradient1); }</style>
+         <path d="filter" style=" "/>
+        </svg>
         <svg id="close-circle-bold-fill"></svg>
+        <svg id="sort-tt"><style>.circle {  fill: orange; }</style><g id="phnormal/sort-tt" stroke-width="1" fill-rule="nonzero"><path d="M10 10 0 0 1 5.1 1.5v12.99a.6.6 0 0 1-1.2 0V3.607L2.466 6.082a.6.6 0 1 1-1.04-.6l2.47-4.262a.599.599 0 0 1 .426-.293z" id="ph形状结合"/><path d="M13.765 1.027a.6.6 0 0 1 .778.573v12.805a.6.6 0 1 1-1.2 0V3.682L11.82 6.276a.6.6 0 1 1-1.04-.6l2.558-4.356a.599.599 0 0 1 .427-.293z" id="sort-secondary-color" transform="rotate(-180 12.621 8.003)"/></g></svg>
     </defs>
   </svg>
 `;
