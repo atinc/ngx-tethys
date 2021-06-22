31861ef40e4dc3a560eadf89b61e342519e2c4a4
ggxxgxq <35676994+ggxxgxq@users.noreply.github.com>
2021-06-22 09:45:56 +0800

    docs(radio): optimize documentation #INFR-1830 (#1284)
    * docs(radio): optimize documentation #INFR-1830
    
    * chore: review

---
+++ src/radio/examples/size/size.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-radio-size-example',
+    templateUrl: './size.component.html'
+})
+export class ThyRadioSizeExampleComponent implements OnInit {
+    public checkedValue: string;
+
+    constructor() {}
+
+    ngOnInit() {}
+}
