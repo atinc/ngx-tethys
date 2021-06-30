b820f53051b61e90781c9fa030459d09f24f9f6c
xinglu01 <35676233+xinglu01@users.noreply.github.com>
2021-06-30 10:26:24 +0800

    test(strength): add strength test(#INFR-1841)

--- src/strength/test/strength.component.spec.ts
+++ src/strength/test/strength.component.spec.ts
@@ -1,10 +1,57 @@
-import { Component, DebugElement } from '@angular/core';
+import { Component, DebugElement, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ThyStrengthModule } from '../strength.module';
 import { ThyStrengthComponent } from '../strength.component';
 import { ThyButtonModule } from 'ngx-tethys/button';
+
+@Component({
+    selector: 'thy-strength-basic-test',
+    template: `
+        <thy-strength [(ngModel)]="value"></thy-strength>
+    `
+})
+class StrengthBasicTestComponent {
+    public value = 1;
+}
+
+describe('Strength basic component', () => {
+    let fixture: ComponentFixture<StrengthBasicTestComponent>;
+    let testStrengthBasicComponent: StrengthBasicTestComponent;
+    let strengthBasicDebugComponent: DebugElement;
+    let strengthBasicElement: HTMLElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ThyStrengthModule, FormsModule, ThyButtonModule],
+            declarations: [StrengthBasicTestComponent],
+            providers: []
+        });
+        TestBed.compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(StrengthBasicTestComponent);
+        testStrengthBasicComponent = fixture.debugElement.componentInstance;
+        strengthBasicDebugComponent = fixture.debugElement.query(By.directive(ThyStrengthComponent));
+        strengthBasicElement = strengthBasicDebugComponent.nativeElement;
+    });
+
+    it('should create', () => {
+        expect(testStrengthBasicComponent).toBeTruthy();
+        expect(strengthBasicElement).toBeTruthy();
+    });
+
+    it('should have correct class when ngModel value change', fakeAsync(() => {
+        testStrengthBasicComponent.value = 4;
+        fixture.detectChanges();
+        tick(500);
+        fixture.detectChanges();
+        expect(strengthBasicElement.children[0].children[0].classList.contains('strength-text-highest')).toBe(true);
+        expect(strengthBasicElement.children[1].classList.contains('strength-level-highest')).toBe(true);
+    }));
+});
 
 @Component({
     selector: 'thy-strength-test',
@@ -17,35 +64,11 @@ import { ThyButtonModule } from 'ngx-tethys/button';
             [averageKey]="text[currentTextType].averageKey"
             [lowKey]="text[currentTextType].lowKey"
         ></thy-strength>
-        <h6>切换程度值：</h6>
-        <thy-button-group thyType="outline-default">
-            <button
-                thyButton
-                *ngFor="let curStrength of strengths"
-                (click)="strength = curStrength"
-                [ngClass]="{ active: strength === curStrength }"
-            >
-                {{ curStrength }}
-            </button>
-        </thy-button-group>
-        <h6>切换程度文本：</h6>
-        <thy-button-group thyType="outline-default">
-            <button
-                thyButton
-                *ngFor="let text of toggleText"
-                (click)="currentTextType = text"
-                [ngClass]="{ active: currentTextType === text }"
-            >
-                {{ text }}
-            </button>
-        </thy-button-group>
     `
 })
 class StrengthTestComponent {
     public strength = 2;
     public currentTextType = 'custom';
-    public strengths = [1, 2, 3, 4];
-    public toggleText = ['default', 'custom'];
     public text = {
         default: {
             title: '密码强度',
@@ -67,10 +90,8 @@ class StrengthTestComponent {
 describe('Strength component', () => {
     let fixture: ComponentFixture<StrengthTestComponent>;
     let testStrengthComponent: StrengthTestComponent;
-    let StrengthDebugComponent: DebugElement;
-    let StrengthElement: HTMLElement;
-    let StrengthElementChildText: any;
-    let StrengthElementChildLevel: any;
+    let strengthDebugComponent: DebugElement;
+    let strengthElement: HTMLElement;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -84,25 +105,23 @@ describe('Strength component', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(StrengthTestComponent);
         testStrengthComponent = fixture.debugElement.componentInstance;
-        StrengthDebugComponent = fixture.debugElement.query(By.directive(ThyStrengthComponent));
-        StrengthElement = StrengthDebugComponent.nativeElement;
-        StrengthElementChildText = StrengthElement.children[0];
-        StrengthElementChildLevel = StrengthElement.children[1];
+        strengthDebugComponent = fixture.debugElement.query(By.directive(ThyStrengthComponent));
+        strengthElement = strengthDebugComponent.nativeElement;
     });
 
     it('should create', () => {
         expect(testStrengthComponent).toBeTruthy();
-        expect(StrengthElement).toBeTruthy();
+        expect(strengthElement).toBeTruthy();
     });
 
-    it('should have correct class when ngModel value change', fakeAsync(() => {
+    it('should have correct class when change strength', fakeAsync(() => {
         testStrengthComponent.strength = 3;
         fixture.detectChanges();
         tick(500);
         fixture.detectChanges();
-        expect(StrengthElementChildText.innerText).toBe('评分 🌟🌟🌟');
-        expect(StrengthElementChildText.children[0].classList.contains('strength-text-high')).toBe(true);
-        expect(StrengthElementChildLevel.classList.contains('strength-level-high')).toBe(true);
+        expect((strengthElement.children[0] as HTMLElement).innerText).toBe('评分 🌟🌟🌟');
+        expect(strengthElement.children[0].children[0].classList.contains('strength-text-high')).toBe(true);
+        expect(strengthElement.children[1].classList.contains('strength-level-high')).toBe(true);
     }));
 
     it('should have correct class when change type', fakeAsync(() => {
@@ -110,9 +129,9 @@ describe('Strength component', () => {
         fixture.detectChanges();
         tick(500);
         fixture.detectChanges();
-        expect(StrengthElementChildText.innerText).toBe('密码强度 中');
-        expect(StrengthElementChildText.children[0].classList.contains('strength-text-average')).toBe(true);
-        expect(StrengthElementChildText.children[0].innerText).toBe('中');
-        expect(StrengthElementChildLevel.classList.contains('strength-level-average')).toBe(true);
+        expect((strengthElement.children[0] as HTMLElement).innerText).toBe('密码强度 中');
+        expect(strengthElement.children[0].children[0].classList.contains('strength-text-average')).toBe(true);
+        expect((strengthElement.children[0].children[0] as HTMLElement).innerText).toBe('中');
+        expect(strengthElement.children[1].classList.contains('strength-level-average')).toBe(true);
     }));
 });
