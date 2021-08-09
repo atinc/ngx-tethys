c851fb66c8a963215f38f58c25ab355177097ca7
尹晓斌 <yxb941006@163.com>
2021-08-09 17:14:32 +0800

    fix(copy): fix notifyService multiple calls error (#INFR-2186) (#1322)

--- src/copy/module.ts
+++ src/copy/module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThyCopyDirective } from './copy.directive';
+import { ThyNotifyModule } from '../notify/notify.module';
 
 @NgModule({
     declarations: [ThyCopyDirective],
-    imports: [CommonModule],
+    imports: [CommonModule, ThyNotifyModule],
     exports: [ThyCopyDirective]
 })
 export class ThyCopyModule {}
