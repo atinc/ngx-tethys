32a7767dc0dde327258417abf18958e7ed92a878
why520crazy <why520crazy@users.noreply.github.com>
2021-08-18 14:16:38 +0800

    refactor(core): update default generic to unknown from any and refactor abstract overlay types (#1329)
    、

--- src/dialog/dialog-ref.ts
+++ src/dialog/dialog-ref.ts
@@ -6,9 +6,9 @@ import { ThyDialogContainerComponent } from './dialog-container.component';
 import { ThyDialogConfig } from './dialog.config';
 import { dialogAbstractOverlayOptions } from './dialog.options';
 
-export abstract class ThyDialogRef<T, TResult = any> extends ThyAbstractOverlayRef<T, ThyDialogContainerComponent, TResult> {}
+export abstract class ThyDialogRef<T, TResult = unknown> extends ThyAbstractOverlayRef<T, ThyDialogContainerComponent, TResult> {}
 
-export class ThyInternalDialogRef<T, TResult = any> extends ThyAbstractInternalOverlayRef<T, ThyDialogContainerComponent, TResult> {
+export class ThyInternalDialogRef<T, TResult = unknown> extends ThyAbstractInternalOverlayRef<T, ThyDialogContainerComponent, TResult> {
     constructor(overlayRef: OverlayRef, containerInstance: ThyDialogContainerComponent, config: ThyDialogConfig<T>) {
         super(dialogAbstractOverlayOptions, overlayRef, containerInstance, config);
     }
