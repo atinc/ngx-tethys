b3f23043003b7fec1462a7e89adca854070e4b35
gaoxiangqing <1356857447@qq.com>
2021-08-25 16:48:01 +0800

    feat(vote): vote support disabled #INFR-2229

--- src/vote/examples/module.ts
+++ src/vote/examples/module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgxTethysModule } from 'ngx-tethys';
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ThyVoteBasicExampleComponent } from './basic/basic.component';
+import { ThyVoteDisabledExampleComponent } from './disabled/disabled.component';
 import { ThyVoteIconExampleComponent } from './icon/icon.component';
 import { ThyVoteWeakExampleComponent } from './weak/weak.component';
 
-const COMPONENTS = [ThyVoteBasicExampleComponent, ThyVoteIconExampleComponent, ThyVoteWeakExampleComponent];
+const COMPONENTS = [
+    ThyVoteBasicExampleComponent,
+    ThyVoteIconExampleComponent,
+    ThyVoteWeakExampleComponent,
+    ThyVoteDisabledExampleComponent
+];
 
 @NgModule({
     declarations: COMPONENTS,
