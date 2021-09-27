5c127fc325ce9490ebc3425e7484e0875ac85274
minlovehua <wmm13612286703@gmail.com>
2021-09-27 16:58:18 +0800

    docs(dialog): dialog backdrop example (#INFR-1825)

--- src/dialog/examples/backdrop/backdrop.component.ts
+++ src/dialog/examples/backdrop/backdrop.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThyDialog, ThyDialogConfig } from 'ngx-tethys';
 import { ThyDialogBasicContentComponent } from '../basic/dialog-content.component';
 
 @Component({
     selector: 'thy-dialog-backdrop-example',
-    templateUrl: './backdrop.component.html'
+    templateUrl: './backdrop.component.html',
+    styleUrls: ['./backdrop.component.scss']
 })
-export class ThyDialogBackdropExampleComponent {
-    public config: ThyDialogConfig = {
-        backdropClosable: true,
-        closeOnNavigation: true
-    };
+export class ThyDialogBackdropExampleComponent implements OnInit {
+    public isCustomBackdropClass: boolean;
+
+    public config: ThyDialogConfig;
 
     constructor(private thyDialog: ThyDialog) {}
 
+    ngOnInit() {
+        this.config = {
+            hasBackdrop: true,
+            backdropClosable: true
+        };
+    }
+
     openDialog() {
         this.thyDialog.open(ThyDialogBasicContentComponent, this.config);
+    }
+
+    setBackdropClass() {
+        Object.assign(this.config, {
+            backdropClass: this.isCustomBackdropClass ? 'custom-backdrop-class' : ''
+        });
     }
 }
