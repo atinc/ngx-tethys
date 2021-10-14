e6a6f9c49481c56b04828db4768f85d5bf848a5a
yinxiaobin <yxb941006@163.com>
2021-10-14 17:13:59 +0800

    feat(schematics): add update-12 testing file

--- schematics/ng-update/update-12/index.ts
+++ schematics/ng-update/update-12/index.ts
@@ -3,7 +3,7 @@ import { chain, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { TsMigrationSchematicsRule } from '../../class';
 import { ImportEntryPointChangeMigrationByNg12 } from './migrations/import-entry-point-change';
 import { ImportNameChangeMigrationByNg12 } from './migrations/import-name-change';
-export function main() {
+export default function main() {
     return (tree: Tree, ctx: SchematicContext) => {
         return chain([TsMigrationSchematicsRule([ImportEntryPointChangeMigrationByNg12, ImportNameChangeMigrationByNg12])]);
     };
