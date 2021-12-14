cad1ae80d213071dfcad65e6e93e5906796270ea
minlovehua <321512939@qq.com>
2021-12-14 14:12:11 +0800

    fix(shared): listen event name of ThyStopPropagationDirective must be a string (#INFR-2453)

--- src/shared/directives/thy-stop-propagation.directive.ts
+++ src/shared/directives/thy-stop-propagation.directive.ts
@@ -11,14 +11,11 @@ export class ThyStopPropagationDirective implements OnInit, OnDestroy {
 
     private _eventName = 'click';
 
-    private isStopPropagation = true;
-
     @Input()
     set thyStopPropagation(value: string | boolean) {
         if (value === false || value === 'false') {
-            this.isStopPropagation = false;
+            this._eventName = '';
         } else {
-            this.isStopPropagation = true;
             this._eventName = value && typeof value === 'string' ? value : 'click';
         }
     }
@@ -26,7 +23,7 @@ export class ThyStopPropagationDirective implements OnInit, OnDestroy {
     constructor(private _elementRef: ElementRef, private _renderer: Renderer2) {}
 
     ngOnInit() {
-        if (this.isStopPropagation) {
+        if (this._eventName) {
             this._listener = this._renderer.listen(this._elementRef.nativeElement, this._eventName, ($event: Event) => {
                 $event.stopPropagation();
             });
