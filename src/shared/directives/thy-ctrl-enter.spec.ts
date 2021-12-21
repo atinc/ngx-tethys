5f83bc0aec4c8c5bd89cec5d77a617ba7f6e4d52
luxiaobei <luxiaobei@worktile.com>
2021-12-21 17:25:10 +0800

    test(shared): add test about thyCtrlEnter

---
+++ src/shared/directives/thy-ctrl-enter.spec.ts
@@ -0,0 +1,48 @@
+import { ENTER } from 'ngx-tethys/util';
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { dispatchKeyboardEvent } from '../../testing';
+import { ThySharedModule } from '../shared.module';
+
+@Component({
+    selector: 'thy-autofocus-test',
+    template: `
+        <input #enter (thyCtrlEnter)="ok($event)" />
+    `
+})
+class ThyCtrlEnterTestComponent {
+    @ViewChild('enter') input: ElementRef;
+
+    ok(event: KeyboardEvent) {}
+}
+
+describe('ThyCtrlEnterDirective', () => {
+    let fixture: ComponentFixture<ThyCtrlEnterTestComponent>;
+    let testComponent: ThyCtrlEnterTestComponent;
+    let enterSpy: jasmine.Spy;
+
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                imports: [ThySharedModule],
+                declarations: [ThyCtrlEnterTestComponent]
+            }).compileComponents();
+        })
+    );
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ThyCtrlEnterTestComponent);
+        testComponent = fixture.componentInstance;
+        fixture.detectChanges();
+        enterSpy = spyOn(testComponent, 'ok');
+    });
+
+    it('should call thyCtrlEnter when enter', () => {
+        const event = dispatchKeyboardEvent(testComponent.input.nativeElement, 'keydown', ENTER, '', { control: true, meta: true });
+        fixture.detectChanges();
+        expect(enterSpy).toHaveBeenCalled();
+        expect(enterSpy).toHaveBeenCalledWith(event);
+    });
+});
