ed49797827f9d4af625d312ad864b4cee11f6ca4
Walker <15701592+walkerkay@users.noreply.github.com>
2022-02-15 18:29:55 +0800

    feat(pagination):  support custom pages #INFR-2600 (#1463)

---
+++ src/pagination/examples/custom-pages/custom-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-pagination-custom-pages-example',
+    templateUrl: './custom-pages.component.html'
+})
+export class ThyPaginationCustomPagesExampleComponent {
+    currentIndex = 1;
+
+    pages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    constructor() {}
+
+    pageIndexChange(page: number) {}
+}
