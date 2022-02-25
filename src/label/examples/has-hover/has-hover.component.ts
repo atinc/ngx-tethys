00f5ef3960d1be7e258376a90865f07dd5197f3f
cmq <mengshuicmq@163.com>
2022-02-25 13:40:28 +0800

    docs(label): add label docs(#INFR-2609) (#1470)

---
+++ src/label/examples/has-hover/has-hover.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-label-has-hover-example',
+    templateUrl: './has-hover.component.html'
+})
+export class ThyLabelHasHoverExampleComponent {}
