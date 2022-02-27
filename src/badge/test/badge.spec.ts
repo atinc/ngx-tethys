c52d0739b46856b441481b20b18933d991934007
minlovehua <321512939@qq.com>
2022-02-27 09:30:52 +0800

    test(badge): improve badge test coverage to 100% #INFR-2655

--- src/badge/test/badge.spec.ts
+++ src/badge/test/badge.spec.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgModule, DebugElement } from '@angular/core';
+import { Component, OnInit, DebugElement } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ThyBadgeModule } from '../badge.module';
 import { ThyBadgeComponent } from '../badge.component';
@@ -7,7 +7,7 @@ import { By } from '@angular/platform-browser';
 @Component({
     selector: 'thy-badge-basic',
     template: `
-        <thy-badge [thyCount]="count" [thyKeepShow]="isValueKeepShow" [thyType]="type" [thyMaxCount]="maxCount">
+        <thy-badge [thyCount]="count" [thyKeepShow]="isValueKeepShow" [thyType]="type" [thyMaxCount]="maxCount" [thySize]="size">
             <div>WORKTILE</div>
         </thy-badge>
     `
@@ -17,9 +17,11 @@ class BadgeBasicComponent implements OnInit {
 
     count = 5;
 
-    maxCount = 99;
+    maxCount: number;
 
-    type = `danger`;
+    type: string;
+
+    size: string;
 
     constructor() {}
 
@@ -29,7 +31,7 @@ class BadgeBasicComponent implements OnInit {
 @Component({
     selector: 'thy-badge-context',
     template: `
-        <thy-badge [thyContext]="'new'">
+        <thy-badge [thyContext]="context">
             <div>WORKTILE</div>
         </thy-badge>
     `
@@ -37,39 +39,37 @@ class BadgeBasicComponent implements OnInit {
 class BadgeBasicContextComponent implements OnInit {
     constructor() {}
 
+    context = 'Worktile';
+
     ngOnInit(): void {}
 }
 
 @Component({
-    selector: 'thy-badge-size',
+    selector: 'thy-badge-hollow',
     template: `
-        <thy-badge [thyCount]="count" [thySize]="size" [thyIsDot]="isDot" [thyIsHollow]="isHollow">
+        <thy-badge [thyIsHollow]="isHollow">
             <div>WORKTILE</div>
         </thy-badge>
     `
 })
-class BadgeBasicSizeComponent implements OnInit {
-    count = '10';
-
-    size = '';
-
-    isDot = false;
-
-    isHollow = false;
-
+class BadgeBasicHollowComponent implements OnInit {
     constructor() {}
+
+    isHollow: boolean;
 
     ngOnInit(): void {}
 }
 
 @Component({
-    selector: 'thy-badge-circle',
+    selector: 'thy-badge-dot',
     template: `
-        <thy-badge thyIsDot="true"></thy-badge>
+        <thy-badge [thyIsDot]="isDot"></thy-badge>
     `
 })
-class BadgeBasicCircleComponent implements OnInit {
+class BadgeBasicDotComponent implements OnInit {
     constructor() {}
+
+    isDot: boolean;
 
     ngOnInit(): void {}
 }
@@ -87,9 +87,8 @@ class BadgeBasicSpecialComponent implements OnInit {
 }
 
 describe('thy-badge', () => {
-    let testComponent;
-    let badgeElement;
-    // let badgeSpanElement;
+    let testComponent: any;
+    let badgeElement: HTMLElement;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -97,8 +96,8 @@ describe('thy-badge', () => {
             declarations: [
                 BadgeBasicComponent,
                 BadgeBasicContextComponent,
-                BadgeBasicSizeComponent,
-                BadgeBasicCircleComponent,
+                BadgeBasicHollowComponent,
+                BadgeBasicDotComponent,
                 BadgeBasicSpecialComponent
             ]
         });
@@ -106,30 +105,30 @@ describe('thy-badge', () => {
     });
 
     describe('basic', () => {
-        let basicFixture: ComponentFixture<BadgeBasicComponent>;
-        let badgeBasicDebugComponent;
+        let fixture: ComponentFixture<BadgeBasicComponent>;
+        let badgeComponent: DebugElement;
 
         beforeEach(() => {
-            basicFixture = TestBed.createComponent(BadgeBasicComponent);
-            testComponent = basicFixture.debugElement.componentInstance;
-            badgeBasicDebugComponent = basicFixture.debugElement.query(By.directive(ThyBadgeComponent));
-            badgeElement = badgeBasicDebugComponent.nativeElement;
+            fixture = TestBed.createComponent(BadgeBasicComponent);
+            testComponent = fixture.debugElement.componentInstance;
+            badgeComponent = fixture.debugElement.query(By.directive(ThyBadgeComponent));
+            badgeElement = badgeComponent.nativeElement;
         });
 
         it('should create', () => {
-            expect(basicFixture).toBeTruthy();
-            expect(badgeBasicDebugComponent).toBeTruthy();
+            expect(fixture).toBeTruthy();
+            expect(badgeComponent).toBeTruthy();
             expect(badgeElement).toBeTruthy();
         });
 
         it('should get correct thy-badge-container class', () => {
-            basicFixture.detectChanges();
+            fixture.detectChanges();
             expect(badgeElement.classList.contains('thy-badge-container')).toBe(true);
         });
 
         it('should span get basic correct class', () => {
-            basicFixture.detectChanges();
-            const badgeSpanElement = badgeBasicDebugComponent.nativeElement.children[0];
+            fixture.detectChanges();
+            const badgeSpanElement = badgeComponent.nativeElement.children[0];
             expect(badgeSpanElement).toBeTruthy();
             expect(badgeSpanElement.classList.contains('thy-badge')).toBe(true);
             expect(badgeSpanElement.classList.contains('thy-badge-count')).toBe(true);
@@ -137,147 +136,158 @@ describe('thy-badge', () => {
             expect(badgeSpanElement.classList.contains('thy-badge-sup')).toBe(true);
         });
 
-        it('should show max count', () => {
+        it('thyCount, should show set count success', () => {
+            testComponent.count = 1;
+            fixture.detectChanges();
+            const badgeSpanElement = badgeComponent.nativeElement.querySelector('.thy-badge');
+            expect(badgeSpanElement.textContent).toBe(`1`);
+            testComponent.count = 2;
+            fixture.detectChanges();
+            expect(badgeSpanElement.textContent).toBe(`2`);
+        });
+
+        it('thyMaxCount, should set max count success', () => {
             testComponent.count = 100;
-            basicFixture.detectChanges();
-            const badgeSpanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(badgeSpanElement.textContent).toBe(`${testComponent.maxCount}+`);
+            fixture.detectChanges();
+            const badgeSpanElement = badgeComponent.nativeElement.querySelector('.thy-badge');
+            expect(badgeSpanElement.textContent).toBe(`100`);
+            testComponent.maxCount = 99;
+            fixture.detectChanges();
+            expect(badgeSpanElement.textContent).toBe(`99+`);
         });
 
-        it('should keep show when value is 0', () => {
+        it('thyKeepShow, should keep show badge when count is 0 and thyKeepShow is true', () => {
+            testComponent.count = 0;
+            testComponent.isValueKeepShow = false;
+            fixture.detectChanges();
+            expect(badgeComponent.nativeElement.querySelector('.thy-badge')).toBeFalsy();
             testComponent.isValueKeepShow = true;
-            basicFixture.detectChanges();
-            const badgeSpanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(badgeSpanElement).toBeTruthy();
+            fixture.detectChanges();
+            expect(badgeComponent.nativeElement.querySelector('.thy-badge')).toBeTruthy();
         });
 
-        it('should have correct class when type is primary', () => {
-            testComponent.type = `primary`;
-            basicFixture.detectChanges();
-            const badgeSpanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(badgeSpanElement).toBeTruthy();
-            expect(badgeSpanElement.classList.contains('thy-badge-primary')).toBe(true);
+        it('thyType, should set type success', () => {
+            ['primary', 'warning', 'secondary', 'danger'].forEach(type => {
+                testComponent.type = type;
+                fixture.detectChanges();
+                const badgeSpanElement = badgeComponent.nativeElement.querySelector('.thy-badge');
+                expect(badgeSpanElement).toBeTruthy();
+                expect(badgeSpanElement.classList.contains(`thy-badge-${type}`)).toBe(true);
+            });
         });
 
-        it('should have correct class when type is warning', () => {
-            testComponent.type = `warning`;
-            basicFixture.detectChanges();
-            const badgeSpanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(badgeSpanElement).toBeTruthy();
-            expect(badgeSpanElement.classList.contains('thy-badge-warning')).toBe(true);
-        });
-
-        it('should have correct class when type is secondary', () => {
-            testComponent.type = `secondary`;
-            basicFixture.detectChanges();
-            const badgeSpanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(badgeSpanElement).toBeTruthy();
-            expect(badgeSpanElement.classList.contains('thy-badge-secondary')).toBe(true);
+        it('thySize, should set size success', () => {
+            ['sm', 'lg'].forEach(size => {
+                testComponent.size = size;
+                fixture.detectChanges();
+                const badgeSpanElement = badgeComponent.nativeElement.querySelector('.thy-badge');
+                expect(badgeSpanElement).toBeTruthy();
+                expect(badgeSpanElement.classList.contains(`thy-badge-${size}`)).toBe(true);
+            });
         });
     });
 
     describe('context', () => {
-        let basicFixture: ComponentFixture<BadgeBasicContextComponent>;
-        let badgeBasicDebugComponent;
+        let fixture: ComponentFixture<BadgeBasicContextComponent>;
+        let badgeComponent: DebugElement;
 
         beforeEach(() => {
-            basicFixture = TestBed.createComponent(BadgeBasicContextComponent);
-            testComponent = basicFixture.debugElement.componentInstance;
-            badgeBasicDebugComponent = basicFixture.debugElement.query(By.directive(ThyBadgeComponent));
-            badgeElement = badgeBasicDebugComponent.nativeElement;
+            fixture = TestBed.createComponent(BadgeBasicContextComponent);
+            testComponent = fixture.debugElement.componentInstance;
+            badgeComponent = fixture.debugElement.query(By.directive(ThyBadgeComponent));
+            badgeElement = badgeComponent.nativeElement;
         });
 
         it('should create', () => {
-            expect(basicFixture).toBeTruthy();
-            expect(badgeBasicDebugComponent).toBeTruthy();
+            expect(fixture).toBeTruthy();
+            expect(badgeComponent).toBeTruthy();
             expect(badgeElement).toBeTruthy();
         });
 
-        it('should show new context', () => {
-            basicFixture.detectChanges();
-            const badgeSpanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(badgeSpanElement.textContent).toBe(`new`);
+        it('thyContext, should set context success', () => {
+            fixture.detectChanges();
+            const badgeSpanElement = badgeElement.querySelector('.thy-badge');
+            expect(badgeSpanElement.textContent).toBe(`Worktile`);
+            testComponent.context = 'PingCode';
+            fixture.detectChanges();
+            expect(badgeSpanElement.textContent).toBe(`PingCode`);
         });
     });
 
-    describe('size', () => {
-        let basicFixture: ComponentFixture<BadgeBasicSizeComponent>;
-        let badgeBasicDebugComponent;
+    describe('hollow', () => {
+        let fixture: ComponentFixture<BadgeBasicHollowComponent>;
+        let badgeComponent: DebugElement;
 
         beforeEach(() => {
-            basicFixture = TestBed.createComponent(BadgeBasicSizeComponent);
-            testComponent = basicFixture.debugElement.componentInstance;
-            badgeBasicDebugComponent = basicFixture.debugElement.query(By.directive(ThyBadgeComponent));
-            badgeElement = badgeBasicDebugComponent.nativeElement;
+            fixture = TestBed.createComponent(BadgeBasicHollowComponent);
+            testComponent = fixture.debugElement.componentInstance;
+            badgeComponent = fixture.debugElement.query(By.directive(ThyBadgeComponent));
+            badgeElement = badgeComponent.nativeElement;
         });
 
         it('should create', () => {
-            expect(basicFixture).toBeTruthy();
-            expect(badgeBasicDebugComponent).toBeTruthy();
+            expect(fixture).toBeTruthy();
+            expect(badgeComponent).toBeTruthy();
             expect(badgeElement).toBeTruthy();
         });
 
-        it('should have correct class when size is lg', () => {
-            testComponent.size = `lg`;
-            basicFixture.detectChanges();
-            const spanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(spanElement.classList.contains('thy-badge-lg')).toBe(true);
-        });
-
-        it('should have correct class when size is sm', () => {
-            testComponent.size = `sm`;
-            basicFixture.detectChanges();
-            const spanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(spanElement.classList.contains('thy-badge-sm')).toBe(true);
+        it('thyIsHollow, should set hollow badge success', () => {
+            fixture.detectChanges();
+            expect(badgeElement.querySelector('.thy-badge-hollow')).toBeFalsy();
+            testComponent.isHollow = true;
+            fixture.detectChanges();
+            expect(badgeElement.querySelector('.thy-badge-hollow')).toBeTruthy();
         });
     });
 
-    describe('circle', () => {
-        let basicFixture: ComponentFixture<BadgeBasicCircleComponent>;
-        let badgeBasicDebugComponent;
+    describe('dot', () => {
+        let fixture: ComponentFixture<BadgeBasicDotComponent>;
+        let badgeComponent: DebugElement;
 
         beforeEach(() => {
-            basicFixture = TestBed.createComponent(BadgeBasicCircleComponent);
-            testComponent = basicFixture.debugElement.componentInstance;
-            badgeBasicDebugComponent = basicFixture.debugElement.query(By.directive(ThyBadgeComponent));
-            badgeElement = badgeBasicDebugComponent.nativeElement;
+            fixture = TestBed.createComponent(BadgeBasicDotComponent);
+            testComponent = fixture.debugElement.componentInstance;
+            badgeComponent = fixture.debugElement.query(By.directive(ThyBadgeComponent));
+            badgeElement = badgeComponent.nativeElement;
         });
 
         it('should create', () => {
-            expect(basicFixture).toBeTruthy();
-            expect(badgeBasicDebugComponent).toBeTruthy();
+            expect(fixture).toBeTruthy();
+            expect(badgeComponent).toBeTruthy();
             expect(badgeElement).toBeTruthy();
         });
 
-        it('should have thy-badge-dot class', () => {
-            basicFixture.detectChanges();
-            const spanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(spanElement.classList.contains('thy-badge-dot')).toBe(true);
+        it('thyIsDot, should set dot badge success', () => {
+            fixture.detectChanges();
+            expect(badgeElement.querySelector('.thy-badge-dot')).toBeFalsy;
+            testComponent.isDot = true;
+            fixture.detectChanges();
+            expect(badgeElement.querySelector('.thy-badge-dot')).toBeTruthy;
         });
     });
 
     describe('special', () => {
-        let basicFixture: ComponentFixture<BadgeBasicSpecialComponent>;
-        let badgeBasicDebugComponent;
+        let fixture: ComponentFixture<BadgeBasicSpecialComponent>;
+        let badgeComponent: DebugElement;
 
         beforeEach(() => {
-            basicFixture = TestBed.createComponent(BadgeBasicSpecialComponent);
-            testComponent = basicFixture.debugElement.componentInstance;
-            badgeBasicDebugComponent = basicFixture.debugElement.query(By.directive(ThyBadgeComponent));
-            badgeElement = badgeBasicDebugComponent.nativeElement;
+            fixture = TestBed.createComponent(BadgeBasicSpecialComponent);
+            testComponent = fixture.debugElement.componentInstance;
+            badgeComponent = fixture.debugElement.query(By.directive(ThyBadgeComponent));
+            badgeElement = badgeComponent.nativeElement;
         });
 
         it('should create', () => {
-            expect(basicFixture).toBeTruthy();
-            expect(badgeBasicDebugComponent).toBeTruthy();
+            expect(fixture).toBeTruthy();
+            expect(badgeComponent).toBeTruthy();
             expect(badgeElement).toBeTruthy();
         });
 
-        it('should have color, backgroundColor', () => {
-            basicFixture.detectChanges();
-            const spanElement = badgeBasicDebugComponent.nativeElement.children[0];
-            expect(spanElement.style.color).toBe('rgb(0, 255, 0)');
-            expect(spanElement.style.backgroundColor).toBe('rgb(255, 0, 255)');
+        it('should set text color, backgroundColor success', () => {
+            fixture.detectChanges();
+            const badgeSpanElement = badgeComponent.nativeElement.children[0];
+            expect(badgeSpanElement.style.color).toBe('rgb(0, 255, 0)');
+            expect(badgeSpanElement.style.backgroundColor).toBe('rgb(255, 0, 255)');
         });
     });
 });
