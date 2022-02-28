d96ddf97e28b89cc64d16a9ca66ce706af2d8999
why520crazy <why520crazy@163.com>
2022-02-28 14:44:01 +0800

    fix(button): reset padding-x for only icon button in group #INFR-2657 - move thyButtonIcon examples to btn-icon folder - add icon example for only icon button - refactor styles for button

--- src/button/examples/module.ts
+++ src/button/examples/module.ts
@@ -1,36 +1,8 @@
 import { ThyButtonModule } from 'ngx-tethys/button';
 import { ThySpaceModule } from 'ngx-tethys/space';
 import { ThyIconModule } from 'ngx-tethys/icon';
-
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 
-import { ThyButtonBasicExampleComponent } from './basic/basic.component';
-import { ThyButtonBlockExampleComponent } from './block/block.component';
-import { ThyButtonGroupExampleComponent } from './group/group.component';
-import { ThyButtonIconExampleComponent } from './icon/icon.component';
-import { ThyButtonLinkExampleComponent } from './link/link.component';
-import { ThyButtonLoadingExampleComponent } from './loading/loading.component';
-import { ThyButtonOutlineExampleComponent } from './outline/outline.component';
-import { ThyButtonPairExampleComponent } from './pair/pair.component';
-import { ThyButtonSizeExampleComponent } from './size/size.component';
-
-const COMPONENTS = [
-    ThyButtonBasicExampleComponent,
-    ThyButtonPairExampleComponent,
-    ThyButtonIconExampleComponent,
-    ThyButtonLinkExampleComponent,
-    ThyButtonSizeExampleComponent,
-    ThyButtonOutlineExampleComponent,
-    ThyButtonLoadingExampleComponent,
-    ThyButtonGroupExampleComponent,
-    ThyButtonBlockExampleComponent
-];
-
-@NgModule({
-    declarations: [...COMPONENTS],
-    entryComponents: [...COMPONENTS],
-    imports: [CommonModule, ThyButtonModule, ThyIconModule, ThySpaceModule],
-    exports: [...COMPONENTS]
-})
-export class ThyButtonExamplesModule {}
+export default {
+    imports: [CommonModule, ThyButtonModule, ThyIconModule, ThySpaceModule]
+};
