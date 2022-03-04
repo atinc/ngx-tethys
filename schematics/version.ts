f9a97221e2bc61d31124b245c6a77145fe7637c4
zhangwen <1062680993@qq.com>
2022-03-04 10:06:32 +0800

    chore(release): 12.1.9

--- schematics/version.ts
+++ schematics/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = '12.1.8';
+export const VERSION = '12.1.9';
--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 import { Version } from '@angular/core';
 
-export const VERSION = new Version('12.1.8');
+export const VERSION = new Version('12.1.9');
