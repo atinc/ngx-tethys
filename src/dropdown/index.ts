832e3d8095b08ecc42c952278d917d1486f58921
why520crazy <why520crazy@163.com>
2022-03-09 11:29:14 +0800

    fix(dropdown): export all components for fix build

--- src/dropdown/index.ts
+++ src/dropdown/index.ts
@@ -1,2 +1,5 @@
 export * from './module';
 export * from './dropdown.directive';
+export * from './dropdown-menu.component';
+export * from './dropdown-menu-item.directive';
+export * from './dropdown-submenu.component';
