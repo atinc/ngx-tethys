ec6879978920c78e3bf5c92bb7b8c86fdadd58a3
arturovt <arthurandrosovich@gmail.com>
2022-03-20 03:17:04 +0200

    fix(shared): remove the `contextmenu` listener once the `thyContextMenu` is destroyed

--- src/shared/directives/thy-contextmenu.directive.ts
+++ src/shared/directives/thy-contextmenu.directive.ts
@@ -1,23 +1,29 @@
-import { Directive, Output, ElementRef, EventEmitter, OnInit, NgZone, Renderer2, HostListener } from '@angular/core';
+import { Directive, Output, ElementRef, EventEmitter, OnInit, NgZone, Renderer2, HostListener, OnDestroy } from '@angular/core';
 
 @Directive({
     selector: '[thyContextMenu]'
 })
-export class ThyContextMenuDirective implements OnInit {
+export class ThyContextMenuDirective implements OnInit, OnDestroy {
     @Output() thyContextMenu = new EventEmitter();
+
+    private removeContextListenerFn: VoidFunction;
 
     constructor(private ngZone: NgZone, private elementRef: ElementRef, private renderer: Renderer2) {}
 
-    rightClick(event: Event) {
+    rightClick = (event: Event) => {
         event.preventDefault();
         this.ngZone.run(() => {
             this.thyContextMenu.emit(event);
         });
-    }
+    };
 
     ngOnInit() {
         this.ngZone.runOutsideAngular(() => {
-            this.renderer.listen(this.elementRef.nativeElement, 'contextmenu', this.rightClick.bind(this));
+            this.removeContextListenerFn = this.renderer.listen(this.elementRef.nativeElement, 'contextmenu', this.rightClick);
         });
+    }
+
+    ngOnDestroy(): void {
+        this.removeContextListenerFn();
     }
 }
