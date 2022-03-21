93b493f3b83cf4f0fb55665e2412e1766f7fa236
arturovt <arthurandrosovich@gmail.com>
2022-03-21 18:58:24 +0200

    fix(shared): remove the `keydown` listener once the `ThyCtrlEnterDirective` is destroyed

--- src/shared/directives/thy-ctrl-enter.directive.ts
+++ src/shared/directives/thy-ctrl-enter.directive.ts
@@ -1,15 +1,15 @@
-import { Directive, Output, ElementRef, EventEmitter, OnInit, NgZone, Renderer2 } from '@angular/core';
+import { Directive, Output, ElementRef, EventEmitter, OnInit, NgZone, Renderer2, OnDestroy } from '@angular/core';
 import { keycodes } from 'ngx-tethys/util';
 
-@Directive({
-    selector: '[thyCtrlEnter]'
-})
-export class ThyCtrlEnterDirective implements OnInit {
+@Directive({ selector: '[thyCtrlEnter]' })
+export class ThyCtrlEnterDirective implements OnInit, OnDestroy {
     @Output() thyCtrlEnter = new EventEmitter();
+
+    private removeKeydownListenerFn: VoidFunction;
 
     constructor(private ngZone: NgZone, private elementRef: ElementRef, private renderer: Renderer2) {}
 
-    onKeydown(event: KeyboardEvent) {
+    onKeydown = (event: KeyboardEvent) => {
         const keyCode = event.which || event.keyCode;
         if ((event.ctrlKey || event.metaKey) && keyCode === keycodes.ENTER) {
             event.preventDefault();
@@ -17,11 +17,15 @@ export class ThyCtrlEnterDirective implements OnInit {
                 this.thyCtrlEnter.emit(event);
             });
         }
-    }
+    };
 
     ngOnInit() {
         this.ngZone.runOutsideAngular(() => {
-            this.renderer.listen(this.elementRef.nativeElement, 'keydown', this.onKeydown.bind(this));
+            this.removeKeydownListenerFn = this.renderer.listen(this.elementRef.nativeElement, 'keydown', this.onKeydown);
         });
+    }
+
+    ngOnDestroy(): void {
+        this.removeKeydownListenerFn();
     }
 }
