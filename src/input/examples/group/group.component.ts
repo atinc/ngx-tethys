42f167acba0028651ae2075734da7a455f6ea4eb
why520crazy <why520crazy@163.com>
2022-03-25 19:13:17 +0800

    docs(input): refactor input docs,  add pre-suffix and autofocus examples #INFR-2796

---
+++ src/input/examples/group/group.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-input-group-example',
+    templateUrl: './group.component.html'
+})
+export class ThyInputGroupExampleComponent implements OnInit {
+    public value: any;
+
+    constructor() {}
+
+    ngOnInit() {}
+}
---
+++ src/input/examples/pre-suffix/index.md
@@ -0,0 +1,4 @@
+---
+order: 35
+title: Prepend & Append
+---
