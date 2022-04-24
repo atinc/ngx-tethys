0afa89a5266651f6f145901cdc51561ac9430f54
yinxiaobin <yxb941006@163.com>
2022-04-24 11:14:14 +0800

    feat(popover): add minWidth at popover default config default value is 240px #INFR-2976

--- src/dropdown/examples/module.ts
+++ src/dropdown/examples/module.ts
@@ -5,9 +5,33 @@ import { ThyDropdownModule } from 'ngx-tethys/dropdown';
 import { ThyPopoverModule } from 'ngx-tethys/popover';
 import { ThySpaceModule } from 'ngx-tethys/space';
 import { FormsModule } from '@angular/forms';
-import { ThyFormModule, ThyInputModule, ThySelectModule } from 'ngx-tethys';
+import { ThyFormModule, ThyInputModule, ThyInputNumberModule, ThySelectModule } from 'ngx-tethys';
+import { ThyDropdownIconExampleComponent } from './icon/icon.component';
+import { ThyDropdownTypeExampleComponent } from './type/type.component';
+import { ThyDropdownBasicExampleComponent } from './basic/basic.component';
+import { ThyDropdownGroupExampleComponent } from './group/group.component';
+import { ThyDropdownSplitExampleComponent } from './split/split.component';
+import { ThyDropdownOptionsExampleComponent } from './options/options.component';
+import { ThyDropdownSubmenuExampleComponent } from './submenu/submenu.component';
+import { ThyDropdownTriggerExampleComponent } from './trigger/trigger.component';
+import { ThyDropdownDisabledExampleComponent } from './disabled/disabled.component';
+import { NgModule } from '@angular/core';
 
-export default {
+const COMPONENTS = [
+    ThyDropdownIconExampleComponent,
+    ThyDropdownTypeExampleComponent,
+    ThyDropdownBasicExampleComponent,
+    ThyDropdownGroupExampleComponent,
+    ThyDropdownSplitExampleComponent,
+    ThyDropdownOptionsExampleComponent,
+    ThyDropdownSubmenuExampleComponent,
+    ThyDropdownTriggerExampleComponent,
+    ThyDropdownDisabledExampleComponent
+];
+
+@NgModule({
+    declarations: COMPONENTS,
+    entryComponents: COMPONENTS,
     imports: [
         ThyDropdownModule,
         ThyButtonModule,
@@ -18,6 +42,9 @@ export default {
         ThyInputModule,
         ThySelectModule,
         ThyFormModule,
-        FormsModule
-    ]
-};
+        FormsModule,
+        ThyInputNumberModule
+    ],
+    exports: COMPONENTS
+})
+export class DropdownExampleModule {}
