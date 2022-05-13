291f0ba498359a3b1f4bd82acbe23c0530713a7e
luxiaobei <2548259062@qq.com>
2022-05-13 18:14:12 +0800

    feat(schematics): add schematics for migrate to v13 (#1664)

--- schematics/ng-update/update-12/migrations/import-entry-point-change.ts
+++
@@ -1,9 +0,0 @@
-import { ImportEntryPointChangeMigrationBase } from '../../../class';
-
-export class ImportEntryPointChangeMigrationByNg12 extends ImportEntryPointChangeMigrationBase {
-    readonly changeModulePackageGroup: { [name: string]: { with: string; replace: string } } = {};
-
-    readonly changePackageGroup = {
-        'ngx-tethys/store': '@tethys/store'
-    };
-}
