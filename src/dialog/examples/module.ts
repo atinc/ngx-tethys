137b5653f193f700f8302aeae20980ffe64c707f
why520crazy <why520crazy@163.com>
2022-05-25 15:21:57 +0800

    docs: add lost module imports for examples

--- src/dialog/examples/module.ts
+++ src/dialog/examples/module.ts
@@ -3,31 +3,12 @@ import { ThySelectModule } from 'ngx-tethys/select';
 
 import { CdkScrollableModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { ThyDialogBackdropExampleComponent } from './backdrop/backdrop.component';
-import { ThyDialogBasicExampleComponent } from './basic/basic.component';
-import { ThyDialogBasicContentComponent } from './basic/dialog-content.component';
-import { ThyDialogConfirmExampleComponent } from './confirm/confirm.component';
+import { ThyButtonModule } from 'ngx-tethys/button';
 import { ThyDialogFooterLayoutExampleComponent } from './layout/dialog-layout.component';
-import { ThyDialogLayoutExampleComponent } from './layout/layout.component';
-import { ThyDialogSizeExampleComponent } from './size/size.component';
+import { ThyDialogBasicContentComponent } from './basic/dialog-content.component';
 
-const COMPONENTS = [
-    ThyDialogBasicExampleComponent,
-    ThyDialogBasicContentComponent,
-    ThyDialogConfirmExampleComponent,
-    ThyDialogSizeExampleComponent,
-    ThyDialogBackdropExampleComponent,
-    ThyDialogLayoutExampleComponent,
-    ThyDialogFooterLayoutExampleComponent
-];
-
-@NgModule({
-    declarations: COMPONENTS,
-    imports: [CommonModule, FormsModule, ThyDialogModule, CdkScrollableModule, ThySelectModule],
-    exports: [],
-    providers: COMPONENTS
-})
-export class ThyDialogExamplesModule {}
+export default {
+    declarations: [ThyDialogFooterLayoutExampleComponent, ThyDialogBasicContentComponent],
+    imports: [CommonModule, FormsModule, ThyDialogModule, CdkScrollableModule, ThySelectModule, ThyButtonModule]
+};
--- src/stepper/examples/module.ts
+++ src/stepper/examples/module.ts
@@ -1,18 +1,9 @@
 import { ThyDialogModule } from 'ngx-tethys/dialog';
 import { ThyStepperModule } from 'ngx-tethys/stepper';
+import { ThyButtonModule } from 'ngx-tethys/button';
 
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 
-import { ThyStepperBasicExampleComponent } from './basic/basic.component';
-import { ThyStepperSwitchDialogExampleComponent } from './switch-dialog/switch-dialog.component';
-
-const COMPONENTS = [ThyStepperBasicExampleComponent, ThyStepperSwitchDialogExampleComponent];
-
-@NgModule({
-    declarations: COMPONENTS,
-    imports: [CommonModule, ThyStepperModule, ThyDialogModule],
-    exports: COMPONENTS,
-    providers: []
-})
-export class ThyStepperExamplesModule {}
+export default {
+    imports: [CommonModule, ThyStepperModule, ThyDialogModule, ThyButtonModule]
+};
