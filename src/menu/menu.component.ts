2f6930edf7fdaa7fb46bf3fbe05f19d949e7395b
MissLixf <1090474924@qq.com>
2022-05-27 14:52:03 +0800

    feat(menu): menu suport divide theme #INFR-3229

--- src/menu/menu.component.ts
+++ src/menu/menu.component.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit, HostBinding, ElementRef } from '@angular/core';
+import { Component, OnInit, HostBinding, ElementRef, Input, Host } from '@angular/core';
 
+export type ThyMenuTheme = 'default' | 'divide';
 @Component({
     selector: 'thy-menu,[thy-menu],[thyMenu]',
     templateUrl: './menu.component.html'
 })
 export class ThyMenuComponent implements OnInit {
     @HostBinding('class.thy-menu') isThyMenu = true;
+
+    @HostBinding('class.menu-theme-divide') menuThemeDivide = false;
+
+    theme: ThyMenuTheme = 'default';
+
+    @Input() set thyTheme(value: ThyMenuTheme) {
+        this.theme = value;
+        if (value === 'divide') {
+            this.menuThemeDivide = true;
+        }
+    }
+
+    get thyTheme() {
+        return this.theme;
+    }
 
     constructor() {}
 
