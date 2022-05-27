54205fbc4ab3f89e65dc3d13f8fe2da79d5333e2
why520crazy <why520crazy@163.com>
2022-05-27 13:10:07 +0800

    docs: update szie desc

--- src/layout/api/zh-cn.js
+++ src/layout/api/zh-cn.js
@@ -85,7 +85,7 @@ module.exports = [
             },
             {
                 name: 'thySize',
-                description: `头部大小，'md' | 'sm' | 'lg'，'md'高度56px，'sm'高度50px，'lg'高度62px`,
+                description: `头部大小，'md' | 'sm' | 'lg'，'md'高度50px，'lg'高度56px，'xlg'高度80px`,
                 type: 'string',
                 default: 'md'
             },
