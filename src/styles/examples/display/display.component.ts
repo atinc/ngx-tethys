3bb0bbd2bcb00bd0978a11e1524eb2c30785e778
why520crazy <why520crazy@users.noreply.github.com>
2022-05-30 10:22:10 +0800

    feat(styles): update spacer to 0-7(4px、8px、12px、16px、20px、24px、28px) and update space component apply to it, refactor styles doc (#1748)

---
+++ src/styles/examples/display/display.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-styles-display-example',
+    templateUrl: './display.component.html'
+})
+export class ThyStylesDisplayExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
---
+++ src/styles/examples/text/text.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-styles-text-example',
+    templateUrl: './text.component.html',
+    styleUrls: ['./text.component.scss']
+})
+export class ThyStylesTextExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
