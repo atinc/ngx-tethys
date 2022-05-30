d0a14777fa22f9187957018af6bf79d105f4c910
aaaaaajie <m18210870360@163.com>
2022-05-30 21:18:04 +0800

    refactor(layout): remove log

--- src/layout/sidebar.component.ts
+++ src/layout/sidebar.component.ts
@@ -145,7 +145,6 @@ export class ThySidebarComponent implements OnInit {
     }
 
     resizeHandler({ width }: ThyResizeEvent) {
-        console.log(width);
         if (width === this.thyLayoutSidebarWidth) {
             return;
         }
