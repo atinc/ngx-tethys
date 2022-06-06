13581c49a7ef5015d0b70004f089b96c6c57ad96
minlovehua <321512939@qq.com>
2022-06-06 16:19:48 +0800

    feat(segment): add segmented module and component #INFR-3073

--- src/segment/segment-item.component.ts
+++ src/segment/segment-item.component.ts
@@ -8,7 +8,9 @@ import {
     Inject,
     Input,
     NgZone,
-    Optional
+    OnDestroy,
+    Optional,
+    Renderer2
 } from '@angular/core';
 import { IThySegmentComponent, THY_SEGMENTED_COMPONENT } from './segment.token';
 import { InputBoolean } from 'ngx-tethys/core';
@@ -27,7 +29,7 @@ import { takeUntil } from 'rxjs/operators';
         class: 'thy-segment-item'
     }
 })
-export class ThySegmentItemComponent<TValue = unknown> implements AfterViewInit {
+export class ThySegmentItemComponent<TValue = unknown> implements AfterViewInit, OnDestroy {
     /**
      * 选项的值
      */
@@ -60,6 +62,7 @@ export class ThySegmentItemComponent<TValue = unknown> implements AfterViewInit 
         public elementRef: ElementRef,
         private ngZone: NgZone,
         private cdr: ChangeDetectorRef,
+        private renderer: Renderer2,
         @Optional() @Inject(THY_SEGMENTED_COMPONENT) private parent: IThySegmentComponent
     ) {
         ngZone.runOutsideAngular(() =>
@@ -77,8 +80,11 @@ export class ThySegmentItemComponent<TValue = unknown> implements AfterViewInit 
     }
 
     ngAfterViewInit(): void {
-        this.isOnlyIcon = assertIconOnly(this.elementRef.nativeElement.children[0]) && this.parent.thyMode === 'inline';
+        const labelDiv = this.elementRef.nativeElement.children[0];
+        this.isOnlyIcon = assertIconOnly(labelDiv) && this.parent.thyMode === 'inline';
         this.cdr.detectChanges();
+
+        this.wrapSpanForText(labelDiv.childNodes);
     }
 
     public select() {
@@ -87,6 +93,17 @@ export class ThySegmentItemComponent<TValue = unknown> implements AfterViewInit 
 
     private unselect() {
         this.elementRef.nativeElement.classList.remove('active');
+    }
+
+    private wrapSpanForText(nodes: NodeList): void {
+        nodes.forEach(node => {
+            if (node.nodeName === '#text') {
+                const span = this.renderer.createElement('span');
+                const parent = this.renderer.parentNode(node);
+                this.renderer.insertBefore(parent, span, node);
+                this.renderer.appendChild(span, node);
+            }
+        });
     }
 
     ngOnDestroy(): void {
