0107a3ff8febf910ab661a9d033e3cfa28b25bd0
why520crazy <why520crazy@163.com>
2022-06-13 13:33:50 +0800

    feat(badge): add thyContent instead of thyContext and update docs and api

---
+++ src/badge/examples/no-wrapper/no-wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-badge-no-wrapper-example',
+    templateUrl: 'no-wrapper.component.html',
+    styleUrls: ['./no-wrapper.component.scss']
+})
+export class ThyBadgeNoWrapperExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
