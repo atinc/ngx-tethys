cd2fe320805fad5de37e5d82e23fcd313f588f1f
why520crazy <why520crazy@163.com>
2022-06-13 17:10:25 +0800

    feat(link): add active class for link and update spacing and size for thy-icon:first-child without link-has-icon #INFR-3481 - add active class for link - update spacing and size for thy-icon:first-child without link-has-icon - refactor docs

---
+++ src/link/examples/active/active.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-link-active-example',
+    templateUrl: './active.component.html',
+    styleUrls: ['./active.component.scss']
+})
+export class ThyLinkActiveExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
