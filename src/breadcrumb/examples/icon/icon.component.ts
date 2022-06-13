2acde1115317d2a8c2e629bff8d5fcabafaf07fa
why520crazy <why520crazy@163.com>
2022-06-13 20:29:36 +0800

    feat(breadcrumb): add vertical-line of thySeparator and update last link hover color to primary #INFR-3474

---
+++ src/breadcrumb/examples/icon/icon.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-breadcrumb-icon-example',
+    templateUrl: './icon.component.html'
+})
+export class ThyBreadcrumbIconExampleComponent {}
---
+++ src/breadcrumb/examples/separator/separator.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-breadcrumb-separator-example',
+    templateUrl: './separator.component.html'
+})
+export class ThyBreadcrumbSeparatorExampleComponent {}
