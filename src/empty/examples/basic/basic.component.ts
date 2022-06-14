042b660ab4486502d760b05f0e09bc3eb6663086
why520crazy <why520crazy@163.com>
2022-06-14 19:09:02 +0800

    feat(empty): update default svg icon #INFR-3509

--- src/empty/examples/basic/basic.component.ts
+++ src/empty/examples/basic/basic.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'thy-empty-basic-example',
-    templateUrl: './basic.component.html'
+    templateUrl: './basic.component.html',
+    styleUrls: ['./basic.component.scss']
 })
 export class ThyEmptyBasicExampleComponent {
     constructor() {}
