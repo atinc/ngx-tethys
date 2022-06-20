35bf07de2e5c7eafb99fbd09f1e7790f28a021df
why520crazy <why520crazy@users.noreply.github.com>
2022-06-20 09:45:08 +0800

    fix(progress): update primary color to $primary #INFR-3563 (#1890)
    BREAKING CHANGES
    In previous versions, the primary color was #73d897 which was close to the success color, you should update thyType="primary" to thyType="success" and set thyType="success" without  thyType's thy-progress

--- src/progress/examples/basic/basic.component.ts
+++ src/progress/examples/basic/basic.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'thy-progress-basic-example',
-    templateUrl: './basic.component.html'
+    templateUrl: './basic.component.html',
+    styleUrls: ['./basic.component.scss']
 })
 export class ThyProgressBasicExampleComponent {
     value = 40;
