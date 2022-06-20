ce0658d681033c92d57a05834d4ce2448a83305b
why520crazy <why520crazy@163.com>
2022-06-20 13:57:23 +0800

    feat(action): add thyHoverIcon support hover switch diff icon and add thyIcon, thyActive instead of thyActionIcon, thyActionActive #INFR-3569 1. add thyHoverIcon support hover switch diff icon 2. add thyIcon, thyActive instead of thyActionIcon, thyActionActive 3. add hover transition styles

--- src/action/action.component.ts
+++ src/action/action.component.ts
@@ -23,16 +23,35 @@ export type ThyActionType = 'primary' | 'success' | 'danger' | 'warning';
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         class: 'thy-action',
-        '[class.active]': 'thyActionActive'
+        '[class.active]': 'active',
+        '[class.thy-action-hover-icon]': 'thyHoverIcon'
     }
 })
 export class ThyActionComponent implements OnInit, AfterViewInit, OnChanges {
     icon: string;
 
+    private active = false;
+
     private type: string = 'primary';
 
     /**
+     * 操作图标类型，默认为'primary'，'primary' | 'success' | 'danger' | 'warning'
+     */
+    @Input()
+    set thyType(value: ThyActionType) {
+        this.setActionType(value || 'primary');
+    }
+
+    /**
      * 操作图标，支持传参同时也支持在投影中写 thy-icon 组件
+     */
+    @Input()
+    set thyIcon(icon: string) {
+        this.icon = icon;
+    }
+
+    /**
+     * 当 thyIcon 和其他指令参数名有冲突时使用 thyActionIcon
      */
     @Input()
     set thyActionIcon(icon: string) {
@@ -44,20 +63,28 @@ export class ThyActionComponent implements OnInit, AfterViewInit, OnChanges {
      */
     @Input()
     @InputBoolean()
-    thyActionActive: string | boolean;
+    set thyActive(value: boolean) {
+        this.active = value;
+    }
 
     /**
-     * 操作图标类型，默认为'primary'，'primary' | 'success' | 'danger' | 'warning'
+     * 当 thyActive 和其他指令参数名有冲突时使用 thyActionActive
      */
     @Input()
-    set thyType(value: ThyActionType) {
-        this.setActionType(value || 'primary');
+    @InputBoolean()
+    set thyActionActive(value: boolean) {
+        this.active = value;
     }
 
     /**
      * 操作图标主题，默认 fill， `fill` 背景色填充，`lite` 简单文本颜色变化
      */
     @Input() thyTheme: 'fill' | 'lite' = 'fill';
+
+    /**
+     * Hover 展示的图标
+     */
+    @Input() thyHoverIcon: string;
 
     constructor(
         private elementRef: ElementRef<HTMLElement>,
