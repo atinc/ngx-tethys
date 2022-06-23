78e0e0831f006e9b6d76723217129ac265fb4caa
why520crazy <why520crazy@163.com>
2022-06-23 17:43:31 +0800

    fix(breadcrumb): update icon color of last child to #999 and link with icon space to 8px #INFR-3638

--- src/breadcrumb/examples/icon/icon.component.ts
+++ src/breadcrumb/examples/icon/icon.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'thy-breadcrumb-icon-example',
-    templateUrl: './icon.component.html'
+    templateUrl: './icon.component.html',
+    styleUrls: ['./icon.component.scss']
 })
 export class ThyBreadcrumbIconExampleComponent {}
--- src/breadcrumb/examples/separator/separator.component.ts
+++ src/breadcrumb/examples/separator/separator.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'thy-breadcrumb-separator-example',
-    templateUrl: './separator.component.html'
+    templateUrl: './separator.component.html',
+    styleUrls: ['./separator.component.scss']
 })
 export class ThyBreadcrumbSeparatorExampleComponent {}
