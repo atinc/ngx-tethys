48ec5127c87c07a166a7e0cb0de84b4e5483f627
why520crazy <why520crazy@163.com>
2022-06-23 17:43:31 +0800

    fix(breadcrumb): update icon color of last child to #999 and link with icon space to 8px #INFR-3638

--- src/breadcrumb/examples/module.ts
+++ src/breadcrumb/examples/module.ts
@@ -1,9 +1,10 @@
 import { ThyBreadcrumbModule } from 'ngx-tethys/breadcrumb';
 import { ThyIconModule } from 'ngx-tethys/icon';
+import { ThyDropdownModule } from 'ngx-tethys/dropdown';
 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 export default {
-    imports: [CommonModule, FormsModule, ThyBreadcrumbModule, ThyIconModule]
+    imports: [CommonModule, FormsModule, ThyBreadcrumbModule, ThyIconModule, ThyDropdownModule]
 };
