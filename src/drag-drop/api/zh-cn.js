a637add4f37dcc663609b4afd1b12216662417b6
xujing0521 <1332570609@qq.com>
2022-07-15 14:00:09 +0800

    docs(drag-drop): add Help documents #INFR-3741

---
+++ src/drag-drop/api/zh-cn.js
@@ -0,0 +1,104 @@
+module.exports = [
+    {
+        type: 'directive',
+        name: 'thyDropContainer',
+        properties: [
+            {
+                name: 'thyDropContainer',
+                type: 'any[]',
+                default: '-',
+                description: '数据'
+            },
+            {
+                name: 'thyDropContainerData',
+                type: 'any[]',
+                default: '-',
+                description: '数据'
+            },
+            {
+                name: 'thyDropContainerDisabled',
+                type: 'boolean',
+                default: 'false',
+                description: '是否禁用拖拽'
+            },
+            {
+                name: 'thyBeforeDragStart',
+                type: 'e: ThyDragStartEvent<T>) => boolean',
+                default: 'false',
+                description: '拖拽之前的回调，函数返回false则阻止拖拽'
+            },
+            {
+                name: 'thyBeforeDragOver',
+                type: '(e: ThyDragOverEvent<T>) => boolean',
+                default: '-',
+                description: '拖拽时回调，函数返回false则阻止移入'
+            },
+            {
+                name: 'thyBeforeDragDrop',
+                type: '(e: ThyDragDropEvent<T>) => boolean',
+                default: '-',
+                description: '拖放到元素时回调，函数返回false则阻止放置'
+            },
+            {
+                name: 'thyDragStarted',
+                type: 'EventEmitter<ThyDragStartEvent<ThyDragDirective>>',
+                default: '-',
+                description: '开始拖拽时调用'
+            },
+            {
+                name: 'thyDragEnded',
+                type: 'EventEmitter<ThyDragEndEvent<ThyDragDirective>>',
+                default: '-',
+                description: 'dragend触发时调用'
+            },
+            {
+                name: 'thyDragOvered',
+                type: 'EventEmitter<ThyDragOverEvent<ThyDragDirective>>',
+                default: '-',
+                description: 'dragover触发时调用'
+            },
+            {
+                name: 'thyDragDropped',
+                type: 'EventEmitter<ThyDragDropEvent<ThyDragDirective>>',
+                default: '-',
+                description: 'drop触发时调用'
+            }
+        ]
+    },
+    {
+        type: 'directive',
+        name: 'thyDrag',
+        properties: [
+            {
+                name: 'thyDrag',
+                type: 'any[]',
+                default: '-',
+                description: '数据'
+            },
+            {
+                name: 'thyDragData',
+                type: 'any[]',
+                default: '-',
+                description: '数据'
+            },
+            {
+                name: 'thyDragDisabled',
+                type: 'boolean',
+                default: 'false',
+                description: '是否禁用拖拽'
+            }
+        ]
+    },
+    {
+        type: 'directive',
+        name: 'thyDragHandle',
+        description: '自定义可拖拽区域内容',
+        properties: []
+    },
+    {
+        type: 'directive',
+        name: 'thyDragContent',
+        description: '自定义拖拽时可经过，拖拽结束时可放置的区域内容',
+        properties: []
+    }
+];
--- src/drag-drop/doc/zh-cn.md
+++ src/drag-drop/doc/zh-cn.md
@@ -2,16 +2,19 @@
 category: other
 title: DragDrop
 subtitle: 拖拽
-label: lack-doc
+label: new
 ---
 
+<alert>用于Tree结构的拖拽，支持拖入拖出更改层级结构，也支持同级的排序。</alert>
 ## CdkDragDrop
 一般情况下我们在业务中遇到拖拽的场景，我们会使用 `Angular Cdk` 中的 `DragDrop` 的解决方案  [CdkDragDrop](https://material.angular.io/cdk/categories)
 >  @angular/cdk/drag-drop 模块为您提供了一种轻松且声明式地创建拖放界面的方法，并支持在其中自由拖动，在列表中进行排序，在列表，动画，触摸设备，自定义拖动手柄，预览和占位符之间传输项目,水平列表和沿轴锁定等功能
 
 <example name="thy-drag-drop-cdk-drag-drop-example" />
 
-## ThyDragDrop
-
-`ThyDragDrop` 主要是用于Tree结构的拖拽，支持拖入拖出更改层级结构，也支持同级的排序
+## 基础使用
 <example name="thy-drag-drop-basic-example" />
+
+## 自定义可拖拽区域内容和可放置区域
+thyDragHandle自定义可拖拽区域，thyDragContent自定义可放置区域
+<example name="thy-drag-drop-with-handle-example" />
