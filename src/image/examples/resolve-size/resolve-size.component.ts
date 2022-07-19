7770cc09d8e4a76021d9865ef833d347126aa034
huanhuanwa <2323666215@qq.com>
2022-07-19 18:06:20 +0800

    docs(image): add resolve size example

---
+++ src/image/examples/resolve-size/resolve-size.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-image-resolve-size-example',
+    templateUrl: './resolve-size.component.html'
+})
+export class ThyImageResolveSizeExampleComponent {
+    imageMeta = {
+        name: 'first.jpg'
+    };
+}
