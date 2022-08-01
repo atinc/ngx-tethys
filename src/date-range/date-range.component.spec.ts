3013cbc716ebceddfaa7733012b6395a86eed9dd
zhangwen <1062680993@qq.com>
2022-08-01 19:18:30 +0800

    fix(date-range): fix date range test #INFR-4051

--- src/date-range/date-range.component.spec.ts
+++ src/date-range/date-range.component.spec.ts
@@ -21,7 +21,7 @@ import {
     addMonths,
     endOfWeek,
     format,
-    isSameDay
+    differenceInDays
 } from 'date-fns';
 
 registerLocaleData(zh);
@@ -154,25 +154,19 @@ describe('ThyTestDateRangeComponent', () => {
 
         it('should support thyDisabledDate', fakeAsync(() => {
             fixture.detectChanges();
-            const compareDate = addDays(startOfMonth(setMonth(CURRENT_DATE, getMonth(CURRENT_DATE) - 2)), -1);
-            let sameDay = false;
-            fixtureInstance.thyDisabledDate = (current: Date) => {
-                sameDay = isSameDay(current, compareDate);
-                return sameDay;
+            fixtureInstance.thyDisabledDate = (date: Date) => {
+                const tooLate = differenceInDays(date, CURRENT_DATE) > 7;
+                const tooEarly = differenceInDays(CURRENT_DATE, date) > 7;
+                return tooEarly || tooLate;
             };
             fixture.detectChanges();
             dispatchClickEvent(getPickerTriggerElement());
             const actionMenuContainers = getPickerContainer().querySelector('.thy-date-range-action-menu-container');
             dispatchClickEvent(actionMenuContainers.lastElementChild as HTMLElement);
-
             const disabledCell = queryFromOverlay(
                 '.thy-calendar-picker-container .thy-calendar-range-left tbody.thy-calendar-tbody td.thy-calendar-disabled-cell'
             );
-            if (sameDay) {
-                expect(disabledCell).not.toBeNull();
-            } else {
-                expect(disabledCell).toBeNull();
-            }
+            expect(disabledCell).not.toBeNull();
         }));
     });
 
