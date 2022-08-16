aa4ad361337935a23fbf35bf41f516e8f326bada
轻语 <243267674@qq.com>
2022-08-16 13:55:15 +0800

    fix(watermark): adjust the var name & add test #INFO-4106 (#2085)

--- src/watermark/examples/basic/basic.component.ts
+++ src/watermark/examples/basic/basic.component.ts
@@ -1,25 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
     selector: 'thy-watermark-basic-example',
     templateUrl: './basic.component.html'
 })
 export class ThyWatermarkBasicExampleComponent implements OnInit {
-    thyDisabled: boolean = false;
-    thyCanvasConfig = {
-        styles: {
-            rotate: 15, // 偏移角度
-            fontSize: '14px', // 水印字体大小
-            color: 'pink', // 字体颜色
-            textAlign: 'left',
-            textBaseline: 'middle'
-        },
-        xSpace: 30, // 水印x轴间隔
-        ySpace: 60, // 水印y轴间隔
-        textLineHeight: 20 // 行高
-    };
-    isChecked: boolean = false;
+    value: string = 'worktile\npingcode';
+    get getValue() {
+        return JSON.stringify(this.value);
+    }
     constructor() {}
-
     ngOnInit() {}
 }
