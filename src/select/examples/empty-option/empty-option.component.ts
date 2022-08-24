ed49db5dd7f066ac9e2008fb41f0a8c2a96677c1
minlovehua <321512939@qq.com>
2022-08-24 09:56:45 +0800

    docs(select): optimize example for select component #INFR-4373

--- src/select/examples/empty-option/empty-option.component.ts
+++ src/select/examples/empty-option/empty-option.component.ts
@@ -1,17 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { listOfOption } from '../mock-data';
 
 @Component({
     selector: 'thy-select-empty-option-example',
     templateUrl: './empty-option.component.html'
 })
 export class ThySelectEmptyOptionExampleComponent implements OnInit {
-    listOfOption: Array<{ label: string; value: string }> = [];
+    listOfOption = listOfOption;
 
-    ngOnInit() {
-        const children: Array<{ label: string; value: string }> = [];
-        for (let i = 10; i < 36; i++) {
-            children.push({ label: i.toString(36) + i, value: i.toString(36) + i });
-        }
-        this.listOfOption = children;
-    }
+    ngOnInit() {}
 }
