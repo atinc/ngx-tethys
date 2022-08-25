e3427458f0889f643bcadf38e04b037cc6333985
walkerkay <walkerkay@163.com>
2022-08-25 18:50:01 +0800

    feat(mention): add isOpened for mention directive #INFR-4414

--- src/mention/examples/module.ts
+++ src/mention/examples/module.ts
@@ -5,7 +5,8 @@ import { ThyMentionModule } from 'ngx-tethys/mention';
 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ThySharedModule } from 'ngx-tethys/shared';
 
 export default {
-    imports: [CommonModule, FormsModule, ThyAvatarModule, ThyMentionModule, ThyInputModule, ThyListModule]
+    imports: [CommonModule, FormsModule, ThyAvatarModule, ThyMentionModule, ThyInputModule, ThyListModule, ThySharedModule]
 };
