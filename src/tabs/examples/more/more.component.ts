11847d4457916b2d248e85b4dfeb727c0cd48e3d
轻语 <243267674@qq.com>
2022-09-26 15:11:21 +0800

    feat(tabs):  add thyResponsive to support 'more' #INFR-4600

---
+++ src/tabs/examples/more/more.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-tabs-more-example',
+    templateUrl: './more.component.html'
+})
+export class ThyTabsMoreExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
