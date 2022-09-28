4bef0a60e586fdf790f2d6611d5125551291846a
yinxiaobin <yxb941006@163.com>
2022-09-28 16:07:37 +0800

    fix(select): update select options position when options data changed (#INFR-4793)

---
+++ src/select/examples/load-async/load-async.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Component({
+    selector: 'thy-select-async-load-data-example',
+    templateUrl: './load-async.component.html'
+})
+export class ThySelectAsyncLoadDataExampleComponent implements OnInit {
+    loadMoreData: Array<{ thyLabelText: string; _id: string }> = [];
+
+    listOfSelectedValue = '00';
+
+    loading = false;
+
+    haveMore = true;
+
+    page = 0;
+
+    loadState = true;
+
+    fetchOptions() {
+        this.loading = true;
+        this.loadState = false;
+        this.loadMoreData = [];
+        return timer(1500).pipe(
+            tap(() => {
+                for (let index = 0; index < 10; index++) {
+                    this.loadMoreData.push({ thyLabelText: `第${this.page + 1}页`, _id: `${this.page}` });
+                }
+
+                this.loadMoreData.forEach((item, index: number) => {
+                    item._id = item._id + index;
+                });
+
+                if (this.page > 3) {
+                    this.haveMore = false;
+                }
+            })
+        );
+    }
+
+    ngOnInit() {
+        this.fetchOptions().subscribe(() => {
+            this.loading = false;
+            this.loadState = true;
+        });
+    }
+
+    expandChange(expand: boolean) {
+        if (expand) {
+            this.fetchOptions().subscribe(() => {
+                this.loading = false;
+                this.loadState = true;
+            });
+        }
+    }
+}
