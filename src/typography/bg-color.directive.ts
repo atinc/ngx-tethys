6a5b79c5e87bff4ed411fd392d66fa2edef34262
ark_65 <liuwufangzhou@vip.qq.com>
2022-09-30 14:07:17 +0800

    feat(typography): add thyBgColor directive #INFR-4780

--- src/typography/bg-color.directive.ts
+++ src/typography/bg-color.directive.ts
@@ -1,33 +1,27 @@
 import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
-import { isBgColor, isThemeColor, ThyBgColor, ThyThemeColor, UpdateHostClassService } from 'ngx-tethys/core';
+import { isBgColor, isThemeColor, ThyBgColor, ThyThemeColor } from 'ngx-tethys/core';
 
 @Directive({
     selector: '[thyBgColor]',
-    exportAs: 'thyBgColor',
-    providers: [UpdateHostClassService]
+    exportAs: 'thyBgColor'
 })
 export class ThyBackgroundColorDirective {
     private bgColor: ThyThemeColor | ThyBgColor | string = '';
-
-    private lastClass: string;
 
     /**
      * 背景颜色，支持设置主题色和自定义颜色值，主题色为 primary success info  danger warning dark secondary light lighter bright content  white transparent
      */
     @Input() set thyBgColor(value: ThyThemeColor | ThyBgColor | string) {
+        this.clearBgColor();
         this.bgColor = value;
         this.setBgColor();
     }
 
-    constructor(private elementRef: ElementRef, private renderer: Renderer2, private updateHostClassService: UpdateHostClassService) {
-        this.updateHostClassService.initializeElement(this.elementRef);
-    }
+    constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
     private setBgColor() {
-        this.clearBgColor();
         if (isThemeColor(this.bgColor) || isBgColor(this.bgColor)) {
-            this.lastClass = `bg-${this.bgColor}`;
-            this.updateHostClassService.addClass(this.lastClass);
+            this.renderer.addClass(this.elementRef.nativeElement, `bg-${this.bgColor}`);
         } else {
             this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', this.bgColor);
         }
@@ -35,9 +29,8 @@ export class ThyBackgroundColorDirective {
 
     private clearBgColor() {
         this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '');
-        if (this.lastClass) {
-            this.updateHostClassService.removeClass(this.lastClass);
-            this.lastClass = null;
+        if (isThemeColor(this.bgColor) || isBgColor(this.bgColor)) {
+            this.renderer.removeClass(this.elementRef.nativeElement, `bg-${this.bgColor}`);
         }
     }
 }
--- src/typography/text-color.directive.ts
+++ src/typography/text-color.directive.ts
@@ -1,45 +1,38 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
-import { isTextColor, isThemeColor, ThyTextColor, ThyThemeColor, UpdateHostClassService } from 'ngx-tethys/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { isTextColor, isThemeColor, ThyTextColor, ThyThemeColor } from 'ngx-tethys/core';
 
 @Directive({
     selector: '[thyTextColor]',
-    exportAs: 'thyTextColor',
-    providers: [UpdateHostClassService]
+    exportAs: 'thyTextColor'
 })
 export class ThyTextColorDirective implements OnInit {
     private color: ThyThemeColor | ThyTextColor | string = '';
-
-    private lastClass: string;
 
     /**
      * 文本颜色，支持设置主题色和自定义颜色值，主题色为 default、primary、success、info、warning、danger、light、secondary、muted、desc、placeholder
      */
     @Input() set thyTextColor(value: ThyThemeColor | ThyTextColor | string) {
+        this.clearColor();
         this.color = value;
         this.setColor();
     }
 
-    constructor(private elementRef: ElementRef, private updateHostClassService: UpdateHostClassService) {
-        this.updateHostClassService.initializeElement(this.elementRef);
-    }
+    constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit(): void {}
 
     private setColor() {
-        this.clearColor();
         if (isThemeColor(this.color) || isTextColor(this.color)) {
-            this.lastClass = `text-${this.color}`;
-            this.updateHostClassService.addClass(this.lastClass);
+            this.renderer.addClass(this.elementRef.nativeElement, `text-${this.color}`);
         } else {
-            this.elementRef.nativeElement.style.color = this.color;
+            this.renderer.setStyle(this.elementRef.nativeElement, 'color', this.color);
         }
     }
 
     private clearColor() {
-        this.elementRef.nativeElement.style.color = '';
-        if (this.lastClass) {
-            this.updateHostClassService.removeClass(this.lastClass);
-            this.lastClass = null;
+        this.renderer.setStyle(this.elementRef.nativeElement, 'color', '');
+        if (isThemeColor(this.color) || isTextColor(this.color)) {
+            this.renderer.removeClass(this.elementRef.nativeElement, `text-${this.color}`);
         }
     }
 }
