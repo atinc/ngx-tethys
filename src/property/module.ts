a82463c760287f3adfec792504c2bbb300ca2635
walkerkay <walkerkay@163.com>
2022-10-18 17:10:33 +0800

    refactor(property): use css grid layout refactor styles #INFR-4636

--- src/property/module.ts
+++ src/property/module.ts
@@ -1,14 +1,13 @@
 import { ThyFlexibleTextModule } from 'ngx-tethys/flexible-text';
-
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { ThyPropertiesComponent } from './properties.component';
 import { ThyPropertyItemComponent } from './property-item.component';
+import { ThyTooltipModule } from 'ngx-tethys/tooltip';
 
 @NgModule({
     declarations: [ThyPropertiesComponent, ThyPropertyItemComponent],
-    imports: [CommonModule, ThyFlexibleTextModule],
+    imports: [CommonModule, ThyFlexibleTextModule, ThyTooltipModule],
     exports: [ThyPropertiesComponent, ThyPropertyItemComponent]
 })
 export class ThyPropertyModule {}
