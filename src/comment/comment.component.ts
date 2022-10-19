d716dabb1e7476fc768e8664ff8e5bb9762d4251
leonbeau <leon@hooc.top>
2022-10-19 17:10:42 +0800

    refactor(comment): automatic document generation #INFR-4936

--- src/comment/comment.component.ts
+++ src/comment/comment.component.ts
@@ -8,10 +8,19 @@ import { ChangeDetectionStrategy, Component, Input, TemplateRef } from '@angular
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThyCommentComponent {
+    /**
+     * 展示评论作者
+     */
     @Input() thyAuthor?: string | TemplateRef<void>;
 
+    /**
+     * 展示评论作者头像
+     */
     @Input() thyDatetime?: string | TemplateRef<void>;
 
+    /**
+     * 展示评论时间
+     */
     @Input() thyAvatar?: string | TemplateRef<void>;
 
     constructor() {}
