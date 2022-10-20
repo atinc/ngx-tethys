85b2090c2fd095af4ce63d4ac34f5227d77d4d7e
leonbeau <leon@hooc.top>
2022-10-20 17:17:02 +0800

    fix(comment): the user can control the display behavior #INFR-4936

--- src/comment/comment-actions.directive.ts
+++ src/comment/comment-actions.directive.ts
@@ -6,6 +6,6 @@ import { Directive } from '@angular/core';
 @Directive({
     selector: '[thyCommentActions]',
     exportAs: 'thyCommentActions',
-    host: { class: 'thy-comment-actions-default' }
+    host: { class: 'thy-comment-actions' }
 })
 export class ThyCommentActionsDirective {}
--- src/comment/comment.component.ts
+++ src/comment/comment.component.ts
@@ -1,15 +1,19 @@
 import { isTemplateRef } from 'ngx-tethys/util';
 import { SafeAny } from './../types/common';
 import { ChangeDetectionStrategy, Component, Input, TemplateRef, OnInit } from '@angular/core';
+import { InputBoolean } from 'ngx-tethys/core';
 @Component({
     selector: 'thy-comment',
     templateUrl: './comment.component.html',
     host: {
-        class: 'thy-comment'
+        class: 'thy-comment',
+        '[class.active]': 'active'
     },
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThyCommentComponent {
+    private active = false;
+
     /**
      * 展示评论作者
      */
@@ -24,6 +28,15 @@ export class ThyCommentComponent {
      * 展示评论作者头像
      */
     @Input() thyAvatar?: string | TemplateRef<SafeAny>;
+
+    /**
+     * 评论 Actions 的 Active 状态，默认为 false，设置为 true 时会在 Comment 上添加 active class
+     */
+    @Input()
+    @InputBoolean()
+    set thyCommentActive(value: boolean) {
+        this.active = value;
+    }
 
     isTemplateRef = isTemplateRef;
 }
