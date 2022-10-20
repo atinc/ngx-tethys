a7c62ab9254cb0464be147677784c18b9c15d947
leonbeau <leon@hooc.top>
2022-10-20 11:22:21 +0800

    refactor(comment): code optimization #INFR-4936

--- src/comment/comment.component.ts
+++ src/comment/comment.component.ts
@@ -9,31 +9,21 @@ import { ChangeDetectionStrategy, Component, Input, TemplateRef, OnInit } from '
     },
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ThyCommentComponent implements OnInit {
+export class ThyCommentComponent {
     /**
      * 展示评论作者
      */
     @Input() thyAuthor?: string | TemplateRef<SafeAny>;
 
     /**
-     * 展示评论作者头像
+     * 展示评论时间
      */
     @Input() thyDatetime?: string | TemplateRef<SafeAny>;
 
     /**
-     * 展示评论时间
+     * 展示评论作者头像
      */
     @Input() thyAvatar?: string | TemplateRef<SafeAny>;
 
-    isAuthorTemplateRef = false;
-
-    isDatetimeTemplateRef = false;
-
-    isAvatarTemplateRef = false;
-
-    ngOnInit(): void {
-        if (isTemplateRef(this.thyAuthor)) this.isAuthorTemplateRef = true;
-        if (isTemplateRef(this.thyDatetime)) this.isDatetimeTemplateRef = true;
-        if (isTemplateRef(this.thyAvatar)) this.isAvatarTemplateRef = true;
-    }
+    isTemplateRef = isTemplateRef;
 }
