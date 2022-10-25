596d349c35a18daff2a6f81022b78445630142fc
huanhuanwa <2323666215@qq.com>
2022-10-25 15:16:32 +0800

    docs(result): optmize code #INFR-5015

--- src/result/result.component.ts
+++ src/result/result.component.ts
@@ -8,49 +8,52 @@ type ThyResultStatus = 'success' | 'warning' | 'error';
 })
 export class ThyResultComponent implements OnInit {
     /**
-     * @description 结果的状态，决定图标和颜色
+     * @description 结果的状态，决定显示的图标
      * @type ThyResultStatus
      */
     @Input() thyStatus: ThyResultStatus;
 
     /**
+     * @description 自定义 icon，作为 img 的 src 显示
+     * @type string
+     */
+    @Input() thyIcon: string;
+
+    /**
      * @description 标题
-     * @type TemplateRef<any> ｜ string
+     * @type string
      */
     @Input() thyTitle: string;
 
     /**
      * @description 二级标题
-     * @type TemplateRef<any> ｜ string
+     * @type string
      */
     @Input() thySubtitle: string;
 
     /**
-     * @description 自定义 icon
-     * @type TemplateRef<any> ｜ string
-     */
-    @Input() thyIcon: string;
-
-    /**
-     * @internal
+     * @description 自定义 icon 模板
+     * @type TemplateRef<any>
      */
     @ContentChild('thyIcon') iconTemplateRef: TemplateRef<any>;
 
     /**
-     * @description 自定义操作区域
-     * @type TemplateRef<void>
-     */
-    @ContentChild('thyExtra') extraTemplateRef: TemplateRef<any>;
-
-    /**
-     * @internal
+     * @description 自定义标题模板
+     * @type TemplateRef<any>
      */
     @ContentChild('thyTitle') titleTemplateRef: TemplateRef<any>;
 
     /**
-     * @internal
+     * @description 自定义二级标题模板
+     * @type TemplateRef<any>
      */
     @ContentChild('thySubtitle') subtitleTemplateRef: TemplateRef<any>;
+
+    /**
+     * @description 自定义操作区域
+     * @type TemplateRef<any>
+     */
+    @ContentChild('thyExtra') extraTemplateRef: TemplateRef<any>;
 
     @HostBinding('class.thy-result') className = true;
 
