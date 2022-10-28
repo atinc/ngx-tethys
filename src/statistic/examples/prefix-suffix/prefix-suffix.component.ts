b4ea55f8935b4331e54a82bd61ae7afd2d35bab4
huanhuanwa <2323666215@qq.com>
2022-10-28 14:51:06 +0800

    docs(statistic): fix demo build error #INFR-5014

--- src/statistic/examples/prefix-suffix/prefix-suffix.component.ts
+++ src/statistic/examples/prefix-suffix/prefix-suffix.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'thy-prefix-suffix-example',
     templateUrl: './prefix-suffix.component.html',
-    styleUrls: ['./prefix-suffix.component.css']
+    styleUrls: ['./prefix-suffix.component.scss']
 })
 export class ThyStatisticPrefixAndSuffixExampleComponent implements OnInit {
     constructor() {}
