0d9885ea146319029dee2cb4cc74ab0180ba2ef0
why520crazy <why520crazy@users.noreply.github.com>
2022-11-09 17:03:48 +0800

    feat(input): add thy-input-count statistical input length #INFR-5028 (#2315)

---
+++ src/input/examples/input-count/input-count.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-input-count-example',
+    templateUrl: './input-count.component.html',
+    styleUrls: ['./input-count.component.scss']
+})
+export class ThyInputCountExampleComponent implements OnInit {
+    public name = '';
+
+    constructor() {}
+
+    ngOnInit() {}
+}
---
+++ src/input/input-count.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, ChangeDetectorRef, Optional } from '@angular/core';
+import { NgModel, NgControl } from '@angular/forms';
+import { ThyInputDirective } from './input.directive';
+import { mixinUnsubscribe, MixinBase } from 'ngx-tethys/core';
+import { takeUntil, switchMap, filter, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { ThyInputGroupComponent } from './input-group.component';
+
+const _Base = mixinUnsubscribe(MixinBase);
+
+/**
+ * 输入框输入文字展示
+ * @name thy-input-count
+ * @order 40
+ */
+@Component({
+    selector: 'thy-input-count',
+    template: '{{inputLength}} / {{maxLength}}',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        class: 'text-muted'
+    }
+})
+export class ThyInputCountComponent extends _Base implements OnInit {
+    private hasInput = false;
+
+    /**
+     * 输入框组件，如果不传默认会读取外层 thy-input-group 下的 thyInput 指令
+     * @type ThyInputDirective
+     */
+    @Input() set thyInput(value: ThyInputDirective) {
+        this.hasInput = true;
+        this.thyInput$.next(value);
+    }
+
+    maxLength: number | string;
+
+    inputLength = 0;
+
+    thyInput$ = new Subject<ThyInputDirective>();
+
+    constructor(private changeDetectorRef: ChangeDetectorRef, @Optional() private inputGroup: ThyInputGroupComponent) {
+        super();
+        this.setup();
+    }
+
+    setup() {
+        this.thyInput$
+            .pipe(
+                filter(input => {
+                    return !!input;
+                }),
+                tap(input => {
+                    this.maxLength = input.nativeElement.getAttribute('maxlength');
+                    this.changeDetectorRef.markForCheck();
+                }),
+                takeUntil(this.ngUnsubscribe$),
+                switchMap(input => {
+                    return input.ngControl.valueChanges;
+                }),
+                tap(value => {
+                    this.inputLength = value?.length;
+                    this.changeDetectorRef.markForCheck();
+                }),
+                takeUntil(this.ngUnsubscribe$)
+            )
+            .subscribe();
+    }
+
+    ngOnInit(): void {
+        if (!this.hasInput && this.inputGroup && this.inputGroup.inputDirective) {
+            this.thyInput$.next(this.inputGroup.inputDirective);
+        }
+    }
+}
