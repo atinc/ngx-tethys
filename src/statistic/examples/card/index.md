ff8957527d8a721a615756c7e735cb9bc7878785
huanhuanwa <44698191+huanhuanwa@users.noreply.github.com>
2022-11-09 13:51:46 +0800

    docs(statistic): add statistic doc and examples  #INFR-5014 (#2293)

---
+++ src/statistic/examples/card/index.md
@@ -0,0 +1,4 @@
+---
+order: 30
+title: Card
+---
--- src/statistic/examples/module.ts
+++ src/statistic/examples/module.ts
@@ -1,17 +1,7 @@
 import { ThyIconModule } from 'ngx-tethys/icon';
 import { ThyStatisticModule } from 'ngx-tethys/statistic';
-
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 
-import { ThyStatisticBasicExampleComponent } from './basic/basic.component';
-import { ThyStatisticCardExampleComponent } from './card/card.component';
-
-const COMPONENTS: any[] = [ThyStatisticBasicExampleComponent, ThyStatisticCardExampleComponent];
-
-@NgModule({
-    declarations: [...COMPONENTS],
-    imports: [CommonModule, ThyStatisticModule, ThyIconModule],
-    exports: [...COMPONENTS]
-})
-export class ThyStatisticExamplesModule {}
+export default {
+    imports: [CommonModule, ThyStatisticModule, ThyIconModule]
+};
