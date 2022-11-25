646c8d5592dd55163235dca65659c750406cdde4
why520crazy <why520crazy@163.com>
2022-11-25 11:53:33 +0800

    docs(nav): refactor nav size example

--- src/nav/examples/size/size.component.ts
+++ src/nav/examples/size/size.component.ts
@@ -2,9 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'thy-nav-size-example',
-    templateUrl: './size.component.html'
+    templateUrl: './size.component.html',
+    styleUrls: ['./size.component.scss']
 })
 export class ThyNavSizeExampleComponent implements OnInit {
+    sizes = [
+        {
+            value: 'sm',
+            text: 'sm'
+        },
+        {
+            value: 'md',
+            text: 'md'
+        },
+        {
+            value: 'lg',
+            text: 'lg'
+        }
+    ];
+
+    size = this.sizes[1];
+
     constructor() {}
 
     ngOnInit(): void {}
