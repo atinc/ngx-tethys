7ebdde2d482699d4da037687c7459534491a5e73
minlovehua <321512939@qq.com>
2022-12-04 22:33:03 +0800

    docs(grid): add overview, examples and api docs #INFR-5322

---
+++ src/grid/examples/use-style-value/use-style-value.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-grid-use-style-value-example',
+    templateUrl: './use-style-value.component.html',
+    styleUrls: ['./use-style-value.component.scss']
+})
+export class ThyGridUseStyleValueExampleComponent {}
