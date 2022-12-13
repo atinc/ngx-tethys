4f86d7f0cb813c9f9be3a9a9cdf3344d174ca3a4
leonbeau <unknown>
2022-12-13 16:25:52 +0800

    chore(arrow-switcher): code optimization #INFR-5735

--- src/arrow-switcher/examples/module.ts
+++ src/arrow-switcher/examples/module.ts
@@ -6,14 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { ThyArrowSwitcherSizeExampleComponent } from './size/size.component';
 import { ThyArrowSwitcherDisabledExampleComponent } from './disabled/disabled.component';
 import { ThyArrowSwitcherLiteExampleComponent } from './lite/lite.component';
-import { ThyArrowSwitcherTooltipExampleComponent } from './tooltip/tooltip.component';
 
 const COMPONENTS = [
     ThyArrowSwitcherBasicExampleComponent,
     ThyArrowSwitcherLiteExampleComponent,
     ThyArrowSwitcherSizeExampleComponent,
-    ThyArrowSwitcherDisabledExampleComponent,
-    ThyArrowSwitcherTooltipExampleComponent
+    ThyArrowSwitcherDisabledExampleComponent
 ];
 @NgModule({
     declarations: [...COMPONENTS],
