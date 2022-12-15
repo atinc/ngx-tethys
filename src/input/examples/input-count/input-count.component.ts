19001309f767751f3d5cc87541c3907a5a20907e
xujing0521 <1332570609@qq.com>
2022-12-15 19:28:41 +0800

    fix(input): thy-input-count 初始化没有赋值设置为 0，验证失败提示信息校验跑版，禁用样式跑版 #INFR-5574 (#2435)

--- src/input/examples/input-count/input-count.component.ts
+++ src/input/examples/input-count/input-count.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ThyPlacement } from 'ngx-tethys/core';
+import { THY_DROPDOWN_DEFAULT_WIDTH } from 'ngx-tethys/dropdown';
 
 @Component({
     selector: 'thy-input-count-example',
@@ -6,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./input-count.component.scss']
 })
 export class ThyInputCountExampleComponent implements OnInit {
-    public name = '';
-
     constructor() {}
 
     ngOnInit() {}
+
+    model = {
+        username: 'Alice',
+        nickname: ''
+    };
+
+    saving = false;
+
+    login() {
+        if (this.saving) {
+            return;
+        }
+        this.saving = true;
+        setTimeout(() => {
+            this.saving = false;
+        }, 2000);
+    }
+
+    cancel() {
+        this.model = {
+            username: 'Alice',
+            nickname: ''
+        };
+    }
 }
