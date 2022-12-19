53386e29b663b31051120cce64e7deec9ebb08d2
walkerkay <walkerkay@163.com>
2022-12-19 19:50:25 +0800

    fix(cdk): remove hotkey conflict logic and don't call hotkey event when active element is form control

--- cdk/hotkey/test/hotkey-dispatcher.spec.ts
+++ cdk/hotkey/test/hotkey-dispatcher.spec.ts
@@ -5,6 +5,7 @@ import { ThyHotkeyDispatcher } from '../hotkey-dispatcher';
 const controlEnterCode = 'Control+Enter';
 const metaEnterCode = 'Meta+Enter';
 const metaEnterCodeEvent = createKeyboardEvent('keydown', null, 'Enter', { meta: true });
+const controlEnterCodeEvent = createKeyboardEvent('keydown', null, 'Enter', { control: true });
 
 describe('HotkeyDispatcher', () => {
     beforeEach(() => {
@@ -46,21 +47,46 @@ describe('HotkeyDispatcher', () => {
             expect(spy).toHaveBeenCalledTimes(1);
         }));
 
-        it('should call last hotkey event when same hotkey code subscribed', fakeAsync(() => {
-            const firstSpy = jasmine.createSpy('first hotkey callback');
-            const secondSpy = jasmine.createSpy('second hotkey callback');
-            hotkeyDispatcher.keydown([metaEnterCode, controlEnterCode]).subscribe(firstSpy);
-            const subscription = hotkeyDispatcher.keydown([metaEnterCode, 'Meta+i']).subscribe(secondSpy);
+        it(`should be don't call hotkey callback with active element is form control`, fakeAsync(() => {
+            // create form control
+            const input = document.createElement('input');
+            input.type = 'text';
+            document.body.appendChild(input);
+            // bind hotkey to document
+            const spy1 = jasmine.createSpy('hotkey callback');
+            hotkeyDispatcher.keydown(metaEnterCode).subscribe(spy1);
+            input.focus();
             document.dispatchEvent(metaEnterCodeEvent);
             tick();
-            expect(firstSpy).not.toHaveBeenCalled();
-            expect(secondSpy).toHaveBeenCalledTimes(1);
+            expect(spy1).not.toHaveBeenCalled();
+            input.blur();
+            document.dispatchEvent(metaEnterCodeEvent);
+            tick();
+            expect(spy1).toHaveBeenCalledTimes(1);
 
-            subscription.unsubscribe();
-            document.dispatchEvent(metaEnterCodeEvent);
+            // bind hotkey to input
+            const spy2 = jasmine.createSpy('hotkey callback');
+            hotkeyDispatcher.keydown(controlEnterCode, input).subscribe(spy2);
+            input.dispatchEvent(controlEnterCodeEvent);
             tick();
-            expect(firstSpy).toHaveBeenCalledTimes(1);
-            expect(secondSpy).toHaveBeenCalledTimes(1);
+            expect(spy2).toHaveBeenCalledTimes(1);
         }));
+
+        // it('should call last hotkey event when same hotkey code subscribed', fakeAsync(() => {
+        //     const firstSpy = jasmine.createSpy('first hotkey callback');
+        //     const secondSpy = jasmine.createSpy('second hotkey callback');
+        //     hotkeyDispatcher.keydown([metaEnterCode, controlEnterCode]).subscribe(firstSpy);
+        //     const subscription = hotkeyDispatcher.keydown([metaEnterCode, 'Meta+i']).subscribe(secondSpy);
+        //     document.dispatchEvent(metaEnterCodeEvent);
+        //     tick();
+        //     expect(firstSpy).not.toHaveBeenCalled();
+        //     expect(secondSpy).toHaveBeenCalledTimes(1);
+
+        //     subscription.unsubscribe();
+        //     document.dispatchEvent(metaEnterCodeEvent);
+        //     tick();
+        //     expect(firstSpy).toHaveBeenCalledTimes(1);
+        //     expect(secondSpy).toHaveBeenCalledTimes(1);
+        // }));
     });
 });
