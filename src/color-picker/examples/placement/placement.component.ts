84475bf369ac5e10361e5628eb46fc34cb0efa44
aaaaaajie <m18210870360@163.com>
2023-04-27 10:29:15 +0800

    feat(color-picker): support placement params #INFR-7526

---
+++ src/color-picker/examples/placement/placement.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-placement-example',
+    templateUrl: './placement.component.html',
+    styles: [
+        `
+            .box {
+                width: 30px;
+                height: 30px;
+                margin-top: 12px;
+                cursor: pointer;
+            }
+        `
+    ]
+})
+export class ThyPlacementExampleComponent {
+    color = '#6698FF';
+
+    change(color: string) {
+        console.log(color);
+    }
+}
