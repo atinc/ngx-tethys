4031642e7c3c910fa2af3610cf2da190e67c789a
minlovehua <321512939@qq.com>
2023-05-13 16:07:45 +0800

    docs(table): optimize api documentation #INFR-6963

--- src/dropdown/dropdown-active.directive.ts
+++ src/dropdown/dropdown-active.directive.ts
@@ -7,6 +7,7 @@ import { useHostRenderer } from '@tethys/cdk/dom';
 /**
  * 跟踪 Dropdown 菜单是否被打开处于激活状态，允许指定一个或多个CSS类，以便在菜单打开状态时添加到元素中
  * @name thyDropdownActive
+ * @order 60
  */
 @Directive({
     selector: '[thyDropdownActive]',
