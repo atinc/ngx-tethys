62edb3633e73032d2ce378a87bf45fe3abd88842
Walker <15701592+walkerkay@users.noreply.github.com>
2023-06-08 13:59:40 +0800

    feat(action): supports  success and error feedback  #INFR-8234 (#2706)

---
+++ src/action/examples/feedback/feedback.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'thy-action-feedback-example',
+    templateUrl: './feedback.component.html',
+    styleUrls: ['./feedback.component.scss']
+})
+export class ThyActionFeedbackExampleComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
--- src/action/examples/module.ts
+++ src/action/examples/module.ts
@@ -3,7 +3,8 @@ import { ThySpaceModule } from 'ngx-tethys/space';
 import { ThyIconModule } from 'ngx-tethys/icon';
 import { ThyActionModule } from 'ngx-tethys/action';
 import { CommonModule } from '@angular/common';
+import { ThyTooltipModule } from 'ngx-tethys/tooltip';
 
 export default {
-    imports: [CommonModule, ThyActionModule, ThyButtonModule, ThyIconModule, ThySpaceModule]
+    imports: [CommonModule, ThyActionModule, ThyButtonModule, ThyIconModule, ThySpaceModule, ThyTooltipModule]
 };
