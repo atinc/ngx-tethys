ccc8eee031decc8c199d63c939116b669b9828f8
yinxiaobin <yxb941006@163.com>
2023-08-15 15:02:24 +0800

    build: modify examples

--- src/dropdown/examples/immediate-render/immediate-render.component.ts
+++ src/dropdown/examples/immediate-render/immediate-render.component.ts
@@ -1,6 +1,4 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, NgZone, ViewChildren, QueryList } from '@angular/core';
-import { take } from 'rxjs/operators';
-import { ThyDropdownDirective } from 'ngx-tethys/dropdown';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ImmediateMenuComponent } from './immediate-menu.component';
 
 @Component({
