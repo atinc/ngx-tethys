c71f0fd8feda4856f1d151dd24ba3c79557f74f0
minlovehua <321512939@qq.com>
2023-08-16 17:41:52 +0800

    feat(input): thy-input-group support textarea suffix INFR-9159

--- src/input/input-group.component.ts
+++ src/input/input-group.component.ts
@@ -9,14 +9,17 @@ import {
     AfterContentChecked,
     OnInit,
     ElementRef,
-    OnDestroy
+    OnDestroy,
+    ChangeDetectorRef,
+    NgZone
 } from '@angular/core';
 import { MixinBase, ThyTranslate, mixinUnsubscribe } from 'ngx-tethys/core';
 import { useHostRenderer } from '@tethys/cdk/dom';
 import { ThyInputDirective } from './input.directive';
 import { NgIf, NgTemplateOutlet } from '@angular/common';
 import { FocusMonitor } from '@angular/cdk/a11y';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, throttleTime } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 export type InputGroupSize = 'sm' | 'lg' | 'md' | '';
 
@@ -40,7 +43,9 @@ const inputGroupSizeMap = {
         class: 'thy-input-group',
         '[class.form-control]': 'prefixTemplate || suffixTemplate',
         '[class.thy-input-group-with-prefix]': 'prefixTemplate',
-        '[class.thy-input-group-with-suffix]': 'suffixTemplate'
+        '[class.thy-input-group-with-suffix]': 'suffixTemplate',
+        '[class.thy-input-group-with-textarea-suffix]': 'isTextareaSuffix',
+        '[class.thy-input-group-with-scroll-bar]': 'isTextareaSuffix && hasScrollbar'
     },
     standalone: true,
     imports: [NgIf, NgTemplateOutlet]
@@ -51,6 +56,10 @@ export class ThyInputGroupComponent extends mixinUnsubscribe(MixinBase) implemen
     public appendText: string;
 
     public prependText: string;
+
+    public isTextareaSuffix: boolean;
+
+    public hasScrollbar: boolean;
 
     @HostBinding('class.disabled') disabled = false;
 
@@ -129,7 +138,13 @@ export class ThyInputGroupComponent extends mixinUnsubscribe(MixinBase) implemen
      */
     @ContentChild(ThyInputDirective) inputDirective: ThyInputDirective;
 
-    constructor(private thyTranslate: ThyTranslate, private elementRef: ElementRef, private focusMonitor: FocusMonitor) {
+    constructor(
+        private thyTranslate: ThyTranslate,
+        private elementRef: ElementRef,
+        private focusMonitor: FocusMonitor,
+        private ngZone: NgZone,
+        private cdr: ChangeDetectorRef
+    ) {
         super();
     }
 
@@ -148,6 +163,41 @@ export class ThyInputGroupComponent extends mixinUnsubscribe(MixinBase) implemen
 
     ngAfterContentChecked(): void {
         this.disabled = !!this.inputDirective?.nativeElement?.hasAttribute('disabled');
+
+        this.isTextareaSuffix = this.inputDirective?.nativeElement?.tagName === 'TEXTAREA';
+        if (this.isTextareaSuffix) {
+            this.determineHasScrollbar();
+        }
+    }
+
+    private determineHasScrollbar() {
+        this.ngZone.runOutsideAngular(() => {
+            this.resizeObserver(this.inputDirective.nativeElement)
+                .pipe(throttleTime(100), takeUntil(this.ngUnsubscribe$))
+                .subscribe(() => {
+                    const hasScrollbar = this.inputDirective.nativeElement.scrollHeight > this.inputDirective.nativeElement.clientHeight;
+                    if (this.hasScrollbar !== hasScrollbar) {
+                        this.ngZone.run(() => {
+                            this.hasScrollbar = hasScrollbar;
+                            this.cdr.detectChanges();
+                        });
+                    }
+                });
+        });
+    }
+
+    private resizeObserver(element: HTMLElement): Observable<ResizeObserverEntry[]> {
+        return typeof ResizeObserver === 'undefined' || !ResizeObserver
+            ? of(null)
+            : new Observable(observer => {
+                  const resize = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+                      observer.next(entries);
+                  });
+                  resize.observe(element);
+                  return () => {
+                      resize.disconnect();
+                  };
+              });
     }
 
     ngOnDestroy() {
