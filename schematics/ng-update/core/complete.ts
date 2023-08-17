3e5f5be031f30773e34a7b3881ff20e0eccd0025
Guoxiin <guoxiin@163.com>
2023-08-17 11:59:52 +0800

    feat(schematics): update tips

--- schematics/ng-update/core/complete.ts
+++ schematics/ng-update/core/complete.ts
@@ -23,7 +23,7 @@ export function onV16MigrationComplete(context: SchematicContext, targetVersion:
 
     if (hasFailures) {
         context.logger.warn(
-            '  ⚠  Some issues were detected but could not be fixed automatically. Please check the output above against documentation, fix these issues manually. Especially check the replace input with variable value in template, if it has a corresponding useless variable value, delete it manually.'
+            '  ⚠  Some issues were detected but could not be fixed automatically. Please check the output above against documentation, fix these issues manually. Especially check the replace input with variable value in template, if it has a corresponding useless variable value, delete it manually. Finally prettier your code please.'
         );
     }
 }
