561b916362968d6b9609e68baeb4467ac23c008e
尹晓斌 <yxb941006@163.com>
2023-09-19 13:43:59 +0800

    feat(input-number): #INFR-7140 restrict decimal places input  (#2841)

--- src/input-number/test/input-number.spec.ts
+++ src/input-number/test/input-number.spec.ts
@@ -336,6 +336,19 @@ describe('input-number component', () => {
         });
     }));
 
+    it('should thyPrecision work when keyup', fakeAsync(() => {
+        fixture.detectChanges();
+        inputNumberComponentInstance.modelValue = '0.991';
+        fixture.detectChanges();
+        tick();
+        dispatchKeyboardEvent(inputElement, 'keyup');
+        fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
+        expect(inputElement.value).toBe('0.99');
+        flush();
+    }));
+
     it('should thyMax and thyMin work', () => {
         fixture.detectChanges();
         inputNumberComponentInstance.inputNumberComponent.onModelChange('11');
