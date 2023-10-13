d096c9133353878eaed274a45398383d8e7e275b
尹晓斌 <yxb941006@163.com>
2023-10-13 11:31:03 +0800

    Revert "build: release 16.2.0-next" (#2852)

--- schematics/version.ts
+++ schematics/version.ts
@@ -1,1 +1,1 @@
-export const VERSION = '16.2.0-next';
+export const VERSION = '16.1.5';
--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 import { Version } from '@angular/core';
 
-export const VERSION = new Version('16.2.0-next');
+export const VERSION = new Version('16.1.5');
