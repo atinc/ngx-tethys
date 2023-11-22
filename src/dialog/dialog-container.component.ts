a20f7b4b4d85c56c407c825dd7c0f13c5773e643
Guoxiin <guoxiin@163.com>
2023-11-22 17:01:28 +0800

    feat(dialog): thy-dialog-container animation to enter add markForCheck #INFR-10586

--- src/dialog/dialog-container.component.ts
+++ src/dialog/dialog-container.component.ts
@@ -141,13 +141,13 @@ export class ThyDialogContainerComponent extends ThyAbstractOverlayContainer imp
         this.clickPositioner.runTaskUseLastPosition(lastPosition => {
             if (lastPosition) {
                 const containerElement: HTMLElement = this.elementRef.nativeElement;
-                const transformOrigin = `${lastPosition.x - containerElement.offsetLeft}px ${
-                    lastPosition.y - containerElement.offsetTop
-                }px 0px`;
+                const transformOrigin = `${lastPosition.x - containerElement.offsetLeft}px ${lastPosition.y - containerElement.offsetTop
+                    }px 0px`;
                 containerElement.style['transform-origin'] = transformOrigin;
                 // 手动修改动画状态为从 void 到 enter, 开启动画
             }
             this.animationState = 'enter';
+            this.changeDetectorRef.markForCheck();
         });
     }
 
