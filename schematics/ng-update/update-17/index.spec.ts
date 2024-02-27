36321d609346e4d7255dc19cdefcadafcab319c2
minlovehua <321512939@qq.com>
2024-02-27 18:45:40 +0800

    test(schematics): should provide schematics for v17 #INFR-11686

--- schematics/ng-update/update-17/index.spec.ts
+++ schematics/ng-update/update-17/index.spec.ts
@@ -1,4 +1,4 @@
-import { Tree } from '@angular-devkit/schematics';
+import { HostTree, Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import { createTestWorkspaceFactory } from '../../testing';
 
@@ -22,5 +22,79 @@ describe('ng-update v17 Schematic', () => {
         expect(file.content.toString()).toBeTruthy();
         const packageJSON = JSON.parse(file.content.toString());
         expect(packageJSON['dependencies']['@angular/core']).toContain('^17.');
+    });
+
+    describe('should provide schematics for v17', () => {
+        let tree: Tree;
+        const schematicRunner = new SchematicTestRunner('migrations', require.resolve('../migration-collection.json'));
+
+        const TEST_COMPONENT_PATH = '/projects/update17test/src/app/app.component.ts';
+        const TEST_COMPONENT_CONTENT = `
+                import { ThyButtonComponent } from 'ngx-tethys/button';
+                import { ThyOptionComponent } from 'ngx-tethys/shared';
+                import { ThySelectComponent, ThySelectCustomComponent } from 'ngx-tethys/select';
+
+                @Component({
+                    selector: 'app-root',
+                    standalone: true,
+                    imports: [
+                        CommonModule,
+                        FormsModule,
+                        RouterOutlet,
+                        ThySelectComponent,
+                        ThySelectCustomComponent,
+                        ThyOptionComponent,
+                        ThyButtonComponent
+                    ],
+                    templateUrl: './app.component.html',
+                    styleUrls: ['./app.component.scss'],
+                })
+                export class AppComponent {}
+        `;
+
+        let workspaceTree: UnitTestTree;
+
+        beforeEach(async () => {
+            const hostTree = new HostTree();
+
+            tree = await schematicRunner.runExternalSchematic(
+                '@schematics/angular',
+                'workspace',
+                {
+                    name: 'test-workspace',
+                    version: '17.0.0',
+                    newProjectRoot: 'projects'
+                },
+                hostTree
+            );
+
+            await schematicRunner.runExternalSchematic('@schematics/angular', 'application', { name: 'update17test' }, tree);
+
+            tree.overwrite(TEST_COMPONENT_PATH, TEST_COMPONENT_CONTENT);
+
+            workspaceTree = await schematicRunner.runSchematic('migration-v17', {}, tree);
+        });
+
+        it('should remove the Component suffix from standalone components', async () => {
+            const result = workspaceTree.read(TEST_COMPONENT_PATH).toString();
+
+            expect(result).not.toContain(`ThyOptionComponent`);
+            expect(result).toContain('ThyOption');
+
+            expect(result).not.toContain(`ThyButtonComponent`);
+            expect(result).toContain('ThyButton');
+        });
+
+        it('should rename ThySelectCustomComponent to ThySelect', async () => {
+            const result = workspaceTree.read(TEST_COMPONENT_PATH).toString();
+            expect(result).not.toContain('ThySelectComponent');
+            expect(result).toContain('ThyNativeSelect');
+        });
+
+        it('should rename ThySelectComponent to ThyNativeSelect', async () => {
+            const result = workspaceTree.read(TEST_COMPONENT_PATH).toString();
+            expect(result).not.toContain(`ThySelectCustomComponent`);
+            expect(result).toContain('ThySelect');
+        });
     });
 });
