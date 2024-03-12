f55cd08a9404ecbfee1e040dd8103e48a91bcef6
xujing0521 <1332570609@qq.com>
2024-03-12 15:03:52 +0800

    build: fix  tips

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -7,15 +7,13 @@ const version = args[2];
 if (version.includes('next') && !command.includes('next')) {
     console.log(
         chalk.bgRed.black('[ERROR]'),
-        chalk.red(`✘ Publish the next version, please use`),
-        chalk.blue('npm run release-next')
+        chalk.red(`✘ Publish the next version, please use ${chalk.blue('npm run release-next')}`),
     );
     process.exit(1);
 } else if (!version.includes('next') && command.includes('next')) {
     console.log(
         chalk.bgRed.black('[ERROR]'),
-        chalk.red(`✘ Publish the latest version, please use`),
-        chalk.blue('npm run release')
+        chalk.red(`✘ Publish the latest version, please use ${chalk.blue('npm run release')}`),
     );
     process.exit(1);
 }
