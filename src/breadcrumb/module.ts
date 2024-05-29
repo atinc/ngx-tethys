c3da50f2625fbc5cd4d7c25215758401176c132e
JingXu <1332570609@qq.com>
2024-05-29 16:30:16 +0800

    feat(breadcrumb): 面包屑导航支持传入items时省略展示 #INFR-12463 @wangkai @xuhaifeng @wumeimin (#3098)

--- src/breadcrumb/module.ts
+++ src/breadcrumb/module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ThyBreadcrumb } from './breadcrumb.component';
 import { ThyBreadcrumbItem } from './breadcrumb-item.component';
 import { ThyIconModule } from 'ngx-tethys/icon';
+import { ThyActionModule } from 'ngx-tethys/action';
+import { ThyDropdownModule } from 'ngx-tethys/dropdown';
 
 @NgModule({
-    imports: [CommonModule, ThyIconModule, ThyBreadcrumb, ThyBreadcrumbItem],
+    imports: [CommonModule, ThyIconModule, ThyBreadcrumb, ThyBreadcrumbItem, ThyActionModule, ThyDropdownModule],
     exports: [ThyBreadcrumb, ThyBreadcrumbItem]
 })
 export class ThyBreadcrumbModule {}
---
+++ src/breadcrumb/test/breadcrumb-with-items.spec.ts
@@ -0,0 +1,85 @@
+import { fakeAsync, TestBed, ComponentFixture, tick, flush, discardPeriodicTasks } from '@angular/core/testing';
+import { ThyBreadcrumbModule } from '../module';
+import { Component } from '@angular/core';
+import { ThyBreadcrumbItem } from '../breadcrumb-item.component';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+@Component({
+    selector: 'thy-demo-breadcrumb-basic',
+    template: `
+        <thy-breadcrumb [thyItems]="breadcrumbs" [thyMaxCount]="maxCount" [thyExpandable]="expandable">
+            <ng-template #item let-value>{{ value }}</ng-template>
+        </thy-breadcrumb>
+    `
+})
+class ThyDemoBreadcrumbWithItemsComponent {
+    breadcrumbs = ['一级', '二级', '三级', '四级', '五级', '六级'];
+
+    maxCount = 4;
+
+    expandable = true;
+}
+
+describe('ThyBreadcrumb With Items', () => {
+    let fixture: ComponentFixture<ThyDemoBreadcrumbWithItemsComponent>;
+
+    beforeEach(fakeAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [ThyBreadcrumbModule, NoopAnimationsModule],
+            declarations: [ThyDemoBreadcrumbWithItemsComponent]
+        });
+
+        TestBed.compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ThyDemoBreadcrumbWithItemsComponent);
+        fixture.detectChanges();
+    });
+
+    it(`should ellipsis when items length less than or equal maxCount`, () => {
+        const breadcrumbs = ['一级', '二级', '三级', '四级'];
+        fixture.componentInstance.breadcrumbs = breadcrumbs;
+        fixture.detectChanges();
+
+        const breadcrumbItems = fixture.debugElement.queryAll(By.directive(ThyBreadcrumbItem));
+        expect(breadcrumbItems.length).toBe(breadcrumbs.length);
+        breadcrumbs.forEach((item, index) => {
+            expect(breadcrumbItems[index].nativeElement.textContent.trim()).toEqual(item);
+        });
+    });
+
+    it(`should not ellipsis when thyMaxCount is 0`, () => {
+        fixture.componentInstance.maxCount = 0;
+        fixture.detectChanges();
+        const breadcrumbItems = fixture.debugElement.queryAll(By.directive(ThyBreadcrumbItem));
+        expect(breadcrumbItems.length).toBe(fixture.componentInstance.breadcrumbs.length);
+    });
+
+    it(`should ellipsis correctly`, fakeAsync(() => {
+        const breadcrumbItems = fixture.debugElement.queryAll(By.directive(ThyBreadcrumbItem));
+        fixture.detectChanges();
+        expect(breadcrumbItems.length).toBe(fixture.componentInstance.maxCount);
+        const ellipsisAction: HTMLElement = breadcrumbItems[1].query(By.css('.ellipsis-action')).nativeElement;
+        expect(ellipsisAction).toBeTruthy();
+
+        ellipsisAction.click();
+        fixture.detectChanges();
+        tick();
+        fixture.detectChanges();
+        const dropdown = document.querySelector('thy-dropdown-menu');
+        const dropdownItems = dropdown.querySelectorAll('.dropdown-menu-item');
+        expect(dropdown).toBeTruthy();
+        expect(dropdownItems.length).toBe(3);
+        discardPeriodicTasks();
+    }));
+
+    it(`should expandable worked`, () => {
+        fixture.componentInstance.expandable = false;
+        fixture.detectChanges();
+        const breadcrumbItems = fixture.debugElement.queryAll(By.directive(ThyBreadcrumbItem));
+        const ellipsisAction: HTMLElement = breadcrumbItems[1].query(By.css('.ellipsis-action')).nativeElement;
+        expect(ellipsisAction.classList.contains('disabled')).toBeTruthy();
+    });
+});
