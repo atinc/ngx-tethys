7691a700a4ff4bb11eec38386a97b4959e4d6a72
ggxxgxq <1356857447@qq.com>
2024-07-12 17:46:48 +0800

    fix: fix review

---
+++ src/stepper/examples/custom/custom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thy-stepper-custom-example',
+    templateUrl: './custom.component.html',
+    styleUrls: ['./custom.component.scss']
+})
+export class ThyStepperCustomExampleComponent {
+    constructor() {}
+}
