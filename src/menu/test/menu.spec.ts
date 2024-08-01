cc4ca3ca5d2d1176c020935731dc6f41b129172c
why520crazy <why520crazy@163.com>
2024-08-01 14:41:41 +0800

    test: import fakeAsync

--- src/menu/test/menu.spec.ts
+++ src/menu/test/menu.spec.ts
@@ -1,7 +1,7 @@
 import { bypassSanitizeProvider, injectDefaultSvgIconSet } from 'ngx-tethys/testing';
 
 import { Component, DebugElement, NgModule, ViewChild } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
