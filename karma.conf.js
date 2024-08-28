d13574f5562ef8e592adc77ca4efcd12e831388d
minlovehua <321512939@qq.com>
2024-08-28 13:46:37 +0800

    test: eliminate environmental differences for test to fix ci error

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
         basePath: '',
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -38,7 +38,7 @@ module.exports = function(config) {
         customLaunchers: {
             ChromeHeadlessCI: {
                 base: 'ChromeHeadless',
-                flags: ['--no-sandbox']
+                flags: ['--no-sandbox', '--window-size=1920']
             }
         },
         restartOnFileChange: true
