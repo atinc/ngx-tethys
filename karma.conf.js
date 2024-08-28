d81356c945a8aa91ae6e3689b8d251c6bf77e432
minlovehua <321512939@qq.com>
2024-08-28 13:35:12 +0800

    test: eliminate environmental differences for test to fix ci error

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
         customLaunchers: {
             ChromeHeadlessCI: {
                 base: 'ChromeHeadless',
-                flags: ['--no-sandbox', '--window-size=1280,800']
+                flags: ['--no-sandbox', '--window-size=1920,1080']
             }
         },
         restartOnFileChange: true
