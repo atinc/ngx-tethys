04efa2ca0feabe5b46a71b53f65a3cd980afe1ee
minlovehua <321512939@qq.com>
2024-10-08 11:15:41 +0800

    refactor: dropdown examples

--- src/dropdown/examples/immediate-render/immediate-menu.component.ts
+++ src/dropdown/examples/immediate-render/immediate-menu.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { ThyDropdownAbstractMenu } from 'ngx-tethys/dropdown';
 
 @Component({
     selector: 'thy-custom-menu',
     template: `
         <thy-dropdown-menu thyImmediateRender>
-            <a thyDropdownMenuItem href="javascript:;">
+            <a class="active" thyDropdownMenuItem href="javascript:;">
                 <span>Immediate Menu Item1</span>
             </a>
             <a thyDropdownMenuItem href="javascript:;">
