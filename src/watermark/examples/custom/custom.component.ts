caaa12dcdb8588aa11f0a487981737fd94e2f479
minlovehua <321512939@qq.com>
2024-11-05 17:29:49 +0800

    feat(watermark): support dark theme for watermark component #TINFR-501

--- src/watermark/examples/custom/custom.component.ts
+++ src/watermark/examples/custom/custom.component.ts
@@ -10,7 +10,7 @@ export class ThyWatermarkCanvasCustomExampleComponent implements OnInit {
     thyCanvasConfig: ThyCanvasConfigType = {
         degree: 30, // 偏移角度
         fontSize: 13, // 字体大小
-        color: 'rgb(255, 192, 203, 0.7)', // 字体颜色
+        color: ['rgb(255, 192, 203, 0.7)', 'rgb(255, 192, 203, 0.3)'], // 字体颜色
         textLineHeight: 25, // 行高
         gutter: [5, 5] // 横纵间距
     };
