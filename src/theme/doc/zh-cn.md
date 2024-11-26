4b146c010e2b0b44c5e4ab5fb8c5588ba22029e9
minlovehua <321512939@qq.com>
2024-11-26 13:53:24 +0800

    docs(theme): docs for light and dark theme

--- src/theme/doc/zh-cn.md
+++ src/theme/doc/zh-cn.md
@@ -9,10 +9,17 @@ subtitle: 主题
 
 
 
-## 主题
+## 变量设计
 目前支持`亮色主题`和`暗黑主题`两套主题颜色。
 <example name="thy-theme-color-example"/>
 
+## 基本使用
+NGX-TETHYS 使用 CSS 变量来构建主题。支持在 .html、.ts、.scss 等文件中通过`var(--gray-*)`的方式使用主题颜色。
+<example name="thy-theme-basic-example"/>
 
 ## 动态切换主题
+组件库通过`:root`元素上的`theme`属性来设置当前的主题，你只要修改这个属性，即可完成主题的切换。
 <example name="thy-theme-switch-example">
+
+## 注意
+NGX-TETHYS 使用 CSS 变量来构建主题。不支持使用`rgba`且不支持使用 Sass 的 `color.adjust()`、`color.scale-color()` 等函数对 CSS 变量进行计算。
--- src/theme/examples/module.ts
+++ src/theme/examples/module.ts
@@ -3,7 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { ThyDividerModule } from 'ngx-tethys/divider';
 import { ThyGridModule } from 'ngx-tethys/grid';
 import { ThySegmentModule } from 'ngx-tethys/segment';
+import { ThySpaceModule } from 'ngx-tethys/space';
 
 export default {
-    imports: [CommonModule, FormsModule, ThySegmentModule, ThyGridModule, ThyDividerModule]
+    imports: [CommonModule, FormsModule, ThySegmentModule, ThyGridModule, ThyDividerModule, ThySpaceModule]
 };
