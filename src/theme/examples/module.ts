8dbef97ff388976ac437511a077c61eaed3c54ac
minlovehua <321512939@qq.com>
2024-11-26 13:17:11 +0800

    docs(theme): docs for light and dark theme

--- src/theme/examples/module.ts
+++ src/theme/examples/module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ThyDividerModule } from 'ngx-tethys/divider';
 import { ThyGridModule } from 'ngx-tethys/grid';
 import { ThySegmentModule } from 'ngx-tethys/segment';
 
 export default {
-    imports: [CommonModule, FormsModule, ThySegmentModule, ThyGridModule]
+    imports: [CommonModule, FormsModule, ThySegmentModule, ThyGridModule, ThyDividerModule]
 };
