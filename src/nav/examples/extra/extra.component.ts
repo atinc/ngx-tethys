ec84486506cad2034bd54f555f249aaf488d3186
minlovehua <321512939@qq.com>
2025-01-09 14:17:09 +0800

    feat(nav): support card type and support thyIsExtraAppend

--- src/nav/examples/extra/extra.component.ts
+++ src/nav/examples/extra/extra.component.ts
@@ -6,15 +6,12 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./extra.component.scss']
 })
 export class ThyNavExtraExampleComponent implements OnInit {
-    public activeIndex = 0;
+    public activeIndex = 1;
     public items: { index: number; name: string; disabled?: boolean }[] = [];
 
     ngOnInit(): void {
         for (let i = 0; i < 15; i++) {
             this.items.push({ index: i, name: `Item ${i + 1}` });
-            if (i === 3) {
-                this.items[i].disabled = true;
-            }
         }
     }
 }
