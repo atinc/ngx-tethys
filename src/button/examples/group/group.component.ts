757d2e04cadf1293884f8f0bd550518706b36ddb
minlovehua <321512939@qq.com>
2025-04-08 14:35:19 +0800

    feat: demo

--- src/button/examples/group/group.component.ts
+++ src/button/examples/group/group.component.ts
@@ -1,12 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ThyButton, ThyButtonGroup } from 'ngx-tethys/button';
+import { ThyButton, ThyButtonGroup, ThyButtonIcon } from 'ngx-tethys/button';
 import { ThyIcon } from 'ngx-tethys/icon';
 
 @Component({
     selector: 'thy-button-group-example',
     templateUrl: './group.component.html',
-    imports: [ThyButton, ThyButtonGroup, NgClass, ThyIcon]
+    imports: [ThyButton, ThyButtonGroup, ThyButtonIcon, NgClass, ThyIcon]
 })
 export class ThyButtonGroupExampleComponent implements OnInit {
     sizes = [
--- src/typography/examples/background/background.component.ts
+++ src/typography/examples/background/background.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ThySpace, ThySpaceItemDirective } from 'ngx-tethys/space';
-import { ThyText } from 'ngx-tethys/typography';
+import { ThyBackgroundColorDirective, ThyText, ThyTextColorDirective } from 'ngx-tethys/typography';
 
 @Component({
     selector: 'thy-typography-background-example',
     templateUrl: './background.component.html',
-    imports: [ThySpace, ThySpaceItemDirective, ThyText]
+    imports: [ThySpace, ThySpaceItemDirective, ThyText, ThyBackgroundColorDirective, ThyTextColorDirective]
 })
 export class ThyTypographyBackgroundExampleComponent implements OnInit {
     constructor() {}
