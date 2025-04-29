c5f85254066301ad539a601eaa482a10f0c9a3eb
minlovehua <321512939@qq.com>
2025-04-29 19:01:36 +0800

    refactor(collapse): migrate to signal for collapse #TINFR-1458

--- src/collapse/collapse.token.ts
+++ src/collapse/collapse.token.ts
@@ -1,8 +1,8 @@
-import { EventEmitter, InjectionToken, ModelSignal } from '@angular/core';
+import { InjectionToken, ModelSignal, OutputEmitterRef } from '@angular/core';
 
 export interface IThyCollapseItemComponent {
     thyActive: ModelSignal<boolean>;
-    thyActiveChange: EventEmitter<{ active: boolean; event: Event }>;
+    thyActiveChange: OutputEmitterRef<{ active: boolean; event: Event }>;
     markForCheck: () => void;
 }
 
