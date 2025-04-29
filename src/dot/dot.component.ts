4538dd1c1691e28f9b06967d201f6a5748fb0334
minlovehua <321512939@qq.com>
2025-04-29 19:06:29 +0800

    refactor(dot): migrate to signal for dot #TINFR-1467

--- src/dot/dot.component.ts
+++ src/dot/dot.component.ts
@@ -1,6 +1,17 @@
 import { isThemeColor, ThyThemeColor } from 'ngx-tethys/core';
 
-import { ChangeDetectionStrategy, Component, ElementRef, Input, Renderer2, ViewEncapsulation, inject } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Renderer2,
+    Signal,
+    ViewEncapsulation,
+    computed,
+    effect,
+    inject,
+    input
+} from '@angular/core';
 
 export type ThyColorType = ThyThemeColor | string;
 export type ThySizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xlg';
@@ -26,90 +37,71 @@ export const DEFAULT_SHAPE_NAME = 'circle';
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         class: 'thy-dot',
-        '[class.dot-theme-fill]': 'theme === "fill"',
-        '[class.dot-theme-outline]': 'theme === "outline"',
-        '[class.dot-shape-square]': 'shape === "square"',
-        '[class.dot-shape-circle]': 'shape === "circle"',
-        '[class.dot-size-md]': 'size === "md"',
-        '[class.dot-size-sm]': 'size === "sm"',
-        '[class.dot-size-xs]': 'size === "xs"',
-        '[class.dot-size-lg]': 'size === "lg"',
-        '[class.dot-size-xlg]': 'size === "xlg"'
+        '[class.dot-theme-fill]': 'theme() === "fill"',
+        '[class.dot-theme-outline]': 'theme() === "outline"',
+        '[class.dot-shape-square]': 'shape() === "square"',
+        '[class.dot-shape-circle]': 'shape() === "circle"',
+        '[class.dot-size-md]': 'size() === "md"',
+        '[class.dot-size-sm]': 'size() === "sm"',
+        '[class.dot-size-xs]': 'size() === "xs"',
+        '[class.dot-size-lg]': 'size() === "lg"',
+        '[class.dot-size-xlg]': 'size() === "xlg"'
     }
 })
 export class ThyDot {
     private el = inject(ElementRef);
     private renderer = inject(Renderer2);
-
-    public size: ThySizeType = DEFAULT_SIZE_NAME;
-    public theme: ThyThemeType = DEFAULT_THEME_NAME;
-    public shape: ThyShapeType = DEFAULT_SHAPE_NAME;
-    public color: ThyColorType = DEFAULT_COLOR_NAME;
     private nativeElement: HTMLElement;
+
     constructor() {
         this.nativeElement = this.el.nativeElement;
-        this.updateColorStyle();
+
+        effect(() => {
+            this.updateColorStyle();
+        });
     }
 
     /**
      * 颜色，可选值为：`primary` `success` `info` `warning` `danger` `default` `light`和自定义颜色，如`#2cccda` `red`  `rgb(153, 153, 153)`
      * @type ThyThemeColor | string
-     * @default primary
      */
-    @Input()
-    set thyColor(value: ThyColorType) {
-        if (value) {
-            this.color = value;
-            this.updateColorStyle();
-        }
-    }
+    readonly thyColor = input<ThyColorType>(DEFAULT_COLOR_NAME);
 
     /**
      * 大小
      * @type xs | sm | md | lg | xlg
-     * @default sm
      */
-    @Input()
-    set thySize(value: ThySizeType) {
-        if (value) {
-            this.size = value;
-        }
-    }
+    readonly thySize = input<ThySizeType>(DEFAULT_SIZE_NAME);
+
+    size: Signal<ThySizeType> = computed(() => this.thySize() || DEFAULT_SIZE_NAME);
 
     /**
      * 主题
      * @type outline(线框) | fill(填充)
-     * @default fill
      */
-    @Input()
-    set thyTheme(value: ThyThemeType) {
-        if (value) {
-            this.theme = value;
-        }
-    }
+    readonly thyTheme = input<ThyThemeType>(DEFAULT_THEME_NAME);
+
+    theme: Signal<ThyThemeType> = computed(() => this.thyTheme() || DEFAULT_THEME_NAME);
 
     /**
      * 形状
      * @type circle(圆形) | square(方形)
-     * @default circle
      */
-    @Input()
-    set thyShape(value: ThyShapeType) {
-        if (value) {
-            this.shape = value;
-        }
-    }
+    readonly thyShape = input<ThyShapeType>(DEFAULT_SHAPE_NAME);
+
+    shape: Signal<ThyShapeType> = computed(() => this.thyShape() || DEFAULT_SHAPE_NAME);
 
     updateColorStyle() {
         Array.from(this.nativeElement.classList)
             .filter(it => /^dot-color-[\w]+$/.test(it))
             .forEach(it => this.renderer.removeClass(this.nativeElement, it));
 
-        if (isThemeColor(this.color)) {
+        const color = this.thyColor() || DEFAULT_COLOR_NAME;
+        if (isThemeColor(color)) {
             this.renderer.setStyle(this.nativeElement, 'borderColor', 'none');
-            this.renderer.addClass(this.nativeElement, `dot-color-${this.color}`);
+            this.renderer.addClass(this.nativeElement, `dot-color-${color}`);
         } else {
-            this.renderer.setStyle(this.nativeElement, 'borderColor', this.color);
+            this.renderer.setStyle(this.nativeElement, 'borderColor', color);
         }
     }
 }
