db4c677173b40afea5a07fb923754de8bf8778cd
zhangwen <1062680993@qq.com>
2025-05-08 16:34:29 +0800

    refactor(card): migrate to signal for card #TINFR-1454

--- src/card/card.component.ts
+++ src/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input } from '@angular/core';
 import { coerceBooleanProperty } from 'ngx-tethys/util';
 
 /**
@@ -12,11 +12,11 @@ import { coerceBooleanProperty } from 'ngx-tethys/util';
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         class: 'thy-card',
-        '[class.thy-card--divided]': '!!thyDivided',
-        '[class.thy-card--bordered]': '!!thyBordered',
-        '[class.thy-card--clear-left-right-padding]': '!thyHasLeftRightPadding',
-        '[class.thy-card-sm]': 'thySize === "sm"',
-        '[class.thy-card-lg]': 'thySize === "lg"'
+        '[class.thy-card--divided]': '!!thyDivided()',
+        '[class.thy-card--bordered]': '!!thyBordered()',
+        '[class.thy-card--clear-left-right-padding]': '!thyHasLeftRightPadding()',
+        '[class.thy-card-sm]': 'thySize() === "sm"',
+        '[class.thy-card-lg]': 'thySize() === "lg"'
     }
 })
 export class ThyCard {
@@ -24,28 +24,24 @@ export class ThyCard {
      * 左右是否有内边距，已废弃，如需配置间距使用 spacing 工具样式覆盖默认间距
      * @deprecated
      */
-    @Input({ transform: coerceBooleanProperty })
-    thyHasLeftRightPadding: boolean | string = true;
+    readonly thyHasLeftRightPadding = input<boolean | string, boolean | string | number>(true, { transform: coerceBooleanProperty });
 
     /**
      * 是否是分割模式，分割模式头部和内容区之间有一条分割线
      * @default false
      */
-    @Input({ transform: coerceBooleanProperty })
-    thyDivided: boolean | string;
+    readonly thyDivided = input<boolean | string, boolean | string | number>(false, { transform: coerceBooleanProperty });
 
     /**
      * 是否有边框，边框颜色为#eee
      * @default false
      */
-    @Input({ transform: coerceBooleanProperty })
-    thyBordered: boolean | string;
+    readonly thyBordered = input<boolean | string, boolean | string | number>(false, { transform: coerceBooleanProperty });
 
     /**
      * 大小
      * @type sm | md | lg
      * @default md
      */
-    @Input()
-    thySize: 'md' | 'sm' | 'lg';
+    readonly thySize = input<'md' | 'sm' | 'lg'>('md');
 }
