3e71d222698d4bd1cce72a51f08a6f6435024fd5
luxiaobei <luxiaobei@worktile.com>
2025-05-15 14:04:34 +0800

    refactor(watermark): migrate inputs to signal #TINFR-1789

--- src/util/helpers/helpers.ts
+++ src/util/helpers/helpers.ts
@@ -1,6 +1,8 @@
 import { _isNumberValue, coerceCssPixelValue as coerceCssPixel } from '@angular/cdk/coercion';
 import { ElementRef, TemplateRef } from '@angular/core';
 
+export type ThyBoolean = boolean | string | number;
+
 export function isUndefined(value: any): value is undefined {
     return value === undefined;
 }
@@ -229,7 +231,7 @@ export function isElementRef(value: any): value is ElementRef {
 /**
  * fix angular cdk's coerceBooleanProperty will transform 0 to true
  */
-export function coerceBooleanProperty(value: boolean | string | number): boolean {
+export function coerceBooleanProperty(value: ThyBoolean): boolean {
     if (value === '' || (value && value !== 'false')) {
         return true;
     } else {
