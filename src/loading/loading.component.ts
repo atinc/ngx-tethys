952a9733ee7c74fd4e659966929bb86c5fc90c93
wangyuan-ky <342007229@qq.com>
2025-05-16 14:56:05 +0800

    refactor(empty): migrate to signal for loading #TINFR-1483

--- src/loading/loading.component.ts
+++ src/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, HostBinding, ChangeDetectionStrategy, ChangeDetectorRef, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, inject, input } from '@angular/core';
 
 import { coerceBooleanProperty } from 'ngx-tethys/util';
 
@@ -14,41 +14,22 @@ import { coerceBooleanProperty } from 'ngx-tethys/util';
     imports: []
 })
 export class ThyLoading {
-    private changeDetectorRef = inject(ChangeDetectorRef);
-
-    public isDone: boolean;
-
-    public tip: string;
-
-    public isMask: boolean;
-
     /**
      * 数据是否加载完成
      * @default false
      */
-    @Input({ transform: coerceBooleanProperty })
-    set thyDone(value: boolean) {
-        this.isDone = value;
-        this.changeDetectorRef.markForCheck();
-    }
+    readonly thyDone = input(false, { transform: coerceBooleanProperty });
 
     /**
      * 自定义加载提示文案
      */
-    @Input()
-    set thyTip(value: string) {
-        this.tip = value;
-        this.changeDetectorRef.markForCheck();
-    }
+    readonly thyTip = input<string>('');
 
     /**
      * 加载时是否启用嵌套遮罩模式，不传或传 false，没有遮罩层，加载完成出现内容
      */
-    @Input({ transform: coerceBooleanProperty })
-    set thyIsMask(value: boolean) {
-        this.isMask = value;
-        this.changeDetectorRef.markForCheck();
-    }
+    readonly thyIsMask = input(false, { transform: coerceBooleanProperty });
+
 
     @HostBinding('class.thy-loading') loadingClassName = true;
 }
