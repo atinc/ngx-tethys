d88df2ca611a6741f6060b1acd4e122347e8ef27
why520crazy <why520crazy@users.noreply.github.com>
2025-05-23 15:40:16 +0800

    feat(property): thy-property-item support single without  thy-properties #TINFR-2098 (#3453)

--- src/property/properties.component.ts
+++ src/property/properties.component.ts
@@ -14,8 +14,8 @@ export type ThyPropertiesLayout = 'horizontal' | 'vertical';
         class: 'thy-properties',
         '[class.thy-properties-vertical]': 'layout() === "vertical"',
         '[class.thy-properties-horizontal]': 'layout() === "horizontal"',
-        '[class.thy-properties-edit-trigger-hover]': 'thyEditTrigger() === "hover"',
-        '[class.thy-properties-edit-trigger-click]': 'thyEditTrigger() === "click"',
+        '[class.thy-property-edit-trigger-hover]': 'thyEditTrigger() === "hover"',
+        '[class.thy-property-edit-trigger-click]': 'thyEditTrigger() === "click"',
         '[style.grid-template-columns]': 'gridTemplateColumns()'
     }
 })
