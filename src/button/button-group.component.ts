23452b9381abca2db1e356ee1a83c1141e03bd1c
JingXu <1332570609@qq.com>
2025-06-12 11:09:06 +0800

    refactor(button): migrate to signal #TINFR-1452 @wumeimin @xinglu (#3470)
    * refactor(button): migrate to signal #TINFR-1452
    
    * fix: remove console
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- src/button/button-group.component.ts
+++ src/button/button-group.component.ts
@@ -1,6 +1,6 @@
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { useHostRenderer } from '@tethys/cdk/dom';
-import { Component, HostBinding, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, effect, HostBinding, input, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 export type ButtonGroupSize = 'sm' | 'lg' | 'xs' | 'md';
 
@@ -21,15 +21,13 @@ const buttonGroupSizeMap = {
 @Component({
     selector: 'thy-button-group',
     template: '<ng-content></ng-content>',
+    host: {
+        class: 'btn-group',
+        '[class.btn-group-clear-min-width]': 'thyClearMinWidth()'
+    },
     encapsulation: ViewEncapsulation.None
 })
-export class ThyButtonGroup implements OnInit {
-    private initialized = false;
-
-    private type: string;
-
-    private size: string;
-
+export class ThyButtonGroup {
     private hostRenderer = useHostRenderer();
 
     /**
@@ -37,54 +35,36 @@ export class ThyButtonGroup implements OnInit {
      * @type xs | sm | md | lg
      * @default md
      */
-    @Input()
-    set thySize(size: ButtonGroupSize) {
-        this.size = size;
-        if (this.initialized) {
-            this.setClasses();
-        }
-    }
+    readonly thySize = input<ButtonGroupSize>();
 
     /**
      * 类型
      * @type outline-default | outline-primary
      * @default outline-default
      */
-    @Input()
-    set thyType(type: ButtonGroupType) {
-        this.type = type;
-        if (this.initialized) {
-            this.setClasses();
-        }
-    }
+    readonly thyType = input<ButtonGroupType>();
 
     /**
      * 是否需要最小宽度，默认按钮最小宽度为80px
      * @default false
      */
-    @Input()
-    set thyClearMinWidth(value: string) {
-        this.thyClearMinWidthClassName = coerceBooleanProperty(value);
-    }
+    readonly thyClearMinWidth = input(false, { transform: coerceBooleanProperty });
 
-    @HostBinding('class.btn-group') _isButtonGroup = true;
-    @HostBinding(`class.btn-group-clear-min-width`)
-    thyClearMinWidthClassName = false;
-
-    constructor() {}
-
-    ngOnInit() {
-        this.setClasses();
-        this.initialized = true;
+    constructor() {
+        effect(() => {
+            this.setClasses();
+        });
     }
 
     private setClasses() {
+        const type = this.thyType();
+        const size = this.thySize();
         let classNames: string[] = [];
-        if (this.type) {
-            classNames.push(`btn-group-${this.type}`);
+        if (type) {
+            classNames.push(`btn-group-${type}`);
         }
-        if (buttonGroupSizeMap[this.size]) {
-            classNames.push(buttonGroupSizeMap[this.size]);
+        if (buttonGroupSizeMap[size]) {
+            classNames = classNames.concat(...buttonGroupSizeMap[size]);
         }
         this.hostRenderer.updateClass(classNames);
     }
--- src/button/button-icon.component.ts
+++ src/button/button-icon.component.ts
@@ -1,12 +1,12 @@
 import { NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit, ViewEncapsulation, computed, effect, input } from '@angular/core';
 import { useHostRenderer } from '@tethys/cdk/dom';
 import { ThyIcon } from 'ngx-tethys/icon';
-import { coerceBooleanProperty } from 'ngx-tethys/util';
+import { coerceBooleanProperty, ThyBooleanInput } from 'ngx-tethys/util';
 
 export type ThyButtonIconShape = '' | 'circle-dashed' | 'circle-solid' | 'circle-thick-dashed' | 'circle-thick-solid' | 'self-icon';
 
-const sizeClassesMap = {
+const sizeClassesMap: Record<string, string[]> = {
     lg: ['btn-icon-lg'],
     md: ['btn-icon-md'],
     sm: ['btn-icon-sm'],
@@ -34,140 +34,107 @@ const themeClassesMap: any = {
     selector: 'thy-button-icon,[thy-button-icon],[thyButtonIcon]',
     templateUrl: './button-icon.component.html',
     encapsulation: ViewEncapsulation.None,
+    host: {
+        class: 'btn btn-icon',
+        '[class.btn-icon-light]': 'thyLight()',
+        '[class.btn-icon-active]': 'thyActive()'
+    },
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [ThyIcon, NgClass]
 })
-export class ThyButtonIcon implements OnInit {
+export class ThyButtonIcon {
     /**
      * 大小
      * @type xs | sm | md | lg
      * @default 36px
      */
-    @Input()
-    set thySize(size: string) {
-        this.size = size;
-        this.setClasses();
-    }
+    readonly thySize = input<string>();
 
     /**
      * 图标, 和`thyButtonIcon`相同，当使用`thy-button-icon`时，只能使用 thyIcon 设置图标
      */
-    @Input()
-    set thyIcon(icon: string) {
-        this.setIconClass(icon);
-    }
+    readonly thyIcon = input<string>();
 
     /**
      * 图标按钮的图标
      */
-    @Input()
-    set thyButtonIcon(icon: string) {
-        this.setIconClass(icon);
-    }
+    readonly thyButtonIcon = input<string>();
 
     /**
      * 展示的形状，默认只显示字体图标图标，circle-dashed, circle-solid 展示成虚线,实线边框圆形图标, circle-thick-dashed, circle-thick-solid 边框加粗
      */
-    @Input()
-    set thyShape(value: ThyButtonIconShape) {
-        this.shape = value;
-        this.setClasses();
-    }
+    readonly thyShape = input<ThyButtonIconShape>();
 
     /**
      * 亮色，颜色更浅，适合左侧导航顶部的按钮
      * @default false
      */
-    @Input({ transform: coerceBooleanProperty })
-    set thyLight(value: boolean) {
-        this._isLighted = value;
-    }
+    readonly thyLight = input<boolean, ThyBooleanInput>(false, { transform: coerceBooleanProperty });
 
     /**
      * 设置为选中状态
      * @default false
      */
-    @Input({ transform: coerceBooleanProperty })
-    set thyActive(value: boolean) {
-        this._isActive = value;
-    }
+    readonly thyActive = input<boolean, ThyBooleanInput>(false, { transform: coerceBooleanProperty });
 
     /**
      * 按钮展示类型，默认图标移上去显示主色， danger-weak 鼠标移上去显示 danger 红色
      */
-    @Input()
-    set thyTheme(value: string) {
-        this.theme = value;
-        this.setClasses();
+    readonly thyTheme = input<string>();
+
+    readonly thyColor = input<string>();
+
+    constructor() {
+        effect(() => {
+            this.setClasses();
+        });
     }
-
-    constructor() {}
-
-    private initialized = false;
-
-    private shape: ThyButtonIconShape;
-
-    private size: string;
 
     private hostRenderer = useHostRenderer();
 
-    iconPrefix = 'wtf';
+    private icon = computed(() => {
+        return this.thyButtonIcon() || this.thyIcon();
+    });
 
-    iconClasses: string[];
+    private isWtfIcon = computed(() => {
+        const icon = this.icon();
+        return icon && icon.includes('wtf');
+    });
 
-    icon: string;
+    protected svgIconName = computed(() => {
+        if (!this.isWtfIcon()) {
+            return this.icon();
+        }
+        return null;
+    });
 
-    theme: string;
-
-    svgIconName: string;
-
-    @HostBinding('class.btn') _isBtn = true;
-    @HostBinding('class.btn-icon') _isBtnIcon = true;
-    @HostBinding('class.btn-icon-light') _isLighted = false;
-    @HostBinding('class.btn-icon-active') _isActive = false;
-
-    @Input() thyColor: string;
-
-    private setIconClass(icon: string) {
-        if (icon) {
-            if (icon.includes('wtf')) {
-                const classes = icon.split(' ');
-                if (classes.length === 1) {
-                    classes.unshift('wtf');
-                }
-                this.iconClasses = classes;
-                this.svgIconName = null;
-            } else {
-                this.svgIconName = icon;
+    protected iconClasses = computed<string[]>(() => {
+        const icon = this.icon();
+        if (this.isWtfIcon()) {
+            const classes = icon.split(' ');
+            if (classes.length === 1) {
+                classes.unshift('wtf');
             }
-        } else {
-            this.iconClasses = null;
-            this.svgIconName = null;
+            return classes;
         }
-    }
+        return null;
+    });
 
-    private setClasses(first = false) {
-        // 设置样式判断是否已经初始化，未初始化直接返回，除非是初次调用
-        // 只有 ngOnInit 调用会传入 first = true
-        if (!first && !this.initialized) {
-            return;
-        }
-        const classes = sizeClassesMap[this.size] ? [...sizeClassesMap[this.size]] : [];
-        if (this.shape && shapeClassesMap[this.shape]) {
-            shapeClassesMap[this.shape].forEach((className: string) => {
+    private setClasses() {
+        const size = this.thySize();
+        const shape = this.thyShape();
+        const theme = this.thyTheme();
+        const classes = sizeClassesMap[size] ? [...sizeClassesMap[size]] : [];
+        if (shape && shapeClassesMap[shape]) {
+            shapeClassesMap[shape].forEach((className: string) => {
                 classes.push(className);
             });
         }
-        if (this.theme && themeClassesMap[this.theme]) {
-            themeClassesMap[this.theme].forEach((className: string) => {
+        if (theme && themeClassesMap[theme]) {
+            themeClassesMap[theme].forEach((className: string) => {
                 classes.push(className);
             });
         }
         this.hostRenderer.updateClass(classes);
-    }
-
-    ngOnInit() {
-        this.setClasses(true);
-        this.initialized = true;
     }
 }
