c47519675f2a10983bf24ef3de9987989da888ff
minlovehua <321512939@qq.com>
2025-09-29 16:46:31 +0800

    perf(select): support virtual scroll for select to improve performance

--- src/shared/option/module.ts
+++ src/shared/option/module.ts
@@ -8,16 +8,25 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ThyListOption } from './list-option/list-option.component';
 import { ThyOptionGroup } from './option-group.component';
 import { ThyIconModule } from 'ngx-tethys/icon';
 import { ThyOption } from './option.component';
+import { ThyOptionRender } from './option-render.component';
 import { ThySelectOptionGroup } from './group/option-group.component';
-import { ThyOptionsContainer } from './options-container.component';
+import { ThyOptionGroupRender } from './group/option-group-render.component';
 
 @NgModule({
-    imports: [CommonModule, ThyIconModule, ThyListOption, ThyOptionGroup, ThyOption, ThySelectOptionGroup, ThyOptionsContainer],
-    exports: [ThyListOption, ThyOptionGroup, ThyOption, ThySelectOptionGroup, ThyOptionsContainer]
+    imports: [
+        CommonModule,
+        ThyIconModule,
+        ThyListOption,
+        ThyOptionGroup,
+        ThyOption,
+        ThyOptionRender,
+        ThySelectOptionGroup,
+        ThyOptionGroupRender
+    ],
+    exports: [ThyListOption, ThyOptionGroup, ThyOption, ThyOptionRender, ThySelectOptionGroup, ThyOptionGroupRender]
 })
 export class ThyOptionModule {}
