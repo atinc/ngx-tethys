c9563e3e3fde3ea02bf712c3fc3564611ef58672
luxiaobei <2548259062@qq.com>
2025-11-03 13:39:48 +0800

    pref(cascader): support virtual scroll #TINFR-2960 (#3601)

--- src/cascader/examples/width/width.component.ts
+++ src/cascader/examples/width/width.component.ts
@@ -1,32 +1,26 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ThyNotifyService } from 'ngx-tethys/notify';
-import { clone, options } from '../cascader-address-options';
 import { ThyCascader } from 'ngx-tethys/cascader';
+import { clone, options } from '../cascader-address-options';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'thy-cascader-multiple-example',
-    templateUrl: './multiple.component.html',
+    selector: 'thy-cascader-width-example',
+    templateUrl: './width.component.html',
     imports: [ThyCascader, FormsModule]
 })
-export class ThyCascaderMultipleExampleComponent implements OnInit {
+export class ThyCascaderWidthExampleComponent implements OnInit {
     private notifyService = inject(ThyNotifyService);
-
-    private timeout: any;
 
     public areaCode: any[] = [];
 
-    public values: any[] = [
-        ['12', '1201', '120102'],
-        ['12', '1201', '120103'],
-        ['14', '1404', '140471']
-    ];
+    public values: any[] = ['12', '1201', '120102'];
 
     ngOnInit() {
         this.areaCode = clone(options);
     }
 
     public selectChanges(values: any): void {
-        this.notifyService.info(`selected Value is ${values}`);
+        this.notifyService.info(`selected data is ${values}`);
     }
 }
--- src/cascader/index.ts
+++ src/cascader/index.ts
@@ -2,3 +2,4 @@ export * from './module';
 export * from './cascader.component';
 export * from './cascader-li.component';
 export * from './types';
+export * from './cascader.pipe';
