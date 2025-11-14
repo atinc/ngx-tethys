ea4082a5302094494aa2003b87f4c1355fcf4c8a
minlovehua <321512939@qq.com>
2025-11-14 08:20:47 +0800

    build(cdk): fix run lint by using the inject() function over constructor parameter injection

--- cdk/event/click-dispatcher.ts
+++ cdk/event/click-dispatcher.ts
@@ -1,5 +1,4 @@
-import { Injectable, NgZone, OnDestroy, inject, DOCUMENT } from '@angular/core';
-
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ThyEventDispatcher } from './event-dispatcher';
 
@@ -9,12 +8,7 @@ const DEFAULT_CLICKED_TIME = 100;
     providedIn: 'root'
 })
 export class ThyClickDispatcher extends ThyEventDispatcher {
-    constructor() {
-        const document = inject(DOCUMENT);
-        const ngZone = inject(NgZone);
-
-        super(document, ngZone, 'click');
-    }
+    eventName = 'click';
 
     clicked(auditTimeInMs: number = DEFAULT_CLICKED_TIME): Observable<Event> {
         return this.subscribe(auditTimeInMs);
