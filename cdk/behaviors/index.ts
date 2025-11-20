261f04f8b35d7dd1d8290cea4c3e6ede4df3c374
wumeimin0107 <wmm13612286703@gmail.com>
2025-11-20 18:00:23 +0800

    fix: export behaviors as public api (#3617)

--- cdk/behaviors/index.ts
+++ cdk/behaviors/index.ts
@@ -2,5 +2,5 @@ export * from './run-in-zone';
 export * from './async-behavior';
 export * from './action-behavior';
 export * from './error-handler';
-export { BehaviorContext } from './behavior';
+export { BehaviorContext, Behavior } from './behavior';
 export * from './types';
