6936d020525a93f16f14e174633b489f5aa5c2bb
wumeimin0107 <wmm13612286703@gmail.com>
2025-12-09 17:34:09 +0800

    feat(select): support thyItemSize #TINFR-3170 (#3635)

--- src/select/custom-select/custom-select.component.ts
+++ src/select/custom-select/custom-select.component.ts
@@ -227,18 +227,20 @@ export class ThySelect extends TabIndexDisabledControlValueAccessorMixin impleme
         return getFlexiblePositions(this.placement(), this.defaultOffset);
     });
 
-    public itemSize = SELECT_OPTION_MAX_HEIGHT;
+    public thyItemSize = input(SELECT_OPTION_MAX_HEIGHT, { transform: value => numberAttribute(value) });
 
     readonly virtualHeight = computed<number>(() => {
         const maxVirtualHeight = SELECT_PANEL_MAX_HEIGHT - SELECT_PANEL_PADDING_TOP - SELECT_PANEL_PADDING_BOTTOM;
-        const height = this.filteredGroupsAndOptions().length * this.itemSize;
+        const height = this.filteredGroupsAndOptions().length * this.thyItemSize();
         return Math.min(height, maxVirtualHeight);
     });
 
     /**
-     * 视觉上能看到的最大选项个数
+     * 出现滚动条时，视觉上能看到的最大选项个数
      */
-    private maxItemLength = 7;
+    private maxItemLength = computed(() => {
+        return Math.round(this.virtualHeight() / this.thyItemSize());
+    });
 
     public triggerRectWidth: WritableSignal<number> = signal(undefined);
 
@@ -786,7 +788,7 @@ export class ThySelect extends TabIndexDisabledControlValueAccessorMixin impleme
         this.scrolledIndex = index;
 
         if (this.thyEnableScrollLoad()) {
-            const isScrollToBottom = index + this.maxItemLength >= this.filteredGroupsAndOptions().length;
+            const isScrollToBottom = index + this.maxItemLength() >= this.filteredGroupsAndOptions().length;
             if (isScrollToBottom) {
                 this.thyOnScrollToBottom.emit();
             }
@@ -966,7 +968,7 @@ export class ThySelect extends TabIndexDisabledControlValueAccessorMixin impleme
             return;
         }
 
-        if (targetIndex < this.scrolledIndex || targetIndex >= this.scrolledIndex + this.maxItemLength) {
+        if (targetIndex < this.scrolledIndex || targetIndex >= this.scrolledIndex + this.maxItemLength()) {
             this.cdkVirtualScrollViewport()?.scrollToIndex(targetIndex || 0);
         }
 
