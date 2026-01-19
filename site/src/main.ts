c4bee2e2b16a0247eff87abd8fd635622dd7e1e9
why520crazy <why520crazy@users.noreply.github.com>
2026-01-19 10:30:33 +0800

    docs: use zoneless (#3704)

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,7 +1,6 @@
-import { provideZoneChangeDetection } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
 platformBrowser()
-    .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()] })
+    .bootstrapModule(AppModule, { applicationProviders: [] })
     .catch(err => console.error(err));
