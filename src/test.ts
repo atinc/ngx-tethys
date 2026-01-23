41e6c184cd7f0241954002d95166c1b50249971b
minlovehua <321512939@qq.com>
2026-01-23 10:23:09 +0800

    feat: migrate to zoneless

--- src/test.ts
+++ src/test.ts
@@ -1,21 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/testing';
-import { getTestBed, TestBed } from '@angular/core/testing';
+import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 import { setPrintErrorWhenIconNotFound } from './icon';
 import { setWarnDeprecation } from './util';
 import { DebounceTimeWrapper } from './core';
 import { debounce, interval } from 'rxjs';
-import { provideZoneChangeDetection } from '@angular/core';
-
-const originalConfigureTestingModule = TestBed.configureTestingModule;
-TestBed.configureTestingModule = moduleDef => {
-    return originalConfigureTestingModule.call(TestBed, {
-        ...moduleDef,
-        providers: [...(moduleDef.providers || []), provideZoneChangeDetection()]
-    });
-};
 
 /**
  * mock debounceTime for issue https://github.com/angular/angular/issues/44351
