b518485d161bc534ba2267244a8cbbb67da9eff5
why520crazy <why520crazy@163.com>
2026-05-26 15:57:01 +0800

    build: bump docgeni to 2.8.0-next.5

--- site/src/app/app.routes.server.ts
+++ site/src/app/app.routes.server.ts
@@ -1,9 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-  },
+    {
+        path: '~examples/:name',
+        renderMode: RenderMode.Client,
+    },
+    {
+        path: '**',
+        renderMode: RenderMode.Prerender,
+    },
 ];
