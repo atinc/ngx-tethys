05dc6b7d75472907033532b7d636d8116d2698df
pubuzhixing8 <pubuzhixing@gmail.com>
2026-07-01 11:50:55 +0800

    docs(popover): add flexible position demo

---
+++ src/popover/examples/flexible-position/flexible-position.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { THY_GLOBAL_CONFIG } from 'ngx-tethys/core';
+import { ThyButton } from 'ngx-tethys/button';
+import { ThyPopover, ThyPopoverBody, ThyPopoverConfig, ThyPopoverDirective, ThyPopoverHeader } from 'ngx-tethys/popover';
+
+@Component({
+    selector: 'thy-popover-flexible-position-example',
+    templateUrl: './flexible-position.component.html',
+    styleUrls: ['./flexible-position.component.scss'],
+    imports: [ThyButton, ThyPopoverDirective, ThyPopoverHeader, ThyPopoverBody],
+    providers: [
+        ThyPopover,
+        {
+            provide: THY_GLOBAL_CONFIG,
+            useValue: {
+                overlay: {
+                    flexiblePosition: false
+                }
+            }
+        }
+    ]
+})
+export class ThyPopoverFlexiblePositionExampleComponent {
+    overrideFlexibleConfig: Partial<ThyPopoverConfig> = {
+        flexiblePosition: true
+    };
+}
