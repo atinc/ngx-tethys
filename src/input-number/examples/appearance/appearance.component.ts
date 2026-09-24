a173d98010250a62872dc4030ff6a9e22431efb8
mengshuicmq <mengshuicmq@163.com>
2026-09-24 15:59:29 +0800

    fix(input-number): fix example

--- src/input-number/examples/appearance/appearance.component.ts
+++ src/input-number/examples/appearance/appearance.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ThyColDirective, ThyRowDirective } from 'ngx-tethys/grid';
 import { ThyInputNumber } from 'ngx-tethys/input-number';
 
 @Component({
     selector: 'thy-input-number-appearance-example',
     templateUrl: './appearance.component.html',
     changeDetection: ChangeDetectionStrategy.Eager,
-    imports: [ThyInputNumber, FormsModule]
+    imports: [ThyInputNumber, ThyColDirective, ThyRowDirective, FormsModule]
 })
 export class ThyInputNumberAppearanceExampleComponent {
     value = 0;
